8e71673748042ce7ea81edf48cf31d1c8e370ac4
Pablo Manrubia <pmanrubia@gmail.com>
2016-11-23 14:55:54 +0000

    Add number of content items to Organisation JSON
    To count the number of content items per organisation we need to create
    the model (ContentItem) along with setting up the associations.
    
    To facilitate testing we are including the library Shoulda[1]
    
    We are using a very basic mechanism to serialise to JSON the
    organisations and the requested number of Content Items.
    This is because most of this code will go away once we expose
    an API to other apps. We will need to rely on ApiSerializers[3] or
    using libraries like Grape[2]
    
    It is not ideal either the way we are mocking the HTTP requests, as we
    are not webmoking the get request, but the library. Again, this is
    something that will go away in a few weeks, so no need to work any
    further.
    
    [1]: https://github.com/thoughtbot/shoulda-matchers
    [2]: https://github.com/ruby-grape/grape
    [3]: https://github.com/rails-api/active_model_serializers

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Organisation, type: :model do
+  it { should have_many(:content_items) }
+
+  describe '#total_content_items' do
+    it 'returns the total content items' do
+      content_item1 = ContentItem.new
+      content_item2 = ContentItem.new
+      organisation = Organisation.create!(slug: 'a_slug', content_items: [content_item1, content_item2])
+
+      expect(organisation.total_content_items).to eq(2)
+    end
+  end
 end
--- spec/requests/organisations_spec.rb
+++ spec/requests/organisations_spec.rb
@@ -8,11 +8,13 @@ RSpec.describe "Organisations", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it 'returns a JSON with the ids of the organisation' do
-      Organisation.create!(slug: 'a_slug')
+    it 'returns a JSON with the ids of the organisation and the number of content_items' do
+      content_item1 = ContentItem.new
+      content_item2 = ContentItem.new
+      Organisation.create!(slug: 'a_slug', content_items: [content_item1, content_item2])
 
       get organisations_path
-      expect(response.body).to eq([{slug: 'a_slug'}].to_json)
+      expect(response.body).to eq([{slug: 'a_slug', total_content_items: 2}].to_json)
     end
   end
 end
