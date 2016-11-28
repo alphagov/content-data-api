90c4d4b869f5b1a393c05ad1aa4bec509f082512
Pablo Manrubia <pmanrubia@gmail.com>
2016-11-28 10:48:15 +0000

    DRY-UP RSpec tests to import organisations
    A few refactorings to simplify the way the tests are setup.
    We are using `let` to setup the test fixture, and a method:
    `build_response` to build a SearchAPI response for the expected payload.

--- spec/models/importers/organisation_spec.rb
+++ spec/models/importers/organisation_spec.rb
@@ -1,28 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Importers::Organisation do
+  let(:one_content_item_response) { build_seach_api_response [content_id: 'content-id-1'] }
+  let(:two_content_items_response) { build_seach_api_response [{content_id: 'content-id-1'}, {content_id: 'content-id-2'}] }
+
   it 'queries the search API with the organisation\'s slug' do
-    response = double(body: {
-      results: [
-        {
-          some_field: 'some_value',
-        },
-      ]}.to_json)
-    expected_url = 'https://www.gov.uk/api/search.json?filter_organisations=MY-SLUG&count=99&fields=content_id'
-    expect(HTTParty).to receive(:get).with(expected_url).and_return(response)
+    expected_url = 'https://www.gov.uk/api/search.json?filter_organisations=MY-SLUG&count=99&fields=content_id&start=0'
+    expect(HTTParty).to receive(:get).with(expected_url).and_return(one_content_item_response)
 
     Importers::Organisation.run('MY-SLUG', batch: 99)
   end
 
   context 'Organisation' do
     it 'imports an organisation with the provided slug' do
-      response = double(body: {
-        results: [
-          {
-            some_field: 'some_value',
-          },
-        ]}.to_json)
-      allow(HTTParty).to receive(:get).and_return(response)
+      expect(HTTParty).to receive(:get).and_return(one_content_item_response)
 
       slug = 'hm-revenue-customs'
       Importers::Organisation.run(slug)
@@ -31,89 +22,55 @@ RSpec.describe Importers::Organisation do
       expect(Organisation.first.slug).to eq(slug)
     end
 
-
     it 'raises an exception with an organisation that does not exist' do
       response = double(body: {results: []}.to_json)
       allow(HTTParty).to receive(:get).and_return(response)
 
-      expect { Importers::Organisation.run("none-existing-org") }.to raise_error("No result for slug")
+      expect { Importers::Organisation.run('none-existing-org') }.to raise_error('No result for slug')
     end
   end
+
   context 'Content Items' do
     it 'imports all content items for the organisation' do
-      response = double(body: {
-        results: [
-          {
-            content_id: "number-1",
-          },
-          {
-            content_id: "number-2",
-          },
-        ]}.to_json)
-      allow(HTTParty).to receive(:get).and_return(response)
-      Importers::Organisation.run("a-slug")
-      organisation = Organisation.find_by(slug: "a-slug")
+      allow(HTTParty).to receive(:get).and_return(two_content_items_response)
+      Importers::Organisation.run('a-slug')
+      organisation = Organisation.find_by(slug: 'a-slug')
+
       expect(organisation.content_items.count).to eq(2)
     end
 
     it 'imports a `content_id` for every content item' do
-      response = double(body: {
-        results: [
-          {
-            content_id: "number-1",
-          },
-          {
-            content_id: "number-2",
-          },
-        ]}.to_json)
-      allow(HTTParty).to receive(:get).and_return(response)
-      Importers::Organisation.run("a-slug")
-      organisation = Organisation.find_by(slug: "a-slug")
+      allow(HTTParty).to receive(:get).and_return(two_content_items_response)
+      Importers::Organisation.run('a-slug')
+      organisation = Organisation.find_by(slug: 'a-slug')
 
       content_ids = organisation.content_items.pluck(:content_id)
-      expect(content_ids).to eq(%w(number-1 number-2))
+      expect(content_ids).to eq(%w(content-id-1 content-id-2))
     end
   end
 
   context 'Pagination' do
     it 'paginates through all the content items for an organisation' do
-      response1 = double(body: {
-        results: [
-          {
-            content_id: "number-1",
-          },
-          {
-            content_id: "number-2",
-          }
-        ]}.to_json)
-      response2 = double(body: {
-        results: [
-          {
-            content_id: "number-3",
-          },
-        ]}.to_json)
-      expect(HTTParty).to receive(:get).twice.and_return(response1, response2)
-      Importers::Organisation.run("a-slug", batch: 2)
-      organisation = Organisation.find_by(slug: "a-slug")
+      expect(HTTParty).to receive(:get).twice.and_return(two_content_items_response, one_content_item_response)
+      Importers::Organisation.run('a-slug', batch: 2)
+      organisation = Organisation.find_by(slug: 'a-slug')
 
       expect(organisation.content_items.count).to eq(3)
     end
 
     it 'handles last page with 0 results' do
-      response1 = double(body: {
-        results: [
-          {
-            content_id: "number-1",
-          },
-        ]}.to_json)
-      response2 = double(body: {
-        results: [
-        ]}.to_json)
-      expect(HTTParty).to receive(:get).twice.and_return(response1, response2)
-      Importers::Organisation.run("a-slug", batch: 1)
-      organisation = Organisation.find_by(slug: "a-slug")
+      expect(HTTParty).to receive(:get).twice.and_return(one_content_item_response, build_seach_api_response([]))
+      Importers::Organisation.run('a-slug', batch: 1)
+      organisation = Organisation.find_by(slug: 'a-slug')
 
       expect(organisation.content_items.count).to eq(1)
     end
   end
+
+  def build_seach_api_response(payload)
+    double(body: {
+      results: payload
+    }.to_json)
+  end
+
 end
