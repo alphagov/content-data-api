b796aac74439d83fa4cbcf21d75caeb6f0fd479e
Pablo Manrubia <pmanrubia@gmail.com>
2016-11-28 15:31:51 +0000

    Refactor Organisation importer: improve readability
    Extract methods in `Importers::Organisation` using the instance variables
    created in the previous commit.
    We extract methods with meaningful names that describe the behaviour.

--- app/models/importers/organisation.rb
+++ app/models/importers/organisation.rb
@@ -1,6 +1,7 @@
 class Importers::Organisation
 
   attr_reader :slug, :batch, :start
+  attr_writer :start
 
   def initialize(slug, batch: 10, start: 0)
     @slug = slug
@@ -11,19 +12,36 @@ class Importers::Organisation
   def run
     organisation = ::Organisation.create!(slug: slug)
     loop do
-      response = HTTParty.get "https://www.gov.uk/api/search.json?filter_organisations=#{slug}&count=#{batch}&fields=content_id&start=#{start}"
-      results = JSON.parse(response.body).fetch('results')
-
-      raise "No result for slug" if results.empty? && organisation.content_items.empty?
-
-      results.each do |result|
-        attributes = result.slice('content_id')
+      result = search_content_items_for_organisation
+      result.each do |content_item_attributes|
+        attributes = content_item_attributes.slice('content_id')
         organisation.content_items << ContentItem.new(attributes)
       end
 
-      break if results.length < batch
-      start += batch
+      break if last_page?(result)
+
+      next_page!
     end
+    raise 'No result for slug' if organisation.content_items.empty?
+  end
+
+  private
+
+  def last_page?(results)
+    results.length < batch
+  end
+
+  def next_page!
+    self.start += batch
+  end
+
+  def search_content_items_for_organisation
+    response = HTTParty.get(search_api_end_point)
+    JSON.parse(response.body).fetch('results')
+  end
+
+  def search_api_end_point
+    "https://www.gov.uk/api/search.json?filter_organisations=#{slug}&count=#{batch}&fields=content_id&start=#{start}"
   end
 end
 
