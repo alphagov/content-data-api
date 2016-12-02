8598ce87632da3b545dd4b798fffbb804ae18f4b
Ikenna Okpala <me@ikennaokpala.com>
2016-12-02 14:44:36 +0000

    Update Rspec tests.
    To obtain the last time a piece of content_item was updated, this endpoint
    [1] is queried with the base_path supplied. As a result of this extra call to
    the content item store, all organisation importer tests have been affected
    (i.e have to make at least call to search and content item api). A before
    hook has been introduced to stub calls to HTTParty's get method for search
    [2] and content item [1] api.
    
    To test the two calls to search [2] and content item [1] api, a with matcher
    from [3] is used to stub each call to these urls separately returning the
    expected response respectively.
    
    [1] /api/content/:content_item_base_path
    [2] /api/search.json?filter_organisations=hm-revenue-customs&start=0&count=10
    [3] http://www.relishapp.com/rspec/rspec-mocks/v/3-5/docs/setting-constraints/matching-arguments

--- spec/models/importers/organisation_spec.rb
+++ spec/models/importers/organisation_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Importers::Organisation do
+  let(:search_api_url_pattern) { /^https:\/\/www.gov.uk\/api\/search\.json\?.*$/ }
+  let(:content_items_api_url_pattern) { /^https:\/\/www.gov.uk\/api\/content\/.*$/ }
+
   let(:one_content_item_response) {
     build_search_api_response [
       {
@@ -33,41 +36,54 @@ RSpec.describe Importers::Organisation do
     )
   }
 
-  it "queries the search API with the organisation's slug" do
-    expected_url = 'https://www.gov.uk/api/search.json?filter_organisations=MY-SLUG&count=99&fields=content_id,link,title&start=0'
-    expected_content_url = 'https://www.gov.uk/api/content/item/1/path'
+  describe 'making API calls with HTTParty' do
+    it 'queries the search API with the organisation\'s slug' do
+      expected_url = 'https://www.gov.uk/api/search.json?filter_organisations=MY-SLUG&count=99&fields=content_id,link,title&start=0'
+      allow(HTTParty).to receive(:get).with(content_items_api_url_pattern).and_return(content_item_response)
+      expect(HTTParty).to receive(:get).once.with(expected_url).and_return(two_content_items_response)
 
-    expect(HTTParty).to receive(:get).once.with(expected_url).and_return(one_content_item_response)
-    allow(HTTParty).to receive(:get).once.with(expected_content_url).and_return(content_item_response)
+      Importers::Organisation.new('MY-SLUG', batch: 99).run
+    end
 
-    Importers::Organisation.new('MY-SLUG', batch: 99).run
+    it 'queries the content item API with base path' do
+      content_api_url_with_base_path_1 = 'https://www.gov.uk/api/content/item/1/path'
+      content_api_url_with_base_path_2 = 'https://www.gov.uk/api/content/item/2/path'
+
+      allow(HTTParty).to receive(:get).once.with(search_api_url_pattern).and_return(two_content_items_response)
+
+      expect(HTTParty).to receive(:get).once.with(content_api_url_with_base_path_1).and_return(content_item_response)
+      expect(HTTParty).to receive(:get).once.with(content_api_url_with_base_path_2).and_return(content_item_response)
+
+      Importers::Organisation.new('MY-SLUG', batch: 99).run
+    end
   end
 
   context 'Organisation' do
     it 'imports an organisation with the provided slug' do
-      allow(HTTParty).to receive(:get).and_return(one_content_item_response)
+      allow(HTTParty).to receive(:get).with(search_api_url_pattern).and_return(one_content_item_response)
+      allow(HTTParty).to receive(:get).with(content_items_api_url_pattern).and_return(content_item_response)
 
       slug = 'hm-revenue-customs'
-      Importers::Organisation.new(slug).run
-
-      expect(Organisation.count).to eq(1)
+      expect { Importers::Organisation.new(slug).run }.to change { Organisation.count }.by(1)
       expect(Organisation.first.slug).to eq(slug)
     end
 
     it 'raises an exception with an organisation that does not exist' do
       response = double(body: { results: [] }.to_json)
-      allow(HTTParty).to receive(:get).and_return(response)
+      allow(HTTParty).to receive(:get).once.with(search_api_url_pattern).and_return(response)
 
       expect { Importers::Organisation.new('none-existing-org').run }.to raise_error('No result for slug')
     end
   end
 
-  context 'Content Items' do
-    before { allow(HTTParty).to receive(:get).and_return(two_content_items_response) }
-    let(:organisation) { Organisation.find_by(slug: 'a-slug') }
+  describe 'Content Items' do
+    before { allow(HTTParty).to receive(:get).with(content_items_api_url_pattern).and_return(content_item_response) }
 
     it 'imports all content items for the organisation' do
+      allow(HTTParty).to receive(:get).with(search_api_url_pattern).and_return(two_content_items_response)
+
       Importers::Organisation.new('a-slug').run
+      organisation = Organisation.find_by(slug: 'a-slug')
       expect(organisation.content_items.count).to eq(2)
     end
 
@@ -86,54 +102,55 @@ RSpec.describe Importers::Organisation do
       ]))
 
       Importers::Organisation.new('a-slug').run
+      organisation = Organisation.find_by(slug: 'a-slug')
       expect(organisation.content_items.count).to eq(1)
     end
 
-    it 'imports a `content_id` for every content item' do
-      Importers::Organisation.new('a-slug').run
-      content_ids = organisation.content_items.pluck(:content_id)
-      expect(content_ids).to eq(%w(content-id-1 content-id-2))
-    end
+    describe 'Fields ' do
+      before { allow(HTTParty).to receive(:get).with(search_api_url_pattern).and_return(one_content_item_response) }
 
-    it 'imports a `link` for every content item' do
-      Importers::Organisation.new('a-slug').run
-      links = organisation.content_items.pluck(:link)
-      expect(links).to eq(%w(/item/1/path /item/2/path))
-    end
+      it 'imports a `content_id` for every content item' do
+        Importers::Organisation.new('a-slug').run
+        organisation = Organisation.find_by(slug: 'a-slug')
 
-    it 'imports a `title` for every content item' do
-      allow(HTTParty).to receive(:get).and_return(two_content_items_response)
-      Importers::Organisation.new('a-slug').run
+        content_ids = organisation.content_items.pluck(:content_id)
+        expect(content_ids).to eq(%w(content-id-1))
+      end
 
-      organisation = Organisation.find_by(slug: 'a-slug')
-      titles = organisation.content_items.pluck(:title)
+      it 'imports a `link` for every content item' do
+        Importers::Organisation.new('a-slug').run
+        organisation = Organisation.find_by(slug: 'a-slug')
+        links = organisation.content_items.pluck(:link)
+        expect(links).to eq(%w(/item/1/path))
+      end
 
-      expect(titles).to eq(%w(title-1 title-2))
-    end
+      it 'imports a `title` for every content item' do
+        Importers::Organisation.new('a-slug').run
 
-    it 'imports a `public_updated_at` for each content item' do
-      search_url = "https://www.gov.uk/api/search.json?filter_organisations=a-slug&count=10&fields=content_id,link&start=0"
-      content_base_path_1 = "https://www.gov.uk/api/content/item/1/path"
-      content_base_path_2 = "https://www.gov.uk/api/content/item/2/path"
+        organisation = Organisation.find_by(slug: 'a-slug')
+        titles = organisation.content_items.pluck(:title)
 
-      allow(HTTParty).to receive(:get).once.with(search_url).and_return(two_content_items_response)
-      allow(HTTParty).to receive(:get).once.with(content_base_path_1).and_return(content_item_response)
-      allow(HTTParty).to receive(:get).once.with(content_base_path_2).and_return(content_item_response)
+        expect(titles).to eq(%w(title-1))
+      end
 
-      Importers::Organisation.new('a-slug').run
-      organisation = Organisation.find_by(slug: 'a-slug')
+      it 'imports a `public_updated_at` for each content item' do
+        Importers::Organisation.new('a-slug').run
+        organisation = Organisation.find_by(slug: 'a-slug')
 
-      content_id = organisation.content_items.pluck(:content_id).first
-      public_update = organisation.content_items.pluck(:public_updated_at).first
+        content_item = organisation.content_items.first
 
-      expect(content_id).to eq('content-id-1')
-      expect(public_update).to eq(Time.parse('2016-11-01 11:20:45.481868'))
+        expect(content_item.content_id).to eq('content-id-1')
+        expect(content_item.public_updated_at).to eq(Time.parse('2016-11-01 11:20:45.481868'))
+      end
     end
   end
 
   context 'Pagination' do
+    before { allow(HTTParty).to receive(:get).with(content_items_api_url_pattern).and_return(content_item_response) }
+
     it 'paginates through all the content items for an organisation' do
-      expect(HTTParty).to receive(:get).exactly(5).times.and_return(two_content_items_response, one_content_item_response)
+      expect(HTTParty).to receive(:get).twice.with(search_api_url_pattern).and_return(two_content_items_response, one_content_item_response)
+
       Importers::Organisation.new('a-slug', batch: 2).run
       organisation = Organisation.find_by(slug: 'a-slug')
 
