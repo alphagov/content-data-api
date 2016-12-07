6c84eec5f7ee7405bb84dc45ca56e1bc8bbfb681
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-07 23:14:18 +0000

    Add `guard` to improve testing feedback
    Guard::RSpec[1] allows to automatically launch specs when files are
    modified. I have also included spring commands for spec [2] to speed up
    RSpec tests.
    
    To use it run:
    
    ```bash
    $ guard
    ```
    
    [1]: https://github.com/guard/guard-rspec
    [2]: https://github.com/jonleighton/spring-commands-rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,8 @@ require 'support/factory_girl'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+require 'shoulda-matchers'
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
