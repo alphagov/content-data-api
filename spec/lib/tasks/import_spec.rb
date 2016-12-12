5927f6e5741b97978c37f237c05eef7a062c6f84
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-12 17:16:38 +0000

    Refactor `Importers::Organisation` rake task test
    Use `instance_double`[1] to verifies that any methods being stubbed
    would be present on an instance of that class. In  addition, when it
    receives messages, it verifies that the provided arguments are supported
    by the method signature, both in terms of arity and allowed or required
    keyword  arguments, if any.
    
    Extract a description context for the constructor parameters so the
    description  of the RSpec object is more clear.
    
    ```text
    Randomized with seed 22688
    
    Import organisation rake task
      import:organisation
        runs the process to import the organisations
        Importer parameters
          receives the slug
          receives the logger
          raises an error if a slug parameter is not present
    
    Finished in 0.12665 seconds (files took 5.65 seconds to load)
    4 examples, 0 failures
    ```
    
    [1]: https://relishapp.com/rspec/rspec-mocks/v/3-2/docs/verifying-doubles/using-an-instance-double

--- spec/lib/tasks/import_spec.rb
+++ spec/lib/tasks/import_spec.rb
@@ -1,31 +1,38 @@
 require 'rails_helper'
 require 'rake'
 
-
 RSpec.describe 'Import organisation rake task' do
   describe 'import:organisation' do
     before do
       Rake::Task['import:organisation'].reenable
     end
 
-    let(:importer) { double('importer') }
-
     it 'runs the process to import the organisations' do
-      expect(Importers::Organisation).to receive(:new).with('a_slug', anything).and_return(importer)
-      expect(importer).to receive(:run)
+      expect_any_instance_of(Importers::Organisation).to receive(:run)
 
       Rake::Task['import:organisation'].invoke('a_slug')
     end
 
-    it 'pass the logger to the importer' do
-      expect(Importers::Organisation).to receive(:new).with(anything, hash_including(logger: instance_of(Logger))).and_return(importer)
-      allow(importer).to receive(:run)
+    describe 'Importer parameters' do
+      let(:importer) { instance_double(Importers::Organisation, run: nil) }
 
-      Rake::Task['import:organisation'].invoke('a-string')
-    end
+      it 'raises an error if a slug parameter is not present' do
+        expect { Rake::Task['import:organisation'].invoke }.to raise_error('Missing slug parameter')
+      end
 
-    it 'raises an error if a slug parameter is not present' do
-      expect { Rake::Task['import:organisation'].invoke }.to raise_error('Missing slug parameter')
+      it 'receives the slug' do
+        expected = 'a-slug'
+        expect(Importers::Organisation).to receive(:new).with(expected, anything).and_return(importer)
+
+        Rake::Task['import:organisation'].invoke('a-slug')
+      end
+
+      it 'receives the logger' do
+        expected = hash_including(logger: instance_of(Logger))
+        expect(Importers::Organisation).to receive(:new).with(anything, expected).and_return(importer)
+
+        Rake::Task['import:organisation'].invoke('a-string')
+      end
     end
   end
 end
