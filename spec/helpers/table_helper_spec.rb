8f09e0a646d695c018b9007a7b11930561d86ceb
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2016-12-19 15:31:38 +0000

    Add sortable column header
    New helper module for tables instead of in application helper.
    Includes sort_table_header to generate sortable table headers with state.
    
    TableHelper tests found in in table_helper_spec.rb

---
+++ spec/helpers/table_helper_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe TableHelper, type: :helper do
+  describe '#sort_table_header', :sort_table_header do
+    let(:organisation) { build(:organisation) }
+    before { assign(:organisation, organisation) }
+
+    let(:params_asc) { { order: 'asc', sort: attribute_name, organisation_id: organisation } }
+    let(:params_desc) { { order: 'desc', sort: attribute_name, organisation_id: organisation } }
+
+    let(:heading) { 'Last Updated' }
+    let(:attribute_name) { 'public_updated_at' }
+
+    subject { helper.sort_table_header(heading, attribute_name) }
+
+    describe 'Accessibility' do
+      context 'When the column is unsorted' do
+        it 'sets aria-sort to none ' do
+          expect(subject).to have_selector('[aria-sort=none]')
+        end
+
+        it 'sets screenreader text to sort asc' do
+          expect(subject).to have_selector('.visually-hidden', text: ', sort ascending'.downcase)
+        end
+      end
+
+      context 'When the column is sorted asc' do
+        before { controller.params = params_asc }
+
+        it 'sets aria-sort to ascending' do
+          expect(subject).to have_selector('[aria-sort=ascending]')
+        end
+
+        it 'sets screenreader text to sort desc' do
+          expect(subject).to have_selector('.visually-hidden', text: ', sort descending'.downcase)
+        end
+      end
+
+      context 'When the column is sorted desc' do
+        before { controller.params = params_desc }
+
+        it 'sets aria-sort to descending' do
+          expect(subject).to have_selector('[aria-sort=descending]')
+        end
+
+        it 'sets screenreader text to sort asc' do
+          expect(subject).to have_selector('.visually-hidden', text: ', sort ascending'.downcase)
+        end
+      end
+    end
+
+    describe 'Sorting' do
+      context 'When the column is unsorted' do
+        it 'has a link to sort asc' do
+          link_href = organisation_content_items_path(params_asc)
+
+          expect(subject).to have_link(href: link_href)
+        end
+      end
+
+      context 'When the column is sorted asc' do
+        before { controller.params = params_asc }
+
+        it 'has a link to sort desc' do
+          link_href = organisation_content_items_path(params_desc)
+
+          expect(subject).to have_link(href: link_href)
+        end
+      end
+
+      context 'When the column is sorted desc' do
+        before { controller.params = params_desc }
+
+        it 'has a link to sort asc' do
+          link_href = organisation_content_items_path(params_asc)
+
+          expect(subject).to have_link(href: link_href)
+        end
+      end
+    end
+  end
+end
