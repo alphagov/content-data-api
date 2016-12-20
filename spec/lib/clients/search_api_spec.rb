a8a31c242d433e97fc481271b2332e3b545316d4
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-20 14:05:18 +0100

    Remove `start` parameter in Clients::SearchAPI:fetch
    I have isolated the client to connect to the Search API, so we don't
    really need to expose the start parameter in the client API.
    
    This commit is part of a major refactor (PR 38). The problem it is
    trying to fix is: `::Importers::Organisation` has many responsibilities,
    and has grown without much control. It is  difficult to test and
    maintain,because it knows about the internals of Content Store and
    Search API (both API and data transformation).
    
    See PR 38 for additional context.

--- spec/lib/clients/search_api_spec.rb
+++ spec/lib/clients/search_api_spec.rb
@@ -37,9 +37,9 @@ RSpec.describe Clients::SearchAPI do
   describe 'making API calls with HTTParty' do
     it 'queries the search API with the organisation\'s slug' do
       expected_url = 'https://www.gov.uk/api/search.json?filter_organisations=MY-SLUG&count=99&fields=content_id,description,link,title,organisations&start=0'
-      expect(HTTParty).to receive(:get).once.with(expected_url).and_return(two_content_items)
+      expect(HTTParty).to receive(:get).once.with(expected_url).and_return(empty_response)
 
-      subject.fetch('MY-SLUG', batch: 99, start: 0) {}
+      subject.fetch('MY-SLUG', batch: 99) {}
     end
   end
 
@@ -57,13 +57,13 @@ RSpec.describe Clients::SearchAPI do
 
       expect(HTTParty).to receive(:get).twice.and_return(two_content_items, another_content_item)
 
-      expect { |b| subject.fetch('a-slug', batch: 2, start: 0, &b) }.to yield_control.exactly(3).times
+      expect { |b| subject.fetch('a-slug', batch: 2, &b) }.to yield_control.exactly(3).times
     end
 
     it 'handles last page with 0 results when organisation already has content items' do
       expect(HTTParty).to receive(:get).exactly(2).times.and_return(one_content_item, empty_response)
 
-      expect { |b| subject.fetch('a-slug', batch: 1, start: 0, &b) }.to yield_control.exactly(1).times
+      expect { |b| subject.fetch('a-slug', batch: 1, &b) }.to yield_control.exactly(1).times
     end
   end
 end
