16978aba1af8686e1720fb69597d113f2a67515a
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-12-20 15:37:05 +0000

    Display the url to the page on GOV.UK on the `detail` page

--- spec/views/content_items/show.html.erb_spec.rb
+++ spec/views/content_items/show.html.erb_spec.rb
@@ -3,15 +3,23 @@ require 'rails_helper'
 RSpec.describe 'content_items/show.html.erb', type: :view do
   let(:content_item) { build(:content_item) }
 
+  before do
+    assign(:content_item, content_item)
+  end
+
   it 'renders the title' do
     content_item.title = 'A Title'
-    assign(:content_item, content_item)
     render
 
     expect(rendered).to have_selector('h1', text: 'A Title')
   end
 
-  xit 'renders the url'
+  it 'renders the url' do
+    content_item.link = '/content/1/path'
+    render
+
+    expect(rendered).to have_link("Page on GOV.UK", href: 'https://gov.uk/content/1/path')
+  end
 
   xit 'renders the document type'
 
