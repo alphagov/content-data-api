dd09391b9641cfe80cd5b5a2b96760189793b0f6
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-12-21 13:51:24 +0000

    Import the `description` of a content item.
    1. Get the content item description from the search api.
    2. Add a description column to ContentItem
    3. Store the description in the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161215145222) do
+ActiveRecord::Schema.define(version: 20161221134137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161215145222) do
     t.string   "link"
     t.string   "title"
     t.string   "document_type"
+    t.string   "description"
     t.index ["content_id"], name: "index_content_items_on_content_id", unique: true, using: :btree
     t.index ["organisation_id"], name: "index_content_items_on_organisation_id", using: :btree
   end
