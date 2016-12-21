bd32b1ee5961016e1a258912e1eec19aeaca5d92
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2016-12-21 16:21:23 +0000

    Activate feature test for content item `detail` page
    This test was previously marked as pending, but now passes.

--- spec/features/content_item_spec.rb
+++ spec/features/content_item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Content Item Details", type: :feature do
   let(:organisation) { create(:organisation_with_content_items, content_items_count: 1) }
 
-  xscenario "the user clicks on the view content item link and is redirected to the content item show page" do
+  scenario "the user clicks on the view content item link and is redirected to the content item show page" do
     visit "organisations/#{organisation.id}/content_items"
     click_on "View detail"
 
