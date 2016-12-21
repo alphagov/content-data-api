ec81f168babf315f5d996bf1a884a1ab418ca587
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-21 15:14:22 +0100

    Use Collectors::ContentItems to fetch Content Items
    Relies on SearchAPI client to query content items. This step will make
    easier to reuse the client to perform additional queries (for example,
    organisations)
    
    This commit is part of a major refactor (PR 38). The problem it is
    trying to fix is: `::Importers::Organisation` has many responsibilities,
    and has grown without much control. It is  difficult to test and
    maintain,because it knows about the internals of Content Store and
    Search API (both API and data transformation).
    
    See PR 38 for additional context.

---
+++ spec/lib/collectors/content_items_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Collectors::ContentItems do
+  describe '#find_each' do
+    it 'queries the search API by organisation' do
+      expected_params = { filter_organisations: 'organisation-slug' }
+      expect_any_instance_of(Clients::SearchAPI).to receive(:find_each).with(expected_params, an_instance_of(Array))
+
+      subject.find_each('organisation-slug') {}
+    end
+    it 'queries the search API by organisation' do
+      expected_fields = %w(content_id link title organisations)
+      expect_any_instance_of(Clients::SearchAPI).to receive(:find_each).with(an_instance_of(Hash), expected_fields)
+
+      subject.find_each('organisation-slug') {}
+    end
+    it 'yields the content items' do
+      result = []
+      allow_any_instance_of(Clients::SearchAPI).to receive(:find_each).and_yield(:a).and_yield(:b)
+      subject.find_each('organisation-slug') { |value| result << value }
+
+      expect(result).to eq([:a, :b])
+    end
+    it 'raises an exception if no block passed' do
+      expect { subject.find_each('organisation-slug') }.to raise_exception('missing block!')
+    end
+  end
+end
