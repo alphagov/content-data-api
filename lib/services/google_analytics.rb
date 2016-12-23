b7747c1973def70ef38e74adf8195e30ce9a8c06
Ikenna Okpala <me@ikennaokpala.com>
2016-12-23 19:08:47 +0000

    Retrieving page view for more than one content item
    This adds implementation that supports the querying for page views from the
    Google Analytics Reporting API for more than one content item.
    
    The request builder has been altered to fit [2] and response from the API is
    returned with a data structure similar to [2].
    
    For more on query, see [1], [2] and [3]
    
    [1] https://developers.google.com/analytics/devguides/reporting/core/v4/parameters
    [2] https://developers.google.com/apis-explorer/?hl=en_GB#p/analyticsreporting/v4/analyticsreporting.reports.batchGet?_h=1&resource=%257B%250A++%2522reportRequests%2522%253A+%250A++%255B%250A++++%257B%250A++++++%2522viewId%2522%253A+%252212345678%2522%252C%250A++++++%2522dimensionFilterClauses%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522filters%2522%253A+%250A++++++++++%255B%250A++++++++++++%257B%250A++++++++++++++%2522expressions%2522%253A+%250A++++++++++++++%255B%2522%252Fcheck-uk-visa%2522%250A++++++++++++++%255D%252C%250A++++++++++++++%2522dimensionName%2522%253A+%2522ga%253ApagePath%2522%252C%250A++++++++++++++%2522operator%2522%253A+%2522EXACT%2522%250A++++++++++++%257D%252C%250A++++++++++++%257B%250A++++++++++++++%2522expressions%2522%253A+%250A++++++++++++++%255B%2522%252Fmarriage-abroad%2522%250A++++++++++++++%255D%252C%250A++++++++++++++%2522dimensionName%2522%253A+%2522ga%253ApagePath%2522%252C%250A++++++++++++++%2522operator%2522%253A+%2522EXACT%2522%250A++++++++++++%257D%250A++++++++++%255D%250A++++++++%257D%250A++++++%255D%252C%250A++++++%2522dateRanges%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522startDate%2522%253A+%25222016-12-19%2522%252C%250A++++++++++%2522endDate%2522%253A+%25222016-12-19%2522%250A++++++++%257D%250A++++++%255D%252C%250A++++++%2522dimensions%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522name%2522%253A+%2522ga%253ApagePath%2522%250A++++++++%257D%250A++++++%255D%250A++++%257D%250A++%255D%250A%257D&

--- lib/services/google_analytics.rb
+++ lib/services/google_analytics.rb
@@ -15,14 +15,19 @@ module Services
       @client
     end
 
-    def page_views(base_path)
-      request_body = build_page_views_body(base_path)
-      response = client.batch_get_reports(request_body)
+    def page_views(base_paths)
+      raise "base_paths isn't an array" unless base_paths.is_a?(Array)
 
-      response.reports[0].data.rows[0].metrics[0].values[0].to_i
+      request_body = build_page_views_body(base_paths)
+      data = client.batch_get_reports(request_body).reports[0].data
+
+      data.rows.inject({}) do |page_views, row|
+        page_views[row.dimensions[0]] = row.metrics[0].values[0].to_i
+        page_views
+      end
     end
 
-    def build_page_views_body(base_path, start_date: "7daysAgo", end_date: "today")
+    def build_page_views_body(base_paths, start_date: "7daysAgo", end_date: "today")
       GetReportsRequest.new.tap do |reports|
         reports.report_requests = Array.new.push(
           ReportRequest.new.tap do |request|
@@ -30,7 +35,20 @@ module Services
               Metric.new.tap { |metric| metric.expression = "ga:pageViews" }
             )
             request.view_id = ENV["CPM_GOVUK_VIEW_ID"]
-            request.filters_expression = "ga:pagePath==#{base_path}"
+            request.dimension_filter_clauses = Array.new.push(
+              DimensionFilterClause.new.tap do |dimension_filter_clause|
+                dimension_filter_clause.filters = base_paths.map do|base_path|
+                  DimensionFilter.new.tap do |dimension_filter|
+                    dimension_filter.expressions = %W(#{base_path})
+                    dimension_filter.dimension_name = "ga:pagePath"
+                    dimension_filter.operator = "EXACT"
+                  end
+                end
+              end
+            )
+            request.dimensions = Array.new.push(
+              Dimension.new.tap { |dimension| dimension.name = "ga:pagePath" }
+            )
             request.date_ranges = Array.new.push(
               DateRange.new.tap do |date_range|
                 date_range.start_date = start_date
