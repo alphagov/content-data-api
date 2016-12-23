d9863d7d0ab0338a131c18be7d0257f864df495f
Ikenna Okpala <me@ikennaokpala.com>
2016-12-23 19:08:47 +0000

    Retrieving page view for more than one content item
    This adds implementation that supports the querying for page views from the
    Google Analytics Reporting API for more than one content item.
    
    The request builder has been altered to fit [2] and response from the API is
    returned with a data structure similar to [2].
    
    For more on query, see [1], [2] and [3]
    
    [1] https://developers.google.com/analytics/devguides/reporting/core/v4/parameters
    [2] https://developers.google.com/apis-explorer/?hl=en_GB#p/analyticsreporting/v4/analyticsreporting.reports.batchGet?_h=1&resource=%257B%250A++%2522reportRequests%2522%253A+%250A++%255B%250A++++%257B%250A++++++%2522viewId%2522%253A+%252212345678%2522%252C%250A++++++%2522dimensionFilterClauses%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522filters%2522%253A+%250A++++++++++%255B%250A++++++++++++%257B%250A++++++++++++++%2522expressions%2522%253A+%250A++++++++++++++%255B%2522%252Fcheck-uk-visa%2522%250A++++++++++++++%255D%252C%250A++++++++++++++%2522dimensionName%2522%253A+%2522ga%253ApagePath%2522%252C%250A++++++++++++++%2522operator%2522%253A+%2522EXACT%2522%250A++++++++++++%257D%252C%250A++++++++++++%257B%250A++++++++++++++%2522expressions%2522%253A+%250A++++++++++++++%255B%2522%252Fmarriage-abroad%2522%250A++++++++++++++%255D%252C%250A++++++++++++++%2522dimensionName%2522%253A+%2522ga%253ApagePath%2522%252C%250A++++++++++++++%2522operator%2522%253A+%2522EXACT%2522%250A++++++++++++%257D%250A++++++++++%255D%250A++++++++%257D%250A++++++%255D%252C%250A++++++%2522dateRanges%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522startDate%2522%253A+%25222016-12-19%2522%252C%250A++++++++++%2522endDate%2522%253A+%25222016-12-19%2522%250A++++++++%257D%250A++++++%255D%252C%250A++++++%2522dimensions%2522%253A+%250A++++++%255B%250A++++++++%257B%250A++++++++++%2522name%2522%253A+%2522ga%253ApagePath%2522%250A++++++++%257D%250A++++++%255D%250A++++%257D%250A++%255D%250A%257D&

--- spec/lib/services/google_analytics_spec.rb
+++ spec/lib/services/google_analytics_spec.rb
@@ -57,13 +57,28 @@ RSpec.describe Services::GoogleAnalytics do
         {
           report_requests: [
             {
+              metrics: [
+                {
+                  expression: "ga:pageViews"
+                }
+              ],
               view_id: "12345678",
-              filters_expression: "ga:pagePath==/check-uk-visa",
-                metrics: [
-                  {
-                    expression: "ga:pageViews"
-                  }
-                ],
+              dimension_filter_clauses: [
+                {
+                  filters: [
+                    {
+                      expressions: ["/check-uk-visa"],
+                      dimension_name: "ga:pagePath",
+                      operator: "EXACT"
+                    }
+                  ]
+                }
+              ],
+              dimensions: [
+                {
+                  name: "ga:pagePath"
+                }
+              ],
               date_ranges: [
                 {
                   start_date: "7daysAgo",
@@ -82,10 +97,44 @@ RSpec.describe Services::GoogleAnalytics do
               data: Google::Apis::AnalyticsreportingV4::ReportData.new(
                 rows: [
                   Google::Apis::AnalyticsreportingV4::ReportRow.new(
+                    dimensions: ["/check-uk-visa"],
                     metrics: [
                       Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
                         values: [
                           "400"
+                        ]
+                      )
+                    ]
+                  )
+                ]
+              )
+            )
+          ]
+        )
+      end
+
+      let(:two_page_views_response) do
+        Google::Apis::AnalyticsreportingV4::GetReportsResponse.new(
+          reports: [
+            Google::Apis::AnalyticsreportingV4::Report.new(
+              data: Google::Apis::AnalyticsreportingV4::ReportData.new(
+                rows: [
+                  Google::Apis::AnalyticsreportingV4::ReportRow.new(
+                    dimensions: ["/check-uk-visa"],
+                    metrics: [
+                      Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
+                        values: [
+                          "400"
+                        ]
+                      )
+                    ]
+                  ),
+                  Google::Apis::AnalyticsreportingV4::ReportRow.new(
+                    dimensions: ["/marriage-abroad"],
+                    metrics: [
+                      Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
+                        values: [
+                          "500"
                         ]
                       )
                     ]
@@ -109,16 +158,35 @@ RSpec.describe Services::GoogleAnalytics do
       subject { Services::GoogleAnalytics.new }
 
       it "returns the number of page views for a content item" do
+        base_path_views = { "/check-uk-visa" => 400 }.with_indifferent_access
         allow(subject.client)
           .to receive(:batch_get_reports).and_return(page_views_response)
-        page_views = subject.page_views("/check-uk-visa")
+        page_views = subject.page_views(["/check-uk-visa"])
 
-        expect(page_views).to eq(400)
+        expect(page_views).to include(base_path_views)
+        expect(page_views["/check-uk-visa"]).to eq(400)
+      end
+
+      it "returns the number of page views for content items" do
+        base_paths = ["/check-uk-visa", "/marriage-abroad"]
+        allow(subject.client)
+          .to receive(:batch_get_reports).and_return(two_page_views_response)
+        page_views = subject.page_views(base_paths)
+
+        expect(page_views.keys).to match_array(base_paths)
+        expect(page_views.values).to match_array([400, 500])
+      end
+
+      it "raises an exception when another type is supplied, instead of an Array" do
+        allow(subject.client)
+          .to receive(:batch_get_reports).and_return(two_page_views_response)
+
+        expect { subject.page_views("/marriage-abroad") }.to raise_error("base_paths isn't an array")
       end
 
       it "builds the requests body with default arguments" do
         built_request = subject.
-          build_page_views_body("/check-uk-visa").as_json
+          build_page_views_body(["/check-uk-visa"]).as_json
 
         expect(built_request).to include(page_views_request)
       end
@@ -128,7 +196,7 @@ RSpec.describe Services::GoogleAnalytics do
         page_views_request[:report_requests][0][:date_ranges][0][:end_date] = "2016/12/22"
 
         built_request = subject.build_page_views_body(
-          "/check-uk-visa",
+          ["/check-uk-visa"],
           start_date: "2016/11/22",
           end_date: "2016/12/22").as_json
 
