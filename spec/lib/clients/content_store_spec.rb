36ddb89d32a9807df45c63a4910ff90796592033
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-26 12:28:28 +0100

    Use class method: `ContentStore.find`
    There is no state in the object. I have also renamed the method `#fetch`
    into `.find` to improve readability.
    
    This commit is part of a major refactor (PR 38). The problem it is
    trying to fix is: `::Importers::Organisation` has many responsibilities,
    and has grown without much control. It is  difficult to test and
    maintain,because it knows about the internals of Content Store and
    Search API (both API and data transformation).
    
    See PR 38 for additional context.

--- spec/lib/clients/content_store_spec.rb
+++ spec/lib/clients/content_store_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Clients::ContentStore do
+  subject { Clients::ContentStore }
+
   it 'queries the content item API with base path' do
     response = double(body: {}.to_json)
     expected_url = 'https://www.gov.uk/api/content/the-path'
     expect(HTTParty).to receive(:get).with(expected_url).and_return(response)
 
-    subject.fetch('/the-path', Array.new)
+    subject.find('/the-path', Array.new)
   end
 
   it 'returns the content item attributes' do
     response = double(body: { param1: :value1, param2: :value2 }.to_json)
     expect(HTTParty).to receive(:get).and_return(response)
 
-    expect(subject.fetch('/the-path', %i(param2))).to eq(param2: 'value2')
+    expect(subject.find('/the-path', %i(param2))).to eq(param2: 'value2')
   end
 end
