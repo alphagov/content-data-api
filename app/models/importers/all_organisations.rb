aa85030d0f3aa284fc2fa7d5123164efa698adf6
Pablo Manrubia <pmanrubia@gmail.com>
2016-12-27 07:35:39 +0100

    Extract method in Importers::AllOrganisations
    Also updates copy in RSpec tests
    
    ```
    Importers::AllOrganisations
      #import_all
        when the organisation already exists
          updates the attributes
          does not create a new one
        when the organisation does not exist
          updates the attributes
          creates an organisation per attribute group
    ```
    
    This commit is part of a major refactor (PR 38). The problem it is
    trying to fix is: `::Importers::Organisation` has many responsibilities,
    and has grown without much control. It is  difficult to test and
    maintain,because it knows about the internals of Content Store and
    Search API (both API and data transformation).
    
    See PR 38 for additional context.

--- app/models/importers/all_organisations.rb
+++ app/models/importers/all_organisations.rb
@@ -2,10 +2,16 @@ module Importers
   class AllOrganisations
     def run
       OrganisationsService.new.find_each do |attributes|
-        organisation = ::Organisation.find_or_create_by(slug: attributes.fetch(:slug))
-        organisation.assign_attributes(attributes)
-        organisation.save!
+        create_or_update!(attributes)
       end
+    end
+
+  private
+
+    def create_or_update!(attributes)
+      organisation_slug = attributes.fetch(:slug)
+      organisation = ::Organisation.find_or_create_by(slug: organisation_slug)
+      organisation.update!(attributes)
     end
   end
 end
