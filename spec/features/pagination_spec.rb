87f0c6b164b41191fef79bb9a694b19fdefe3e18
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2017-01-03 19:41:44 +0000

    Style and markup pagination after gov.uk component
    Change markup and style according to the
    "Previous and next navigation" GOV.UK component
    [http://govuk-component-guide.herokuapp.com/components/previous_and_next_navigation].
    
    It was copied & pasted instead of used directly
    because this app doesn't use slimmer (yet).
    Most of these changes should be deleted when slimmer is used.

--- spec/features/pagination_spec.rb
+++ spec/features/pagination_spec.rb
@@ -16,63 +16,26 @@ RSpec.feature "Content Item's Pagination", type: :feature do
     end
   end
 
-  context "When user navigates to" do
-    scenario "the first page, there is no previous link" do
-      visit "organisations/#{organisation.slug}/content_items"
+  scenario 'When user navigates to the first page, there is no previous link' do
+    visit "organisations/#{organisation.slug}/content_items"
 
-      expect(page).not_to have_selector('.pagination [rel=prev]')
-    end
+    expect(page).not_to have_selector('.govuk-previous-and-next-navigation [rel=prev]')
+    expect(page).to have_selector('.govuk-previous-and-next-navigation [rel=next]', text: '2 of 3')
+  end
 
-    scenario "the first page, there is a next link" do
-      visit "organisations/#{organisation.slug}/content_items"
+  scenario 'When user navigates to the next page, it navigates to page 2' do
+    visit "organisations/#{organisation.slug}/content_items"
+    page.find('.govuk-previous-and-next-navigation a[rel=next]').click
 
-      expect(page).to have_selector('.pagination [rel=next]')
-    end
+    expect(page).to have_selector('.govuk-previous-and-next-navigation [rel=prev]', text: '1 of 3')
+    expect(page).to have_selector('.govuk-previous-and-next-navigation [rel=next]', text: '3 of 3')
+  end
 
-    scenario "the first page, the next page number is 2" do
-      visit "organisations/#{organisation.slug}/content_items"
+  scenario 'When user navigates to the previous page from page 3, it navigates to page 2' do
+    visit "organisations/#{organisation.slug}/content_items?page=3"
+    page.find('.govuk-previous-and-next-navigation a[rel=prev]').click
 
-      expect(page).to have_selector('.pagination .next', text: '2 of 3')
-    end
-
-    scenario "the next page from page 1 the previous page number is 1" do
-      visit "organisations/#{organisation.slug}/content_items"
-      page.find('.pagination a[rel=next]').click
-
-      expect(page).to have_selector('.pagination .prev', text: '1 of 3')
-    end
-
-    scenario "the next page from page 1 the next page number is 3" do
-      visit "organisations/#{organisation.slug}/content_items"
-      page.find('.pagination a[rel=next]').click
-
-      expect(page).to have_selector('.pagination .next', text: '3 of 3')
-    end
-
-    scenario "the previous page from page 3 the next page number is 3" do
-      visit "organisations/#{organisation.slug}/content_items?page=3"
-      page.find('.pagination a[rel=prev]').click
-
-      expect(page).to have_selector('.pagination .next', text: '3 of 3')
-    end
-
-    scenario "the previous page from page 3 the previous page number is 1" do
-      visit "organisations/#{organisation.slug}/content_items?page=3"
-      page.find('.pagination a[rel=prev]').click
-
-      expect(page).to have_selector('.pagination .prev', text: '1 of 3')
-    end
-
-    scenario "the last page, there is no next link" do
-      visit "organisations/#{organisation.slug}/content_items?page=3"
-
-      expect(page).not_to have_selector('.pagination [rel=next]')
-    end
-
-    scenario "the last page, there is a previous link" do
-      visit "organisations/#{organisation.slug}/content_items?page=3"
-
-      expect(page).to have_selector('.pagination [rel=prev]')
-    end
+    expect(page).to have_selector('.govuk-previous-and-next-navigation [rel=prev]', text: '1 of 3')
+    expect(page).to have_selector('.govuk-previous-and-next-navigation [rel=next]', text: '3 of 3')
   end
 end
