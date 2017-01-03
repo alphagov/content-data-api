4333e1b2b97e3d3965a7893c11814becc5ba48d9
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2017-01-03 19:29:51 +0000

    Simplify pagination to only next/prev links
    This creates partials from the default Kaminari template
    [https://github.com/amatsuda/kaminari#customizing-the-pagination-helper]
    to only show next and previous links in the pagination
    together with an indicator of total pages and current page.
    
    This was simplified because previous user research showed
    that users rarely go past the second page.

--- spec/features/pagination_spec.rb
+++ spec/features/pagination_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Content Item's Pagination", type: :feature do
   before do
     Kaminari.configure do |config|
       @default_per_page = config.default_per_page
-      config.default_per_page = 2
+      config.default_per_page = 1
     end
   end
 
@@ -17,39 +17,62 @@ RSpec.feature "Content Item's Pagination", type: :feature do
   end
 
   context "When user navigates to" do
-    scenario "the first page, the current page number is 1" do
+    scenario "the first page, there is no previous link" do
       visit "organisations/#{organisation.slug}/content_items"
 
-      expect(page).to have_selector('nav.pagination .current', text: 1)
+      expect(page).not_to have_selector('.pagination [rel=prev]')
     end
 
-    scenario "the previous page from page 2, the current page number is 1" do
-      visit "organisations/#{organisation.slug}/content_items?page=2"
-      click_on "Prev"
-
-      expect(page).to have_selector('nav.pagination .current', text: 1)
-    end
-
-    scenario "the last page, the current page number is 2" do
+    scenario "the first page, there is a next link" do
       visit "organisations/#{organisation.slug}/content_items"
-      within("nav.pagination") do
-        click_on "Last"
-      end
 
-      expect(page).to have_selector('nav.pagination .current', text: 2)
+      expect(page).to have_selector('.pagination [rel=next]')
     end
 
-    scenario "the next page, the current page number is 2" do
+    scenario "the first page, the next page number is 2" do
       visit "organisations/#{organisation.slug}/content_items"
-      click_on "Next"
 
-      expect(page).to have_selector('nav.pagination .current', text: 2)
+      expect(page).to have_selector('.pagination .next', text: '2 of 3')
     end
-  end
 
-  scenario "The user can see information about the total number of content items" do
-    visit "organisations/#{organisation.slug}/content_items"
+    scenario "the next page from page 1 the previous page number is 1" do
+      visit "organisations/#{organisation.slug}/content_items"
+      page.find('.pagination a[rel=next]').click
 
-    expect(page).to have_selector('div#total-content-items', text: 'Displaying content items 1 - 2 of 3 in total')
+      expect(page).to have_selector('.pagination .prev', text: '1 of 3')
+    end
+
+    scenario "the next page from page 1 the next page number is 3" do
+      visit "organisations/#{organisation.slug}/content_items"
+      page.find('.pagination a[rel=next]').click
+
+      expect(page).to have_selector('.pagination .next', text: '3 of 3')
+    end
+
+    scenario "the previous page from page 3 the next page number is 3" do
+      visit "organisations/#{organisation.slug}/content_items?page=3"
+      page.find('.pagination a[rel=prev]').click
+
+      expect(page).to have_selector('.pagination .next', text: '3 of 3')
+    end
+
+    scenario "the previous page from page 3 the previous page number is 1" do
+      visit "organisations/#{organisation.slug}/content_items?page=3"
+      page.find('.pagination a[rel=prev]').click
+
+      expect(page).to have_selector('.pagination .prev', text: '1 of 3')
+    end
+
+    scenario "the last page, there is no next link" do
+      visit "organisations/#{organisation.slug}/content_items?page=3"
+
+      expect(page).not_to have_selector('.pagination [rel=next]')
+    end
+
+    scenario "the last page, there is a previous link" do
+      visit "organisations/#{organisation.slug}/content_items?page=3"
+
+      expect(page).to have_selector('.pagination [rel=prev]')
+    end
   end
 end
