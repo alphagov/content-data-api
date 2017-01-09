5b799ee5fa8313f829fc04c81167e37e14de9377
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-01-09 11:11:21 +0000

    Start refactoring out methods that could be moved to a helper class

--- app/services/google_analytics/requests/page_views_request.rb
+++ app/services/google_analytics/requests/page_views_request.rb
@@ -10,33 +10,63 @@ module GoogleAnalytics
         GetReportsRequest.new.tap do |reports|
           reports.report_requests = Array.new.push(
             ReportRequest.new.tap do |request|
-              request.metrics = Array.new.push(
-                Metric.new.tap { |metric| metric.expression = "ga:pageViews" }
-              )
-              request.view_id = ENV["CPM_GOVUK_VIEW_ID"]
-              request.dimension_filter_clauses = Array.new.push(
-                DimensionFilterClause.new.tap do |dimension_filter_clause|
-                  dimension_filter_clause.filters = base_paths.map do|base_path|
-                    DimensionFilter.new.tap do |dimension_filter|
-                      dimension_filter.expressions = %W(#{base_path})
-                      dimension_filter.dimension_name = "ga:pagePath"
-                      dimension_filter.operator = "EXACT"
-                    end
-                  end
-                end
-              )
-              request.dimensions = Array.new.push(
-                Dimension.new.tap { |dimension| dimension.name = "ga:pagePath" }
-              )
-              request.date_ranges = Array.new.push(
-                DateRange.new.tap do |date_range|
-                  date_range.start_date = start_date
-                  date_range.end_date = end_date
-                end
-              )
+              request.metrics = metrics(page_views)
+              request.view_id = view_id
+              request.dimension_filter_clauses = filters(base_paths, page_path)
+              request.dimensions = dimensions(page_path)
+              request.date_ranges = date_ranges(start_date, end_date)
             end
           )
         end
+      end
+
+    private
+
+      def date_ranges(start_date, end_date)
+        date_range = DateRange.new
+        date_range.start_date = start_date
+        date_range.end_date = end_date
+        [date_range]
+      end
+
+      def dimensions(name)
+        dimension = Dimension.new
+        dimension.name = name
+        [dimension]
+      end
+
+      def filters(base_paths, name, operator = "EXACT")
+        dimension_filter_clause = DimensionFilterClause.new
+        dimension_filter_clause.filters = base_paths.map do|base_path|
+          filter(base_path, name, operator)
+        end
+        [dimension_filter_clause]
+      end
+
+      def filter(path, name, operator = "EXACT")
+        DimensionFilter.new.tap do |dimension_filter|
+          dimension_filter.expressions = [path]
+          dimension_filter.dimension_name = name
+          dimension_filter.operator = operator
+        end
+      end
+
+      def metrics(name)
+        metric = Metric.new
+        metric.expression = name
+        [metric]
+      end
+
+      def page_path
+        "ga:pagePath"
+      end
+
+      def page_views
+        "ga:pageViews"
+      end
+
+      def view_id
+        @view_id ||= ENV["CPM_GOVUK_VIEW_ID"]
       end
     end
   end
