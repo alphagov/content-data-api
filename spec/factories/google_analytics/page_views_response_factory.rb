80bc90233eb566fe778b3f6547a722c7bc6d1cdc
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-01-10 15:46:51 +0000

    Add feature tests for GoogleAnalyticsService

---
+++ spec/factories/google_analytics/page_views_response_factory.rb
@@ -0,0 +1,29 @@
+module GoogleAnalytics
+  class PageViewsResponseFactory
+    def self.build(responses)
+      Google::Apis::AnalyticsreportingV4::GetReportsResponse.new(
+        reports: [
+          Google::Apis::AnalyticsreportingV4::Report.new(
+            data: Google::Apis::AnalyticsreportingV4::ReportData.new(
+              rows:
+                responses.map do |response|
+                  Google::Apis::AnalyticsreportingV4::ReportRow.new(
+                    dimensions: [
+                      response.fetch(:base_path)
+                    ],
+                    metrics: [
+                      Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
+                        values: [
+                          response.fetch(:page_views)
+                        ]
+                      )
+                    ]
+                  )
+                end
+            )
+          )
+        ]
+      )
+    end
+  end
+end
