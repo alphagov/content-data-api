97d09d407f74c45f9e70b68fcffd31e8cd08960b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-01-12 09:48:47 +0000

    Set a default value for the number_of_views

--- db/migrate/20170105100634_add_number_of_views_to_content_items.rb
+++ db/migrate/20170105100634_add_number_of_views_to_content_items.rb
@@ -1,5 +1,5 @@
 class AddNumberOfViewsToContentItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :content_items, :number_of_views, :integer
+    add_column :content_items, :number_of_views, :integer, default: 0
   end
 end
