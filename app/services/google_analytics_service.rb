3ab3bc7cdf6070d7e3ebadc4328445e354c00886
Ikenna N. Okpala <me@ikennaokpala.com>
2017-02-13 17:27:46 +0000

    Change UPV duration to one month ago
    Based on feedback from user research session content designers have
    highlighted that the previous 7 days unique page view isn’t enough
    data.
    
    Here I have changed the start date to always use the date from one month
    ago.
    
    This change makes for little need to change the existing code and tests.
    
    The the start date has been derived and converted to match [1] the defined
    format on [1].
    
    [1] https://developers.google.com/analytics/devguides/reporting/core/v3/reference#startDate

--- app/services/google_analytics_service.rb
+++ app/services/google_analytics_service.rb
@@ -6,7 +6,10 @@ class GoogleAnalyticsService
   def page_views(base_paths)
     raise "base_paths isn't an array" unless base_paths.is_a?(Array)
 
-    request = GoogleAnalytics::Requests::PageViewsRequest.new.build(base_paths)
+    request = GoogleAnalytics::Requests::PageViewsRequest.new.build(
+      base_paths,
+      start_date: 1.month.ago.strftime("%Y-%m-%d")
+    )
     response = client.batch_get_reports(request)
     GoogleAnalytics::Responses::PageViewsResponse.new.parse(response)
   end
