614c4dc321429d05df2c6463bed1289ab718f444
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2017-02-20 17:24:58 +0000

    Request number of page views from GA in batches
    We were sending a list of all of the content items in an organisation
    to Google Analytics and requesting the number of page views for them all.
    
    This is ok for organisations with a smaller number of content items, but
    for organisations with content items in the tens of thousands, the
    request fails.
    
    To fix this, the request to GA has been wrapped in a batch. The batch
    size has been hard-coded to 1 to get the test to pass, but will made to
    be configurable in the next commit.
    
    Remove unnecessary assignment in the existing test. Assigning content items
    to the organisation does affect the outcome of the test, so it has been
    removed.

--- spec/models/importers/number_of_views_by_organisation_spec.rb
+++ spec/models/importers/number_of_views_by_organisation_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
     let!(:content_item_first) { create(:content_item, base_path: 'the-link/first', organisations: [organisation]) }
     let!(:content_item_second) { create(:content_item, base_path: 'the-link/second', organisations: [organisation]) }
 
-    before do
+    it "updates the number of views for all content items" do
       allow_any_instance_of(GoogleAnalyticsService).to receive(:page_views).and_return(
         [
           {
@@ -19,11 +19,6 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
           },
         ]
       )
-    end
-
-    it "updates the number of views for all content items" do
-      organisation.content_items = [content_item_first, content_item_second]
-
       subject.run('the-slug')
 
       content_item_one = ContentItem.find_by(base_path: 'the-link/first')
@@ -31,6 +26,12 @@ RSpec.describe Importers::NumberOfViewsByOrganisation do
 
       expect(content_item_one.unique_page_views).to eq(3)
       expect(content_item_two.unique_page_views).to eq(2)
+    end
+
+    it "perform the requests in batches" do
+      expect_any_instance_of(GoogleAnalyticsService).to receive(:page_views).twice.and_return([])
+
+      subject.run('the-slug')
     end
   end
 end
