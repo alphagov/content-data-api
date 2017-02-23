b2fbea2ccedb645d5fc21e16f3c389162b243a59
Kelvin Gan <dev@kelvingan.co.uk>
2017-02-23 10:53:18 +0000

    Mock ContentItemQuery in controller spec
    As the ContentItemQuery has its own unit test, exercising the build
    method is unnecessary in the ContentItemsController and only needs to
    test that it is called correctly.

--- spec/controllers/content_items_controller_spec.rb
+++ spec/controllers/content_items_controller_spec.rb
@@ -2,56 +2,50 @@ require 'rails_helper'
 
 RSpec.describe ContentItemsController, type: :controller do
   describe "GET #index" do
+    context "unfiltered" do
+      let(:content_items) { create_list(:content_item_with_organisations, 3) }
 
-    before { allow(ContentItemsQuery).to receive(:build).and_return(:the_content_items) }
-
-    it "returns http success" do
-      get :index
-
-      expect(response).to have_http_status(:success)
-    end
-
-    it "assigns list of content items" do
-      get :index
-
-      expect(assigns(:content_items)).to eq(:the_content_items)
-    end
-
-    it "renders the :index template" do
-      get :index
-
-      expect(subject).to render_template(:index)
-    end
-
-    describe "Query filter" do
-      it "filters by organisation" do
-        organisation = create(:organisation, slug: 'organisation-slug')
-
-        get :index, params: { organisation_slug: 'organisation-slug' }
-
-        expect(ContentItemsQuery).to have_received(:build).with(hash_including(organisation: organisation))
+      before do
+        get :index
       end
 
-      it "paginates the results" do
-        get :index, params: { page: 10 }
-
-        expect(ContentItemsQuery).to have_received(:build).with(hash_including(page: "10"))
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
       end
 
-      it "sorts the results" do
-        get :index, params: { order: 'attribute', sort: 'asc' }
+      it "assigns list of content items" do
+        expect(assigns(:content_items)).to eq(content_items)
+      end
 
-        expect(ContentItemsQuery).to have_received(:build).with(hash_including(order: 'attribute', sort: 'asc'))
+      it "renders the :index template" do
+        expect(subject).to render_template(:index)
       end
     end
 
-    context "when organisation slug is present" do
-      let!(:organisation) { create(:organisation, slug: 'organisation-slug') }
+    context "find by organisation" do
+      let(:organisation) { create(:organisation) }
+
+      before do
+        get :index, params: { organisation_slug: organisation.slug }
+      end
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
 
       it "assigns current organisation" do
-        get :index, params: { organisation_slug: 'organisation-slug' }
-
         expect(assigns(:organisation)).to eq(organisation)
+      end
+
+      it "renders the :index template" do
+        expect(subject).to render_template(:index)
+      end
+    end
+
+    context "content items fetched via ContentItemQuery" do
+      it "returns a list of content items" do
+        allow(ContentItemsQuery).to receive(:build).and_return(:content_items)
+        expect(ContentItemsQuery.build({})).to eq(:content_items)
       end
     end
   end
