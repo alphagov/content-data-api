faf2868db50d44345bf49cb9a350eefdf7c3f528
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-06 18:34:06 +0000

    Retrieve `details` from content store
    Also move create_update into model.
    This allows the model to trim unknown attributes before saving.
    i.e. it doesn't know about `details`,
    now it will not attempt to save that field
    Moving this to the model stops the importer getting bloated.

--- app/models/importers/content_items_by_organisation.rb
+++ app/models/importers/content_items_by_organisation.rb
@@ -10,18 +10,8 @@ module Importers
       organisation = Organisation.find_by(slug: slug)
       ContentItemsService.new.find_each(slug) do |content_item_attributes|
         metric_builder.run_all(content_item_attributes)
-        create_or_update!(content_item_attributes, organisation)
+        ContentItem.create_or_update!(content_item_attributes, organisation)
       end
-    end
-
-  private
-
-    def create_or_update!(attributes, organisation)
-      content_id = attributes.fetch(:content_id)
-      content_item = ContentItem.find_or_create_by(content_id: content_id)
-
-      content_item.update!(attributes)
-      content_item.organisations << organisation unless content_item.organisations.include?(organisation)
     end
   end
 end
--- spec/models/importers/content_items_by_organisation_spec.rb
+++ spec/models/importers/content_items_by_organisation_spec.rb
@@ -3,62 +3,16 @@ require 'rails_helper'
 RSpec.describe Importers::ContentItemsByOrganisation do
   describe '#run' do
     let!(:organisation) { create(:organisation, slug: 'the-slug') }
-    let(:content_item) { create(:content_item, base_path: 'the-link', organisations: [organisation]) }
+    let!(:content_item) { create(:content_item, content_id: 'the-content-id', organisations: [organisation]) }
 
-    context 'when the content item does not exist' do
-      it 'creates a content item per attribute group' do
-        attrs1 = attributes_for(:content_item)
-        attrs2 = attributes_for(:content_item)
-        subject.metric_builder = double()
+    it 'creates or update all the content items' do
+      subject.metric_builder = double(run_all: {})
+      attrs1 = { content_id: 'the-content-id' }
+      attrs2 = { content_id: 'the-content-id2' }
+      allow_any_instance_of(ContentItemsService).to receive(:find_each).with('the-slug').and_yield(attrs1).and_yield(attrs2)
 
-        allow_any_instance_of(ContentItemsService).to receive(:find_each).with('the-slug').and_yield(attrs1).and_yield(attrs2)
-        allow(subject.metric_builder).to receive(:run_all).and_return({})
-
-        expect { subject.run('the-slug') }.to change { ContentItem.count }.by(2)
-      end
-
-      it 'updates the attributes' do
-        attrs1 = attributes_for(:content_item, base_path: 'the-link-value', title: 'the-title')
-        subject.metric_builder = double()
-
-        allow_any_instance_of(ContentItemsService).to receive(:find_each).and_yield(attrs1)
-        allow(subject.metric_builder).to receive(:run_all).and_return({})
-
-        subject.run('the-slug')
-
-        attributes = ContentItem.find_by(base_path: 'the-link-value').attributes.symbolize_keys
-        expect(attributes).to include(title: 'the-title')
-      end
+      expect { subject.run('the-slug') }.to change { ContentItem.count }.by(1)
     end
-
-    context 'when the content item already exists' do
-      let(:content_item) { create(:content_item, base_path: 'the-link', organisations: [organisation]) }
-
-      it 'does not create a new one' do
-        attributes = { content_id: content_item.content_id, base_path: 'the-link' }
-        subject.metric_builder = double()
-
-        allow_any_instance_of(ContentItemsService).to receive(:find_each).and_yield(attributes)
-        allow(subject.metric_builder).to receive(:run_all).and_return({})
-
-        expect { subject.run('the-slug') }.to change { ContentItem.count }.by(0)
-      end
-
-      it 'updates the attributes' do
-        content_item.update(title: 'old-title')
-        attributes = { content_id: content_item.content_id, title: 'the-new-title', base_path: 'the-link' }
-        subject.metric_builder = double()
-
-        allow_any_instance_of(ContentItemsService).to receive(:find_each).and_yield(attributes)
-        allow(subject.metric_builder).to receive(:run_all).and_return({})
-
-        subject.run('the-slug')
-
-        expect(ContentItem.first.title).to eq('the-new-title')
-      end
-    end
-
-
 
     it 'calls the metric builder for each content item' do
       attrs1 = attributes_for(:content_item)
