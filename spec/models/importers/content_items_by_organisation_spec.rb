fa44575503570f17cac8c2151c48905c1206586c
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-06 18:35:06 +0000

    Create content items with metrics
    Pass metrics to content item creation i.e. number_of_pdfs.
    Add migration for new pdf column.

--- spec/models/importers/content_items_by_organisation_spec.rb
+++ spec/models/importers/content_items_by_organisation_spec.rb
@@ -5,26 +5,25 @@ RSpec.describe Importers::ContentItemsByOrganisation do
     let!(:organisation) { create(:organisation, slug: 'the-slug') }
     let!(:content_item) { create(:content_item, content_id: 'the-content-id', organisations: [organisation]) }
 
-    it 'creates or update all the content items' do
-      subject.metric_builder = double(run_all: {})
+    it 'update the metrics of the content item' do
+      subject.metric_builder = double(run_all: { number_of_pdfs: 10 })
+      subject.content_items_service = double
       attrs1 = { content_id: 'the-content-id' }
-      attrs2 = { content_id: 'the-content-id2' }
-      allow_any_instance_of(ContentItemsService).to receive(:find_each).with('the-slug').and_yield(attrs1).and_yield(attrs2)
-
-      expect { subject.run('the-slug') }.to change { ContentItem.count }.by(1)
-    end
-
-    it 'calls the metric builder for each content item' do
-      attrs1 = attributes_for(:content_item)
-      attrs2 = attributes_for(:content_item)
-      subject.metric_builder = double()
-
-      allow_any_instance_of(ContentItemsService).to receive(:find_each).with('the-slug').and_yield(attrs1).and_yield(attrs2)
-      allow(subject.metric_builder).to receive(:run_all).and_return({})
-      
-      expect(subject.metric_builder).to receive(:run_all).twice
+      allow(subject.content_items_service).to receive(:find_each).with('the-slug').and_yield(attrs1)
 
       subject.run('the-slug')
+
+      expect(content_item.reload.number_of_pdfs).to eq(10)
+    end
+
+    it 'creates a new content item and updates an existing one in the same import' do
+      subject.metric_builder = double(run_all: {})
+      subject.content_items_service = double
+      attrs1 = { content_id: 'the-content-id' }
+      attrs2 = { content_id: 'the-content-id2' }
+      allow(subject.content_items_service).to receive(:find_each).with('the-slug').and_yield(attrs1).and_yield(attrs2)
+
+      expect { subject.run('the-slug') }.to change { ContentItem.count }.by(1)
     end
   end
 end
