c3e8bcc3f545353933ae27565fa530014bb21b01
Pablo Manrubia <pmanrubia@gmail.com>
2017-03-09 11:55:26 +0000

    Use decorator to render the organisation links

--- app/decorators/content_item_decorator.rb
+++ app/decorators/content_item_decorator.rb
@@ -8,4 +8,12 @@ class ContentItemDecorator < Draper::Decorator
       "Never"
     end
   end
+
+  def organisation_links
+    names = object.organisations.collect do |organisation|
+      helpers.link_to(organisation.title, helpers.content_items_path(organisation_slug: organisation.slug))
+    end
+
+    names.join(', ').html_safe
+  end
 end
