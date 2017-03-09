3c5adc97812312f6b546108ac73ebf35e4481513
Pablo Manrubia <pmanrubia@gmail.com>
2017-03-09 10:46:08 +0000

    Remove nested resource in OrganisationsHelper

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,7 +1,7 @@
 module OrganisationsHelper
   def stringify_organisations(organisations)
     names = organisations.collect do |organisation|
-      link_to(organisation.title, organisation_content_items_path(organisation_slug: organisation.slug))
+      link_to(organisation.title, content_items_path(organisation_slug: organisation.slug))
     end
 
     names.join(', ').html_safe
