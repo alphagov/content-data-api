dd533e3a0a46ce724cd28b1327784c8062a61406
Kelvin Gan <dev@kelvingan.co.uk>
2017-03-09 15:21:35 +0000

    Add delayed_job to page views import
    We are seeing issues with timeouts when we try to fetch page views for
    large sets of content items, so we are attempting to break the batches
    into separate queues running as background jobs.
    
    We moved the code that does the importing into the job. We also
    refactor the tests as now the tests are exercised against the job's
    code rather than for the importer itself. The importer now just calls
    the background job.

--- app/models/importers/number_of_views_by_organisation.rb
+++ app/models/importers/number_of_views_by_organisation.rb
@@ -1,19 +1,16 @@
 module Importers
   class NumberOfViewsByOrganisation
-    BATCH_SIZE = 3000
+    attr_accessor :batch_size
+
+    def initialize
+      self.batch_size = 500
+    end
 
     def run(slug)
       organisation = Organisation.find_by(slug: slug)
-      google_analytics_service = GoogleAnalyticsService.new
 
-      organisation.content_items.find_in_batches(batch_size: BATCH_SIZE) do |content_items|
-        base_paths = content_items.pluck(:base_path)
-
-        results = google_analytics_service.page_views(base_paths)
-        results.each do |result|
-          content_item = ContentItem.find_by(base_path: result[:base_path])
-          content_item.update!(one_month_page_views: result[:page_views][:one_month])
-        end
+      organisation.content_items.find_in_batches(batch_size: batch_size) do |content_items|
+        ImportPageviewsJob.perform_later(content_items)
       end
     end
   end
