074f9f46a7db9ec37fcf152d9cf03f66696455fd
Pablo Manrubia <pmanrubia@gmail.com>
2017-03-09 10:48:47 +0000

    Remove nested route from the routing spec

--- spec/routes/content_item_spec.rb
+++ spec/routes/content_item_spec.rb
@@ -2,27 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ContentItemsController, type: :routing do
   describe 'routing' do
-    context 'with organisation slug' do
-      it 'routes to #index' do
-        expect(get: organisation_content_items_path('a-slug')).to be_routable
-        expect(get: organisation_content_items_path('a-slug'))
-          .to route_to(
-            controller: 'content_items',
-            action: 'index',
-            organisation_slug: 'a-slug'
-          )
-      end
-    end
-
-    context 'without organisation slug' do
-      it 'routes to #index' do
-        expect(get: content_items_path).to be_routable
-        expect(get: content_items_path)
-          .to route_to(
-            controller: 'content_items',
-            action: 'index'
-          )
-      end
+    it 'routes to #index' do
+      expect(get: content_items_path).to be_routable
+      expect(get: content_items_path)
+        .to route_to(
+          controller: 'content_items',
+          action: 'index'
+        )
     end
 
     it 'routes to #show' do
