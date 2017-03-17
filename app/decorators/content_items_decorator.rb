84961e9fb73f79d70f67aa3e97163839c0eac92d
Kelvin Gan <dev@kelvingan.co.uk>
2017-03-17 15:40:46 +0000

    Show title based on multiple filters
    Previously we only showed the title of the content items listing page to
    be that of the organisation if we were filtering by that criteria. But
    now we can filter by Taxonomy as well and it is any combination of the
    two. This should really be reflected to the user otherwise there is no
    indication other than in the URL.

--- app/decorators/content_items_decorator.rb
+++ app/decorators/content_items_decorator.rb
@@ -2,11 +2,24 @@ class ContentItemsDecorator < Draper::CollectionDecorator
   delegate :current_page, :total_pages, :limit_value, :entry_name, :total_count, :offset_value, :last_page?
 
   def header
-    slug = helpers.params[:organisation_slug]
-    if slug
+    if slug.present? && taxonomy.present?
+      "#{Organisation.find_by(slug: slug).title} + #{taxonomy}"
+    elsif slug.present?
       Organisation.find_by(slug: slug).title
+    elsif taxonomy.present?
+      taxonomy
     else
-      'GOV.UK'
+      "GOV.UK"
     end
+  end
+
+private
+
+  def slug
+    helpers.params[:organisation_slug]
+  end
+
+  def taxonomy
+    helpers.params[:taxonomy]
   end
 end
--- spec/decorators/content_items_decorator_spec.rb
+++ spec/decorators/content_items_decorator_spec.rb
@@ -3,25 +3,46 @@ require "rails_helper"
 RSpec.describe ContentItemsDecorator, type: :decorator do
   include Draper::ViewHelpers
 
-  context 'without organisation' do
-    it 'renders the default page title' do
+  let(:organisation) { create(:organisation, title: "Organisation title", slug: "the-slug") }
+  let(:taxonomy) { create(:taxonomy, title: "taxonomy a") }
+
+  context "without filter params" do
+    it "renders the default page title" do
       allow(helpers).to receive(:params).and_return({})
       content_items = [build(:content_item)]
       subject = ContentItemsDecorator.new(content_items)
 
-      expect(subject.header).to eq('GOV.UK')
+      expect(subject.header).to eq("GOV.UK")
     end
   end
 
-  context 'with organisation filter' do
-    let(:organisation) { create(:organisation, title: 'Organisation title', slug: 'the-slug') }
-
-    it 'renders the organisation title as page title' do
-      allow(helpers).to receive(:params).and_return(organisation_slug: 'the-slug')
+  context "with organisation filter" do
+    it "renders the organisation title as page title" do
+      allow(helpers).to receive(:params).and_return(organisation_slug: "the-slug")
       content_items = [build(:content_item, organisations: [organisation])]
       subject = ContentItemsDecorator.new(content_items)
 
-      expect(subject.header).to eq('Organisation title')
+      expect(subject.header).to eq("Organisation title")
+    end
+  end
+
+  context "with taxonomy filter" do
+    it "renders the taxonomy title as page title" do
+      allow(helpers).to receive(:params).and_return(taxonomy: "taxonomy a")
+      content_items = [build(:content_item, taxonomies: [taxonomy])]
+      subject = ContentItemsDecorator.new(content_items)
+
+      expect(subject.header).to eq("taxonomy a")
+    end
+  end
+
+  context "with both organisation and taxonomy filters" do
+    it "renders the organisation and taxonomy titles seperated by a '+'" do
+      allow(helpers).to receive(:params).and_return(organisation_slug: "the-slug", taxonomy: "taxonomy a")
+      content_items = [build(:content_item, organisations: [organisation], taxonomies: [taxonomy])]
+      subject = ContentItemsDecorator.new(content_items)
+
+      expect(subject.header).to eq("Organisation title + taxonomy a")
     end
   end
 end
