f7f9e59fe8187247630381071f1f5291dd410447
Pablo Manrubia <pmanrubia@gmail.com>
2017-03-19 12:04:45 +0000

    DRY-UP requests/groups_spec.rb

--- spec/requests/groups_spec.rb
+++ spec/requests/groups_spec.rb
@@ -1,61 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe "API::Groups", type: :request do
-  describe "GET /groups/{id}" do
-    it "returns JSON with the group " do
-      group = create :group, name: "a-name", slug: "the-slug"
-      get group_path(group), format: :json
+  let(:headers) { { "ACCEPT" => "application/json" } }
 
+  describe "GET /groups/{id}" do
+    let!(:group) { create :group, name: "a-name", slug: "the-slug" }
+
+    before do
+      get "/groups/#{group.id}", params: {}, headers: headers
+    end
+
+    it "returns JSON with the group " do
       json = JSON.parse(response.body).deep_symbolize_keys
-      expect(json).to include(
-        name: "a-name",
-        slug: "the-slug"
-      )
+
+      expect(json).to include(name: "a-name", slug: "the-slug")
     end
   end
 
   describe "POST /groups" do
     context "with valid params" do
-      let(:valid_params) { { group: { name: "a-name", slug: "the-slug", group_type: "the-group-type" } } }
+      let(:params) { { group: attributes_for(:group) } }
 
       it "creates the group" do
         expect {
-          post groups_path params: valid_params, format: :json
+          post "/groups", params: params, headers: headers
         }.to change(Group, :count).by(1)
       end
 
-      it "returns status `:created` status code" do
-        post groups_path params: valid_params, format: :json
+      it "returns status code :created" do
+        post "/groups", params: params, headers: headers
 
         expect(response).to have_http_status(201)
       end
 
       it "returns a JSON with the group details" do
-        post groups_path params: valid_params, format: :json
+        params[:group][:name] = "a-name"
+        params[:group][:slug] = "the-slug"
+        post "/groups", params: params, headers: headers
 
         json = JSON.parse(response.body).deep_symbolize_keys
         expect(json).to include(
           name: "a-name",
-          slug: "the-slug",
-          group_type: "the-group-type"
+          slug: "the-slug"
         )
       end
 
-      context "when a list a content IDs is provided" do
-        it "add the Content Items to the group" do
-          valid_params[:group][:content_item_ids] = %w(content_id_1 content_id_2)
+      context "when a list of content IDs is provided" do
+        it "adds the Content IDs to the group" do
+          params[:group][:content_item_ids] = %w(content_id_1 content_id_2)
 
-          post groups_path params: valid_params, format: :json
+          post "/groups", params: params, headers: headers
           expect(Group.first.content_item_ids).to eq(%w(content_id_1 content_id_2))
         end
       end
 
       context "when a parent_group is provided" do
-        it "creates an association with the parent" do
-          parent = create(:group, slug: "parent-slug")
-          valid_params[:group].merge!(parent_group_slug: "parent-slug", slug: "child-slug")
+        let!(:parent) { create(:group, slug: "parent-slug") }
 
-          post groups_path params: valid_params, format: :json
+        it "creates an association with the parent" do
+          params[:group].merge!(parent_group_slug: "parent-slug", slug: "child-slug")
+
+          post "/groups", params: params, headers: headers
 
           child = Group.find_by(slug: "child-slug")
           expect(child.parent).to eq(parent)
@@ -66,15 +71,15 @@ RSpec.describe "API::Groups", type: :request do
     context "with invalid params" do
       let(:invalid_params) { { group: { name: "a-name" } } }
 
-      it "returns a `:unprocessable_entity` status code" do
-        post groups_path params: invalid_params, format: :json
+      before do
+        post "/groups", params: invalid_params, headers: headers
+      end
 
+      it "returns a `:unprocessable_entity` status code" do
         expect(response).to have_http_status(422)
       end
 
       it "returns JSON including the errror details" do
-        post groups_path params: invalid_params, format: :json
-
         json = JSON.parse(response.body).deep_symbolize_keys
         expect(json).to include(group_type: ["can't be blank"])
       end
