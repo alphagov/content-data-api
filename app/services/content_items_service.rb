benhazell/content-performance-manager

Use publishing api for content items
Replace calls to content_store and search_api.
Instead content is retrieved by document_type from the publishing-api.
The document types are configured in a .yml file for now.
This will change once there is an endpoint to retrieve a live list.
Org and Taxon associations use the pub-api's expanded_links endpoint.
Calls to get/import content by organisation have been removed.

app/services/content_items_service.rb
@@ -1,23 +1,27 @@
 class ContentItemsService
-  def find_each(organisation_slug)
+  attr_accessor :publishing_api
+
+  def initialize
+    @publishing_api = Clients::PublishingAPI.new
+  end
+
+  def find_each(document_type)
     raise 'missing block!' unless block_given?
 
-    query = { filter_organisations: organisation_slug }
-    fields = %w(link)
-
-    Clients::SearchAPI.find_each(query: query, fields: fields) do |response|
-      base_path = response.fetch(:link)
-      content_item = Clients::ContentStore.find(base_path, attribute_names)
-      if content_item
-        content_item[:taxons] = TaxonomyParser.parse(content_item)
-        yield content_item
-      end
+    publishing_api.find_each(query_fields, build_query_options(document_type)) do |content_item|
+      content_item[:taxons] = content_item[:expanded_links][:taxons] || []
+      content_item[:organisations] = content_item[:expanded_links][:organisations] || []
+      yield content_item
     end
   end
 
 private
 
-  def attribute_names
-    @names ||= %i(content_id description title public_updated_at document_type base_path details links)
+  def build_query_options(document_type)
+    { document_type: document_type, expand_links: true }
+  end
+
+  def query_fields
+    @fields ||= %i(content_id description title public_updated_at document_type base_path details)
   end
 end

spec/services/content_items_service_spec.rb
@@ -2,39 +2,31 @@ require 'rails_helper'
 
 RSpec.describe ContentItemsService do
   describe '#find_each' do
-    before do
-      allow(Clients::ContentStore).to receive(:find)
-      allow(Clients::SearchAPI).to receive(:find_each)
+    it 'queries the publishing API for all content items of a given type' do
+      subject.publishing_api = double
+      expected_field_params = %i(content_id description title public_updated_at document_type base_path details)
+      expected_query_params = { document_type: 'a_document_type', expand_links: true }
+
+      expect(subject.publishing_api).to receive(:find_each).with(expected_field_params, expected_query_params)
+
+      subject.find_each('a_document_type') {}
     end
 
-    it 'queries the search API, filtering by organisation' do
-      expected_params = {
-        query: { filter_organisations: 'organisation-slug' },
-        fields: %w(link)
+    it 'yields the response with taxons and organisations' do
+      results = []
+      subject.publishing_api = double
+      response = { expanded_links: {
+          taxons: [:a],
+          organisations: [:b]
+        }
       }
-      expect(Clients::SearchAPI).to receive(:find_each).with(expected_params)
 
-      subject.find_each('organisation-slug') {}
+      allow(subject.publishing_api).to receive(:find_each).and_yield(response)
+
+      subject.find_each('a_document_type') { |value| results << value }
+
+      expect(results.first).to include(taxons: [:a], organisations: [:b])
     end
-
-    it 'yields the response' do
-      result = []
-      allow(Clients::SearchAPI).to receive(:find_each).and_yield(link: :link1)
-      allow(Clients::ContentStore).to receive(:find).and_return({})
-      subject.find_each('organisation-slug') { |value| result << value }
-
-      expect(result).to match_array([{ taxons: [] }])
-    end
-
-    it "does not yield nil responses from the content store" do
-      result = []
-      allow(Clients::SearchAPI).to receive(:find_each).and_yield(link: :link1)
-      allow(Clients::ContentStore).to receive(:find).and_return(nil)
-      subject.find_each('organisation-slug') { |value| result << value }
-
-      expect(result).to match_array([])
-    end
-
 
     it 'raises an exception if no block is passed' do
       expect { subject.find_each('organisation-slug') }.to raise_exception('missing block!')

alphagov/content-data-api

app/services/content_items_service.rb
@@ -1,23 +1,27 @@
 class ContentItemsService
-  def find_each(organisation_slug)
+  attr_accessor :publishing_api
+
+  def initialize
+    @publishing_api = Clients::PublishingAPI.new
+  end
+
+  def find_each(document_type)
     raise 'missing block!' unless block_given?
 
-    query = { filter_organisations: organisation_slug }
-    fields = %w(link)
-
-    Clients::SearchAPI.find_each(query: query, fields: fields) do |response|
-      base_path = response.fetch(:link)
-      content_item = Clients::ContentStore.find(base_path, attribute_names)
-      if content_item
-        content_item[:taxons] = TaxonomyParser.parse(content_item)
-        yield content_item
-      end
+    publishing_api.find_each(query_fields, build_query_options(document_type)) do |content_item|
+      content_item[:taxons] = content_item[:links][:taxons] || []
+      content_item[:organisations] = content_item[:links][:organisations] || []
+      yield content_item
     end
   end
 
 private
 
-  def attribute_names
-    @names ||= %i(content_id description title public_updated_at document_type base_path details links)
+  def build_query_options(document_type)
+    { document_type: document_type, links: true }
+  end
+
+  def query_fields
+    @fields ||= %i(content_id description title public_updated_at document_type base_path details)
   end
 end

spec/services/content_items_service_spec.rb
@@ -2,39 +2,31 @@ require 'rails_helper'
 
 RSpec.describe ContentItemsService do
   describe '#find_each' do
-    before do
-      allow(Clients::ContentStore).to receive(:find)
-      allow(Clients::SearchAPI).to receive(:find_each)
+    it 'queries the publishing API for all content items of a given type' do
+      subject.publishing_api = double
+      expected_field_params = %i(content_id description title public_updated_at document_type base_path details)
+      expected_query_params = { document_type: 'a_document_type', links: true }
+
+      expect(subject.publishing_api).to receive(:find_each).with(expected_field_params, expected_query_params)
+
+      subject.find_each('a_document_type') {}
     end
 
-    it 'queries the search API, filtering by organisation' do
-      expected_params = {
-        query: { filter_organisations: 'organisation-slug' },
-        fields: %w(link)
+    it 'yields the response with taxons and organisations' do
+      results = []
+      subject.publishing_api = double
+      response = { links: {
+          taxons: [:a],
+          organisations: [:b]
+        }
       }
-      expect(Clients::SearchAPI).to receive(:find_each).with(expected_params)
 
-      subject.find_each('organisation-slug') {}
+      allow(subject.publishing_api).to receive(:find_each).and_yield(response)
+
+      subject.find_each('a_document_type') { |value| results << value }
+
+      expect(results.first).to include(taxons: [:a], organisations: [:b])
     end
-
-    it 'yields the response' do
-      result = []
-      allow(Clients::SearchAPI).to receive(:find_each).and_yield(link: :link1)
-      allow(Clients::ContentStore).to receive(:find).and_return({})
-      subject.find_each('organisation-slug') { |value| result << value }
-
-      expect(result).to match_array([{ taxons: [] }])
-    end
-
-    it "does not yield nil responses from the content store" do
-      result = []
-      allow(Clients::SearchAPI).to receive(:find_each).and_yield(link: :link1)
-      allow(Clients::ContentStore).to receive(:find).and_return(nil)
-      subject.find_each('organisation-slug') { |value| result << value }
-
-      expect(result).to match_array([])
-    end
-
 
     it 'raises an exception if no block is passed' do
       expect { subject.find_each('organisation-slug') }.to raise_exception('missing block!')