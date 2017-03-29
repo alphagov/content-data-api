80b9a5c357b4b12dd7cebbf38d7c6137ac081185
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-29 14:15:41 +0100

    Tidy up ContentItemsQuery tests

--- spec/queries/content_items_query_spec.rb
+++ spec/queries/content_items_query_spec.rb
@@ -2,22 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ContentItemsQuery, type: :query do
   subject { described_class }
-  let!(:content_items) do
-    [
-      create(:content_item, id: 1, public_updated_at: Time.parse("2016-12-09")),
-      create(:content_item, id: 2, public_updated_at: Time.parse("2015-12-09"))
-    ]
-  end
 
-  context "search title" do
-    let!(:content_items) do
-      [
-        create(:content_item, title: 'title 1'),
-        create(:content_item, title: 'title 2')
-      ]
-    end
-
+  context "searching by title" do
     it "returns content_items case insensitively" do
+      create(:content_item, title: 'title 1')
+      create(:content_item, title: 'title 2')
+
       results = subject.build(query: 'TITLE 1')
 
       expect(results.count).to eq(1)
@@ -25,69 +15,65 @@ RSpec.describe ContentItemsQuery, type: :query do
     end
   end
 
-  context "sorts with parameters" do
-    context "in ascending order" do
-      it "returns content_items" do
-        results = subject.build(sort: :public_updated_at, order: :asc)
-
-        expect(results.pluck(:id)).to eq([2, 1])
-      end
+  context "sorting with parameters" do
+    before do
+      create(:content_item, id: 1, public_updated_at: Time.parse("2016-12-09"))
+      create(:content_item, id: 2, public_updated_at: Time.parse("2015-12-09"))
     end
 
-    context "in descending order" do
-      it "returns content_items" do
-        results = subject.build(sort: :public_updated_at, order: :desc)
+    it "returns content_items sorted on a column ascending" do
+      results = subject.build(sort: :public_updated_at, order: :asc)
 
-        expect(results.pluck(:id)).to eq([1, 2])
-      end
+      expect(results.pluck(:id)).to eq([2, 1])
+    end
+
+    it "returns content_items sorted on a column descending" do
+      results = subject.build(sort: :public_updated_at, order: :desc)
+
+      expect(results.pluck(:id)).to eq([1, 2])
     end
   end
 
-  describe "filtering" do
-    context "by organisation" do
-      let(:organisation) { create(:organisation, content_items: content_items) }
+  context "filtering by relationships" do
+    it "returns the content items belonging to the organisation" do
+      create(:content_item)
+      content_items = [create(:content_item)]
+      organisation = create(:organisation, content_items: content_items)
 
-      it "returns the content items belonging to the organisation" do
-        create(:content_item)
-        results = subject.build(organisation: organisation)
+      results = subject.build(organisation: organisation)
 
-        expect(results).to match_array(content_items)
-      end
+      expect(results).to match_array(content_items)
     end
 
-    context "by taxonomy" do
-      let(:taxonomy) { create(:taxonomy, content_items: content_items) }
+    it "returns the content items belonging to the taxonomy" do
+      create(:content_item)
+      content_items = [create(:content_item)]
+      taxonomy = create(:taxonomy, content_items: content_items)
 
-      it "returns the content items belonging to the taxonomy" do
-        create(:content_item)
-        results = subject.build(taxonomy: taxonomy)
+      results = subject.build(taxonomy: taxonomy)
 
-        expect(results).to match_array(content_items)
-      end
+      expect(results).to match_array(content_items)
     end
 
-    context "by organisation and taxonomy" do
-      let(:organisation) { create(:organisation, content_items: content_items) }
-      let(:taxonomy) { create(:taxonomy, content_items: content_items) }
+    it "returns the content items belonging to the organisation and taxonomy" do
+      create(:content_item)
+      content_items = [create(:content_item)]
+      taxonomy = create(:taxonomy, content_items: content_items)
+      organisation = create(:organisation, content_items: content_items)
 
-      it "returns the content items belonging to the organisation and taxonomy" do
-        create(:content_item)
-        results = subject.build(organisation: organisation, taxonomy: taxonomy)
+      results = subject.build(organisation: organisation, taxonomy: taxonomy)
 
-        expect(results).to match_array(content_items)
-      end
+      expect(results).to match_array(content_items)
     end
 
-    context "by organisation and taxonomy and search term" do
-      let(:content_item) { create(:content_item, title: "title 1") }
-      let(:organisation) { create(:organisation, content_items: [content_item]) }
-      let(:taxonomy) { create(:taxonomy, content_items: [content_item]) }
+    it "returns the content items belonging to the organisation and taxonomy where the title is like the query" do
+      content_items = [create(:content_item, title: "title 1")]
+      taxonomy = create(:taxonomy, content_items: content_items)
+      organisation = create(:organisation, content_items: content_items)
 
-      it "returns the content items belonging to the organisation and taxonomy" do
-        results = subject.build(organisation: organisation, taxonomy: taxonomy, query: "title 1")
+      results = subject.build(organisation: organisation, taxonomy: taxonomy, query: "title 1")
 
-        expect(results).to match_array([content_item])
-      end
+      expect(results).to match_array(content_items)
     end
   end
 end
