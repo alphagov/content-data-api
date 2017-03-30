76d002d791bab13cba124961a9ecc27387d8276c
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-30 15:43:47 +0100

    Add zero page views metric

---
+++ app/misc/metrics/zero_page_views.rb
@@ -0,0 +1,13 @@
+module Metrics
+  class ZeroPageViews
+    attr_accessor :content_items
+
+    def initialize(content_items)
+      @content_items = content_items
+    end
+
+    def run
+      { zero_page_views: { value: content_items.where("unique_page_views = 0").count } }
+    end
+  end
+end
