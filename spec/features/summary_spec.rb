96c3c6c0ca921df1d4090669f28fd95df5b973e5
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-31 15:13:08 +0100

    Add pages with pdfs metric to summary

--- spec/features/summary_spec.rb
+++ spec/features/summary_spec.rb
@@ -26,4 +26,13 @@ RSpec.feature "Summary area", type: :feature do
 
     expect(page).to have_selector('.summary-item-value', text: 1)
   end
+
+  scenario "user can see the total number of pages not updated in the last 6 months" do
+    create :content_item, number_of_pdfs: 1
+    create :content_item, number_of_pdfs: 0
+
+    visit 'content_items'
+
+    expect(page).to have_selector('.summary-item-value', text: "50.0%")
+  end
 end
