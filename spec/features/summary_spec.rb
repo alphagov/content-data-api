756f0c8c12428dc0d61245330b4947e9c33b5f8b
Lee Briggs <lee.briggs@digital.cabinet-office.gov.uk>
2017-03-31 10:31:47 +0100

    Add zero view metric into summary

--- spec/features/summary_spec.rb
+++ spec/features/summary_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature "Summary area", type: :feature do
-  background do
-    @content_items = create_list :content_item, 3
-  end
-
   scenario "user can see the total number of pages" do
+    create_list :content_item, 3
+
     visit 'content_items'
 
     expect(page).to have_selector('.summary-item-value', text: 3)
+  end
+
+  scenario "user can see the total number of pages with zero views" do
+    create :content_item, unique_page_views: 1
+    create :content_item, unique_page_views: 0
+
+    visit 'content_items'
+
+    expect(page).to have_selector('.summary-item-value', text: 1)
   end
 end
--- spec/views/content_items/_summary.html.erb_spec.rb
+++ spec/views/content_items/_summary.html.erb_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe 'content_items/_summary.html.erb', type: :view do
+  let(:metrics) {
+    {
+      total_pages: { value: 0 },
+      zero_page_views: { value: 0 }
+    }
+  }
+
   it "renders the total pages metric" do
-    metrics = { total_pages: { value: 123 } }
+    metrics[:total_pages][:value] = 123
     assign(:metrics, metrics)
 
     render
 
     expect(rendered).to have_selector(".summary-item-label", text: "Live pages")
+    expect(rendered).to have_selector(".summary-item-value", text: "123")
+  end
+
+  it "renders the zero page views metric" do
+    metrics[:zero_page_views][:value] = 123
+    assign(:metrics, metrics)
+
+    render
+
+    expect(rendered).to have_selector(".summary-item-label", text: "Pages with zero views")
     expect(rendered).to have_selector(".summary-item-value", text: "123")
   end
 end
