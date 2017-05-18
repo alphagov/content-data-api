a3285313641cf4bf49c6edeabfa216a8c9a75685
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-05-18 13:05:53 +0100

    Create Link entries when importing content

--- app/jobs/import_content_item_job.rb
+++ app/jobs/import_content_item_job.rb
@@ -12,9 +12,32 @@ class ImportContentItemJob < ApplicationJob
     content_id = args[0]
 
     attributes = content_items_service.fetch(content_id)
-    attributes = attributes.merge(content_items_service.links(content_id))
+    links = content_items_service.links(content_id)
+    attributes = attributes.merge(links)
     attributes = attributes.merge(metric_builder.run_all(attributes))
 
-    ContentItem.create_or_update!(attributes)
+    # Use a single transaction, so that the content change and links
+    # change is atomic with respect to other transactions.
+    ActiveRecord::Base.transaction do
+      ContentItem.create_or_update!(attributes)
+      create_links(content_id, links)
+    end
+  end
+
+private
+
+  def create_links(source_content_id, links)
+    new_links = links.flat_map do |type, content_ids|
+      content_ids.map do |content_id|
+        {
+          source_content_id: source_content_id,
+          link_type: type,
+          target_content_id: content_id,
+        }
+      end
+    end
+
+    Link.where(source_content_id: source_content_id).delete_all
+    Link.create(new_links)
   end
 end
--- spec/jobs/import_content_item_job_spec.rb
+++ spec/jobs/import_content_item_job_spec.rb
@@ -33,6 +33,26 @@ RSpec.describe ImportContentItemJob, type: :job do
     expect(content_item.number_of_pdfs).to eq(10)
   end
 
+  it "imports links" do
+    expect { subject.perform(content_id) }
+      .to change(Link, :count).by(4)
+
+    content_item = ContentItem.last
+    content_item_links = Link.where(source_content_id: content_item.content_id)
+
+    expect(
+      content_item_links.where(
+        link_type: "organisations"
+      ).pluck(:target_content_id)
+    ).to contain_exactly(*%w(org-1 org-2))
+
+    expect(
+      content_item_links.where(
+        link_type: "taxons"
+      ).pluck(:target_content_id)
+    ).to contain_exactly(*%w(taxon-1 taxon-2))
+  end
+
   context "when the content item already exists" do
     before do
       FactoryGirl.create(
@@ -51,6 +71,17 @@ RSpec.describe ImportContentItemJob, type: :job do
         .and change { ContentItem.last.organisations.to_a }.from([org1]).to([org1, org2])
         .and change { ContentItem.last.taxonomies.to_a }.from([taxon2]).to([taxon1, taxon2])
         .and change { ContentItem.last.number_of_pdfs }.from(5).to(10)
+    end
+  end
+
+  context "when the links already exists, and the Publishing API is unchanged" do
+    before do
+      subject.perform(content_id)
+    end
+
+    it "doesn't create any additional links" do
+      expect { subject.perform(content_id) }
+      .to_not change(Link, :count)
     end
   end
 end
