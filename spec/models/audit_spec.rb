6ba6472829def03baf19df5ab2aa341f20120b3b
Chris Patuzzo <chris@patuzzo.co.uk>
2017-05-19 14:10:28 +0100

    Test that implicit building of responses works
    When assigning `questions` to the `audit`, Rails
    notices this is a `has_many :through` association
    and automatically builds responses for us.
    
    For this to work, we need to tell Rails the name
    of the method to set the audit on responses, which
    we do with the `inverse_of: :audit` option.

--- spec/models/audit_spec.rb
+++ spec/models/audit_spec.rb
@@ -1,7 +1,22 @@
 RSpec.describe Audit do
-  describe "validations" do
-    subject { FactoryGirl.build(:audit) }
+  subject { FactoryGirl.build(:audit) }
 
+  describe "associations" do
+    it "builds responses when assigning questions" do
+      q1 = FactoryGirl.build(:boolean_question)
+      q2 = FactoryGirl.build(:boolean_question)
+
+      subject.questions = [q1, q2]
+
+      expect(subject.responses.size).to eq(2)
+      first = subject.responses.first
+
+      expect(first.audit).to eq(subject)
+      expect(first.question).to eq(q1)
+    end
+  end
+
+  describe "validations" do
     it "has a valid factory" do
       expect(subject).to be_valid
     end
