6f047f6cb8b5065fd6b2c73c12fa76d633a80486
Pablo Manrubia <pmanrubia@gmail.com>
2017-05-24 18:07:02 +0100

    Show the unique page views for a Content Item (1 month)

--- spec/features/audit/list_content_items_to_audit_spec.rb
+++ spec/features/audit/list_content_items_to_audit_spec.rb
@@ -1,10 +1,11 @@
 RSpec.feature "List Content Items to Audit", type: :feature do
-  let!(:user) { FactoryGirl.create(:user) }
+  let!(:user) {FactoryGirl.create(:user)}
 
   before do
     FactoryGirl.create(:content_item, title: "All about flooding.")
-    FactoryGirl.create(:content_item, title: "All about gardening.", one_month_page_views: 1234)
   end
+
+  let!(:content_item) { FactoryGirl.create(:content_item, title: "All about gardening.", one_month_page_views: 1234) }
 
   scenario "List Content Items to Audit" do
     visit audits_path
@@ -12,7 +13,6 @@ RSpec.feature "List Content Items to Audit", type: :feature do
     expect(page).to have_content("All about flooding.")
     expect(page).to have_content("All about gardening.")
     expect(page).to have_content("1234")
-
-    expect(page).to have_link("All about floading.", href: "https://gov.uk/flooding")
+    expect(page).to have_link("All about gardening.", href: "/content_items/#{content_item.id}/audit")
   end
 end
