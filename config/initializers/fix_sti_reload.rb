5c2e442a43c045dc02d99b02acb705c7f1d2667f
Chris Patuzzo <chris@patuzzo.co.uk>
2017-05-26 10:15:41 +0000

    Fix a problem with the load path and question subclasses

--- config/initializers/fix_sti_reload.rb
+++
@@ -1,6 +0,0 @@
-# This fixes a problem with reloading STI models in development mode.
-# See: https://github.com/rails/rails/issues/8699
-def fix_sti_reload_in_development(file)
-  return unless Rails.env.development?
-  Dir[file.gsub(".rb", "/*.rb")].each { |f| require_dependency f }
-end
