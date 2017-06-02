09b20c4b4cb539b0ed4f8f7d87f51d7509901bda
Chris Patuzzo <chris@patuzzo.co.uk>
2017-06-02 11:30:50 +0000

    Test the page resets to 1 after filtering

--- spec/features/audit/filter_spec.rb
+++ spec/features/audit/filter_spec.rb
@@ -1,6 +1,4 @@
 RSpec.feature "Filter Content Items to Audit", type: :feature do
-  let!(:user) { create(:user) }
-
   before do
     create(:audit, content_item: create(:content_item, title: "Audited content item"))
     create(:content_item, title: "Non-audited content item")
@@ -33,5 +31,17 @@ RSpec.feature "Filter Content Items to Audit", type: :feature do
     expect(page).to_not have_content("Audited content item")
     expect(page).to have_content("Non-audited content item")
     expect(page).to have_select("audit_status", selected: "Non Audited")
+  end
+
+  scenario "Reseting page to 1 after filtering" do
+    FactoryGirl.create_list(:content_item, 25)
+
+    visit audits_path
+    within(".pagination") { click_on "2" }
+
+    select "Non Audited", from: "audit_status"
+    click_on "Filter"
+
+    expect(page).to have_css(".pagination .active", text: "1")
   end
 end
