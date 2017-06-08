e134fab5111e2a6bf2831108dda7f8fd81bb8c52
Tom Gladhill <whoojemaflip@gmail.com>
2017-06-08 12:58:50 +0100

    Keep track of success/errors on import

--- app/models/importers/todos_for_taxonomy_project.rb
+++ app/models/importers/todos_for_taxonomy_project.rb
@@ -1,8 +1,12 @@
 module Importers
   class TodosForTaxonomyProject
+    attr_reader :errors, :completed
+
     def initialize(group_name, csv_parser)
       @name = group_name
       @csv_parser = csv_parser
+      @completed = []
+      @errors = []
     end
 
     def run
@@ -14,12 +18,23 @@ module Importers
               taxonomy_project: project,
               content_item: content_item
             )
+            track_success row['content_id']
+          else
+            track_failure row['content_id']
           end
         end
       end
     end
 
   private
+
+    def track_failure(id)
+      @errors << id
+    end
+
+    def track_success(id)
+      @completed << id
+    end
 
     def project
       @_project ||= TaxonomyProject.create(name: @name)
