56fd24f74f164c6c587cca9ab69d29c4b3d5a933
Chris Patuzzo <chris@patuzzo.co.uk>
2017-06-13 12:31:55 +0000

    Add SubthemeFilter
    This allows us to filter content items by a predetermined list of
    filter rules - a Subtheme. We re-use LinkFilter to filter for the
    Subtheme as it has the behaviour we need.

---
+++ spec/domain/search/subtheme_filter_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe Search::SubthemeFilter do
+  subject { described_class.new(subtheme: subtheme) }
+
+  let(:subtheme) { FactoryGirl.create(:subtheme) }
+  let(:result) { subject.apply(ContentItem.all).map(&:title) }
+
+  def node(title)
+    FactoryGirl.create(:content_item, title: title)
+  end
+
+  def edge(source, target, type)
+    FactoryGirl.create(:link, source: source, target: target, link_type: type)
+  end
+
+  def rule(type, target)
+    FactoryGirl.create(
+      :inventory_rule,
+      subtheme: subtheme,
+      link_type: type,
+      target_content_id: target.content_id,
+    )
+  end
+
+  let!(:a) { node("a") }
+  let!(:b) { node("b") }
+  let!(:c) { node("c") }
+
+  let!(:hmrc) { node("HMRC") }
+  let!(:raib) { node("RAIB") }
+
+  let!(:vat) { node("VAT") }
+  let!(:rail) { node("Railways") }
+
+  before do
+    edge(a, hmrc, "organisations")
+    edge(b, hmrc, "organisations")
+    edge(c, raib, "organisations")
+
+    edge(a, vat, "policies")
+    edge(b, rail, "policies")
+    edge(c, rail, "policies")
+  end
+
+  it "filters content items for a single inventory rule" do
+    rule("organisations", hmrc)
+    expect(result).to match_array %w(a b)
+  end
+
+  it "filters content items for multiple rules of the same type" do
+    rule("organisations", hmrc)
+    rule("organisations", raib)
+
+    expect(result).to match_array %w(a b c)
+  end
+
+  it "filters content items for multiple, single rules of different types" do
+    rule("organisations", hmrc)
+    rule("policies", rail)
+
+    expect(result).to match_array %w(a b c)
+  end
+
+  it "doesn't filter items where the filter does not match a link type" do
+    rule("related", hmrc)
+
+    expect(result).to be_empty
+  end
+end
