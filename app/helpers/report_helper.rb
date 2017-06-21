78c1ab6bed904856c54aa09b5b64a50e33b30b4f
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-06-21 13:41:49 +0100

    Add report helpers for passing/not passing

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -15,6 +15,22 @@ module ReportHelper
     percentage(not_audited_count, out_of: @content_items.total_count)
   end
 
+  def passing_count
+    @search.dimension(:passing).content_items.total_count
+  end
+
+  def not_passing_count
+    @search.dimension(:not_passing).content_items.total_count
+  end
+
+  def passing_percentage
+    percentage(passing_count, out_of: audited_count)
+  end
+
+  def not_passing_percentage
+    percentage(not_passing_count, out_of: audited_count)
+  end
+
 private
 
   def percentage(number, out_of:)
