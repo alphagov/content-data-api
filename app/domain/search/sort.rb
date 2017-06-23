e8947ea25cc66d4f2860d78d615fb3e1a9552b4a
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-06-23 15:35:04 +0100

    Add a label to Sort
    In preparation for displaying a list of sort options.

--- app/domain/search/sort.rb
+++ app/domain/search/sort.rb
@@ -4,14 +4,16 @@ class Search
 
     def self.all
       [
-        Sort.new(:page_views_desc, "six_months_page_views", :desc),
+        Sort.new(:page_views_desc, "Six months page views (Decending)",
+                 "six_months_page_views", :desc),
       ]
     end
 
-    attr_reader :identifier
+    attr_reader :identifier, :label
 
-    def initialize(identifier, field, order)
+    def initialize(identifier, label, field, order)
       @identifier = identifier
+      @label = label
       @field = field
       @order = order
     end
