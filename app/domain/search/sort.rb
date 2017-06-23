423dc8f3c09c3b83c33047f0f8b4020929ceb9ee
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2017-06-23 16:13:54 +0100

    Add more audit sort orders

--- app/domain/search/sort.rb
+++ app/domain/search/sort.rb
@@ -6,6 +6,16 @@ class Search
       [
         Sort.new(:page_views_desc, "Six months page views (Decending)",
                  "six_months_page_views", :desc),
+        Sort.new(:page_views_asc, "Six months page views (Ascending)",
+                 "six_months_page_views", :asc),
+        Sort.new(:title_desc, "Title (Decending)",
+                 "title", :desc),
+        Sort.new(:title_asc, "Title (Ascending)",
+                 "title", :asc),
+        Sort.new(:public_updated_at_asc, "Last public update (Oldest first)",
+                 "public_updated_at", :asc, nulls: :first),
+        Sort.new(:public_updated_at_desc, "Last public update (Most recent first)",
+                 "public_updated_at", :desc, nulls: :last),
       ]
     end
 
