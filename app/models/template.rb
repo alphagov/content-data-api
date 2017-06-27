67f1e578972f6eefb2b09dc4b1861e2f37ec9cff
Kelvin Gan <dev@kelvingan.co.uk>
2017-06-27 18:03:26 +0100

    Ensure ordering of questions
    Before this change, if we made a change to existing questions the order
    of questions returned in an odd way. We are setting to ID for now so that
    it is at least deterministic.

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,7 +1,7 @@
 class Template
   def questions
     fail "You need to run rake db:seed to create the questions" if Question.none?
-    Question.all.to_a
+    Question.all.order(:id).to_a
   end
 
   def mandatory?(question)
