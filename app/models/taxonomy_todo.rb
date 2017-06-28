8a866c3021c5c702de2b0db8c730ff3a3ea81e1d
Tijmen Brommet <tijmen@gmail.com>
2017-06-28 11:28:04 +0100

    Add progress stats to project page
    This adds progress stats to the taxonomy project page. Users can use
    this to see how much work they still have to do.
    
    https://trello.com/c/FfihRuUx

--- app/models/taxonomy_todo.rb
+++ app/models/taxonomy_todo.rb
@@ -5,6 +5,7 @@ class TaxonomyTodo < ApplicationRecord
   belongs_to :user, primary_key: :uid, foreign_key: :completed_by, optional: true
 
   scope :still_todo, -> { where(completed_at: nil) }
+  scope :done, -> { where('completed_at IS NOT NULL') }
 
   def completed?
     completed_at && completed_by
