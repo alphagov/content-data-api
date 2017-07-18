a3b632d8b4f840381a4d37a72f6313d43a8e5270
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-18 17:31:01 +0100

    Remove Importer for all taxons
    There is no need to import all taxons as we are retrieving all
    the Links from PublishingAPI and we can easily infer the taxons
    by filtering by link type.

--- app/models/importers/all_inventory.rb
+++ app/models/importers/all_inventory.rb
@@ -1,7 +1,6 @@
 module Importers
   class AllInventory
     def run
-      Importers::AllTaxons.new.run
       Importers::AllContentItems.new.run
     end
   end
--- spec/lib/tasks/import_spec.rb
+++ spec/lib/tasks/import_spec.rb
@@ -13,17 +13,6 @@ RSpec.describe 'Import organisation rake task' do
     end
   end
 
-  describe 'import:all_taxons' do
-    before do
-      Rake::Task['import:all_taxons'].reenable
-    end
-
-    it 'runs the process to import all taxons' do
-      expect_any_instance_of(Importers::AllTaxons).to receive(:run)
-
-      Rake::Task['import:all_taxons'].invoke
-    end
-  end
 
   describe 'import:number_of_views_by_organisation' do
     before do
