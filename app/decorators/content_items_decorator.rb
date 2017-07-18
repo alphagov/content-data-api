6b7585197d36fe42370cf68f0176a27e55548221
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-18 17:59:04 +0100

    Render the filter parameters using Links
    We no longer have organisations and taxons; we need to render the
    filtering criteria on top of the list of content items.

--- app/decorators/content_items_decorator.rb
+++ app/decorators/content_items_decorator.rb
@@ -9,19 +9,4 @@ class ContentItemsDecorator < Draper::CollectionDecorator
     :last_page?,
     :next_item,
   )
-
-  def header(filters = {})
-    organisation = filters[:organisation]
-    taxon = filters[:taxon]
-
-    if organisation.present? && taxon.present?
-      "#{organisation.title} + #{taxon.title}"
-    elsif organisation.present?
-      organisation.title
-    elsif taxon.present?
-      taxon.title
-    else
-      "GOV.UK"
-    end
-  end
 end
---
+++ spec/features/performance/filter_header_spec.rb
@@ -0,0 +1,24 @@
+RSpec.feature "Header of Content Items List", type: :feature do
+  before do
+    FactoryGirl.create(:user)
+  end
+
+  context "When no filter is present" do
+    scenario "Renders GOV.UK" do
+      visit "/content_items"
+
+      expect(page).to have_selector("h1", text: "GOV.UK")
+    end
+  end
+
+  context "When a filter is present" do
+    let!(:hmrc) { create(:content_item, content_id: 1, title: "HMRC") }
+    let!(:taxon1) { create(:content_item, content_id: 2, title: "TAXON") }
+
+    scenario "Renders GOV.UK" do
+      visit "/content_items?organisations=1&taxons=2"
+
+      expect(page).to have_selector('h1', text: "HMRC + TAXON")
+    end
+  end
+end
