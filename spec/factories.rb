290e832fa9966c9c5b68ddeffbdf118852b8f3a7
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-18 17:48:18 +0100

    Remove unused factory: organisation_with_content_items

--- spec/factories.rb
+++ spec/factories.rb
@@ -75,14 +75,4 @@ FactoryGirl.define do
       end
     end
   end
-
-    factory :organisation_with_content_items do
-      transient do
-        content_items_count 1
-      end
-      after(:create) do |organisation, evaluator|
-        create_list(:content_item, evaluator.content_items_count, organisations: [organisation])
-      end
-    end
-  end
 end
