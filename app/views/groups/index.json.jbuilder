db4e3f40e2b1ac9ec172d10ec11764befc8b88f6
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-21 14:27:21 +0100

    Remove Groups endpoint
    We no longer expose an endpoint to manage groups, as we plan to provide
    a tool to perform operations on the inventory of ContentItems

--- app/views/groups/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.groups @groups do |group|
-  json.slug group.slug
-  json.total group.content_item_ids.length
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'content_items#index'
 
-  resources :groups, only: %w(show create index destroy), param: "slug"
-
   resources :content_items, only: %w(index show) do
     get :audit, to: "audits#show"
     post :audit, to: "audits#save"
