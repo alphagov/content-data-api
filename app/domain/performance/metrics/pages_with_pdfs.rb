3e959fa88c7ae43e7be464ed9f8f1037b9cf8e5e
Pablo Manrubia <pmanrubia@gmail.com>
2017-07-23 21:06:44 +0100

    Rename `content_items` attribute to `scope` in Metrics
    It is more explicit that we are working on an existing scope.

--- app/domain/performance/metrics/pages_with_pdfs.rb
+++ app/domain/performance/metrics/pages_with_pdfs.rb
@@ -1,16 +1,16 @@
 module Performance::Metrics
   class PagesWithPdfs
-    attr_accessor :content_items
+    attr_reader :scope
 
-    def initialize(content_items)
-      @content_items = content_items
+    def initialize(scope)
+      @scope = scope
     end
 
     def run
       percentage = 0
-      total = content_items.where("number_of_pdfs > ?", 0).count
+      total = scope.where("number_of_pdfs > ?", 0).count
 
-      percentage = (total.to_f / content_items.count.to_f) * 100 if total.positive?
+      percentage = (total.to_f / scope.count.to_f) * 100 if total.positive?
 
       {
         pages_with_pdfs: {
