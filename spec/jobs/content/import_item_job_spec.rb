ad7b03e8949b54430375d426921efbd0a8afc5e2
Andrew Garner <andrew@andrewgarner.com>
2017-08-11 09:14:28 +0100

    Rename jobs inline with content module

--- spec/jobs/content/import_item_job_spec.rb
+++ spec/jobs/content/import_item_job_spec.rb
@@ -1,5 +1,5 @@
 module Content
-  RSpec.describe ImportContentItemJob, type: :job do
+  RSpec.describe ImportItemJob, type: :job do
     it "calls the single content item importer" do
       expect(Importers::SingleContentItem).to receive(:run).with(content_id: "id-123", locale: "en")
       subject.perform(content_id: "id-123", locale: "en")
