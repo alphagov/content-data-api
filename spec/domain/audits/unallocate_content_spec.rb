cc4c35f608ef239b2e756cf41635e62373e388af
Pablo Manrubia <pmanrubia@gmail.com>
2017-08-15 07:11:01 +0100

    DRY-UP UnallocateContent

--- spec/domain/audits/unallocate_content_spec.rb
+++ spec/domain/audits/unallocate_content_spec.rb
@@ -1,28 +1,23 @@
 module Audits
   RSpec.describe UnallocateContent do
-    it "Unallocates content items" do
-      user = create :user
-      item1 = create :content_item, content_id: "content_id_1"
-      item2 = create :content_item, content_id: "content_id_2"
+    let(:user) { create :user }
 
+    let(:item1) { create :content_item, content_id: "content_id_1" }
+    let(:item2) { create :content_item, content_id: "content_id_2" }
+
+    before do
       create(:allocation, user: user, content_item: item1)
       create(:allocation, user: user, content_item: item2)
+    end
 
+    it "Unallocates content items" do
       UnallocateContent.call(content_ids: %w(content_id_1))
 
       expect(Allocation.count).to eq(1)
-      expect(Allocation.first.user).to eq(user)
-      expect(Allocation.first.content_item).to eq(item2)
+      expect(Allocation.first).to have_attributes(content_item: item2, user: user)
     end
 
     it "Returns a message with the number of unallocated items" do
-      user = create :user
-      item1 = create :content_item, content_id: "content_id_1"
-      item2 = create :content_item, content_id: "content_id_2"
-
-      create(:allocation, user: user, content_item: item1)
-      create(:allocation, user: user, content_item: item2)
-
       result = UnallocateContent.call(content_ids: %w(content_id_1 content_id_2))
 
       expect(result.message).to eq("2 items unallocated")
