4e6702c3ce8e89603893c66008a265851907d170
Kelvin Gan <dev@kelvingan.co.uk>
2017-08-21 17:45:58 +0100

    Address selectAllPages JavaScript feedback
    Feedback in:
    
    https://github.com/alphagov/content-performance-manager/pull/290
    
    that needs addressing but was added after the PR was merged. This commit
    fixes these issues.

--- app/assets/javascripts/select-all.js
+++ app/assets/javascripts/select-all.js
@@ -2,24 +2,24 @@
   "use strict";
 
   Modules.SelectAll = function () {
-    this.start = function (element) {
+    this.start = function ($element) {
       function addEventListener() {
-        var selectAll = element.find('#select_all');
-        var selectAllPages = element.find('#select_all_pages');
-        var selectAllPagesContainer = selectAllPages.closest('.if-js-hide');
+        var $selectAll = $element.find('#select_all');
+        var $selectAllPages = $element.find('#select_all_pages');
+        var $selectAllPagesContainer = $selectAllPages.closest('.if-js-hide');
 
-        if (selectAll != undefined) {
-          selectAll.on('change', function (e) {
-            var checked = selectAll.prop('checked');
-            var checkboxes = $('.select-content-item');
-            checkboxes.prop('checked', checked);
+        if ($selectAll.length) {
+          $selectAll.on('change', function (e) {
+            var $checked = $selectAll.prop('checked');
+            var $checkboxes = $('.select-content-item');
+            $checkboxes.prop('checked', $checked);
 
-            if (selectAllPages != undefined) {
-              selectAllPages.prop('checked', false);
+            if ($selectAllPages.length) {
+              $selectAllPages.prop('checked', false);
             }
 
-            if (selectAllPagesContainer != undefined) {
-              selectAllPagesContainer.toggleClass('if-js-hide', !checked);
+            if ($selectAllPagesContainer.length) {
+              $selectAllPagesContainer.toggleClass('if-js-hide', !$checked);
             }
           });
         }
