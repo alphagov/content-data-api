125e8b295c3b38d7ce1c80e4980b8a38fc883b21
Pablo Manrubia <pmanrubia@gmail.com>
2017-08-24 15:59:42 +0100

    Drop organisations model
    Organisations are no longer modelled independently. We rely on `Links`
    and `Content Items` to represent the relation between content items and
    organisations.

---
+++ db/migrate/20170824145426_drop_organisations.rb
@@ -0,0 +1,12 @@
+class DropOrganisations < ActiveRecord::Migration[5.1]
+  def change
+    drop_table :organisations do |t|
+      t.string "slug"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.string "title"
+      t.string "content_id"
+      t.index ["slug"], name: "index_organisations_on_slug", unique: true
+    end
+  end
+end
