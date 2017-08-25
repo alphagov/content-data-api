e3dc33e18ee6b2c2a546c0ffb98d47f54f99e7d0
Pablo Manrubia <pmanrubia@gmail.com>
2017-08-25 17:28:36 +0100

    Add Null object to represent no-policy filter
    It feels better to model an empty policy with a null object rather than
    with custom policies like `allocated_and_unallocated` or 
    `audited_and_not_audited`

--- app/domain/audits/policies/allocated_and_unallocated.rb
+++
@@ -1,9 +0,0 @@
-module Audits
-  module Policies
-    class AllocatedAndUnallocated
-      def self.call(scope, allocated_to: nil) # rubocop:disable Lint/UnusedMethodArgument
-        scope
-      end
-    end
-  end
-end
