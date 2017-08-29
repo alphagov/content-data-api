324f8641cfb5f3ee3b4df99c18123a43beb1c33e
Pablo Manrubia <pmanrubia@gmail.com>
2017-08-29 18:08:12 +0100

    DRY-Up feature test for sorting

--- spec/features/audit/lists/sorting_spec.rb
+++ spec/features/audit/lists/sorting_spec.rb
@@ -1,12 +1,12 @@
 RSpec.feature "Sort content items to audit", type: :feature do
   scenario "Default sorting by popularity" do
-    content_items[0].update!(six_months_page_views: 0)
-    content_items[1].update!(six_months_page_views: 1234)
+    create(:content_item, six_months_page_views: 0, title: "item1")
+    create(:content_item, six_months_page_views: 1234, title: "item2")
 
     visit audits_path
 
     rows = page.all('main tbody tr')
-    expect(rows[0].text).to match(content_items[1].title)
-    expect(rows[1].text).to match(content_items[0].title)
+    expect(rows[0].text).to match("item2")
+    expect(rows[1].text).to match("item1")
   end
 end
