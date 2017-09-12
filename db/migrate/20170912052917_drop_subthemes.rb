b8d66eb63d0cb5aeb070c2f066793128e8c1be4b
Pablo Manrubia <pmanrubia@gmail.com>
2017-09-12 14:22:25 +0100

    Uses %w notation to define an Array

--- db/migrate/20170912052917_drop_subthemes.rb
+++ db/migrate/20170912052917_drop_subthemes.rb
@@ -5,7 +5,7 @@ class DropSubthemes < ActiveRecord::Migration[5.1]
       t.string "name", null: false
       t.timestamps
 
-      t.index ["theme_id", "name"], name: "index_subthemes_on_theme_id_and_name", unique: true
+      t.index %w[theme_id name], name: "index_subthemes_on_theme_id_and_name", unique: true
       t.index ["theme_id"], name: "index_subthemes_on_theme_id"
     end
   end
--- db/migrate/20170912052927_drop_themes.rb
+++ db/migrate/20170912052927_drop_themes.rb
@@ -3,7 +3,7 @@ class DropThemes < ActiveRecord::Migration[5.1]
     drop_table :themes do |t|
       t.string "name", null: false
       t.timestamps
-        
+
       t.index ["name"], name: "index_themes_on_name", unique: true
     end
   end
