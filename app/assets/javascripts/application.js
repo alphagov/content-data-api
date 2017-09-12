398fcbcc06519516e03ca5fb0b6b8f64ea2a5c8d
Pablo Manrubia <pmanrubia@gmail.com>
2017-09-12 05:10:37 +0100

    Add JS namespaces support for Modules

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require bootstrap-tagsinput
 //= require cable
 //= require google-tag-manager
+//= require namespaces
 //= require selectize
 //= require accessible-autocomplete.min
 //= require organisation-autocomplete
