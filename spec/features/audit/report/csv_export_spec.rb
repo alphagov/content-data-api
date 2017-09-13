75a48d8bfdad9f421dbc7874dfc725771a00b72b
Kelvin Gan <gds@kelvingan.co.uk>
2017-09-13 23:31:49 +0100

    Remove query params from tab links
    We no longer want to preserve filter state when navigating between the
    different tabs, but instead fall back to the defaults set in the
    previous commit.

--- spec/features/audit/report/csv_export_spec.rb
+++ spec/features/audit/report/csv_export_spec.rb
@@ -67,26 +67,6 @@ RSpec.feature "Exporting a CSV from the report page" do
       expect(page).to have_content("Example 1,https://gov.uk/example1")
       expect(page).to have_no_content("Example 2,https://gov.uk/example2")
     end
-
-    scenario "Discard audit status filter when clicking from content view to report, and then exporting CSV" do
-      visit audits_path
-      select "Anyone", from: "allocated_to"
-
-      choose "Audited"
-
-      click_on "Apply filters"
-      expect(page).to have_content("Example 1")
-      expect(page).to have_no_content("Example 2")
-
-      click_link "Audit progress"
-
-      click_link "Export filtered audit to CSV"
-      expect(content_disposition).to include(
-        'filename="Transformation_audit_report_CSV_download.csv"',
-      )
-      expect(page).to have_content("Example 1")
-      expect(page).to have_content("Example 2")
-    end
   end
 
   context "Multiple pages of content items are in the database" do
--- spec/features/audit/report/tabs_spec.rb
+++ spec/features/audit/report/tabs_spec.rb
@@ -1,14 +1,21 @@
 RSpec.feature "Tabs" do
-  scenario "preserving filters across tabs" do
+  scenario "reset filters on each tab" do
     visit audits_path
-    choose "Audited"
+    expect(page).to have_select("allocated_to", selected: "Me")
 
+    choose "Audited"
+    select "Anyone", from: "allocated_to"
     click_on "Apply filters"
-    expect(page).to have_checked_field("audit_status_audited")
+    expect(page).to have_select("allocated_to", selected: "Anyone")
+
+    click_on "Assign content"
+    expect(page).to have_select("allocated_to", selected: "No one")
 
     click_link "Audit progress"
+    expect(page).to have_select("allocated_to", selected: "Me")
 
+    select "Anyone", from: "allocated_to"
     click_link "Audit content"
-    expect(page).to have_checked_field("audit_status_audited")
+    expect(page).to have_select("allocated_to", selected: "Me")
   end
 end
