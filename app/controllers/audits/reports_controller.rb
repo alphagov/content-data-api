0ff76c58d965a88963798e1053b7d15178ae30d8
Alec Gibson <alec.gibson@digital.cabinet-office.gov.uk>
2017-09-15 16:29:29 +0100

    Update default filter for "Audit Progress" tab
    When a user visits the "Audit Progress" tab, they are there to check on
    their own auditing progress. As such, the default filter for that view
    should reflect that.
    
    This change updates the tab to default to:
    
    - Assigned to: me
    - Audit status: all
    - Organisations: my organisation

--- app/controllers/audits/reports_controller.rb
+++ app/controllers/audits/reports_controller.rb
@@ -1,6 +1,12 @@
 module Audits
   class ReportsController < BaseController
     def show
+      @default_filter = {
+        allocated_to: current_user.uid,
+        audit_status: Audits::Audit::ALL,
+        organisations: [current_user.organisation_content_id],
+      }
+
       @monitor = ::Audits::Monitor.new(filter)
     end
   end
--- spec/features/audit/report/progress_spec.rb
+++ spec/features/audit/report/progress_spec.rb
@@ -1,53 +1,72 @@
 RSpec.feature "Reporting on audit progress" do
-  let!(:content_item) { create(:content_item, document_type: "transaction") }
-  let!(:flying) { create(:content_item, document_type: "transaction") }
-  let!(:insurance) { create(:content_item, document_type: "transaction") }
-
-  before do
-    create(:passing_audit, content_item: flying)
-    create(:failing_audit, content_item: insurance)
+  let!(:my_organisation) do
+    create(
+      :organisation
+    )
   end
 
-  def width(id)
-    page.find("#{id} .progress .progress-bar")[:style]
+  let!(:me) do
+    create(
+      :user,
+      organisation: my_organisation,
+    )
   end
 
-  scenario "Displaying the number of items included in the audit" do
-    visit audits_report_path
-    expect(page).to have_selector(".nav")
+  context "I have been allocated some audited and unaudited content items belonging to my organisation" do
+    before(:each) do
+      content_items = create_list(
+        :content_item,
+        3,
+        document_type: "transaction",
+        allocated_to: me,
+        primary_publishing_organisation: my_organisation,
+      )
 
-    expect(page).to have_content("3 Content items")
+      create(:passing_audit, content_item: content_items[0])
+      create(:failing_audit, content_item: content_items[1])
+    end
 
-    select "Anyone", from: "allocated_to"
+    scenario "Displaying the number of items included in the audit" do
+      visit audits_report_path
 
-    select "Guide", from: "document_type"
-    click_on "Apply filters"
-    expect(page).to have_content("0 Content items")
+      expect(page).to have_content("3 Content items")
 
-    select "Transaction", from: "document_type"
-    click_on "Apply filters"
-    expect(page).to have_content("3 Content items")
-  end
+      select "Anyone", from: "allocated_to"
+      select "Guide", from: "document_type"
+      click_on "Apply filters"
 
-  scenario "Displaying the number of items audited/not audited" do
-    visit audits_report_path
+      expect(page).to have_content("0 Content items")
 
-    expect(page).to have_content("Items audited 2 67%")
-    expect(page).to have_content("Items still to audit 1 33%")
-    expect(width("#progress")).to eq("width: 66.66667%;")
-  end
+      select "Transaction", from: "document_type"
+      click_on "Apply filters"
 
-  scenario "Displaying the number of items needing improvement/not needing improvement" do
-    visit audits_report_path
+      expect(page).to have_content("3 Content items")
+    end
 
-    expect(page).to have_content("Items that need improvement 1 50%")
-    expect(page).to have_content("Items that don't need improvement 1 50%")
-    expect(width("#items-needing-improvement")).to eq("width: 50%;")
+    scenario "Displaying the number of items audited/not audited" do
+      visit audits_report_path
+
+      expect(page).to have_content("Items audited 2 67%")
+      expect(page).to have_content("Items still to audit 1 33%")
+      expect(width("#progress")).to eq("width: 66.66667%;")
+    end
+
+    scenario "Displaying the number of items needing improvement/not needing improvement" do
+      visit audits_report_path
+
+      expect(page).to have_content("Items that need improvement 1 50%")
+      expect(page).to have_content("Items that don't need improvement 1 50%")
+      expect(width("#items-needing-improvement")).to eq("width: 50%;")
+    end
   end
 
   scenario "Audit status filter is not present" do
     visit audits_report_path
 
     expect(page).to have_no_content("Audit status")
+  end
+
+  def width(id)
+    page.find("#{id} .progress .progress-bar")[:style]
   end
 end
