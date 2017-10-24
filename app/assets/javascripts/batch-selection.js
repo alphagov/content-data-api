dcffee4ae8fd088538f71f9ad81b12a62497f596
Alec Gibson <alec.gibson@digital.cabinet-office.gov.uk>
2017-10-24 15:11:40 +0100

    Add a "Select all" checkbox for allocation

--- app/assets/javascripts/batch-selection.js
+++ app/assets/javascripts/batch-selection.js
@@ -4,24 +4,43 @@
   Modules.BatchSelection = function () {
     this.start = function ($root) {
       var $batchSizeInput = $root.find("#batch_size"),
-        $checkboxes = $root.find("input[type=checkbox]");
+        $checkboxes = $root.find("tbody input[type=checkbox]"),
+        $selectAll = $root.find("#select-all");
 
       $batchSizeInput.keyup(function () {
-          $checkboxes
-            .prop('checked', false)
-            .slice(0, $batchSizeInput.val())
-            .prop('checked', true);
-        }
-      );
+        selectFirstCheckboxes($batchSizeInput.val());
+      });
 
       $checkboxes.click(function () {
-          var checked = $checkboxes.filter(function () {
-            return $(this).is(':checked');
-          });
+        var checked = $checkboxes.filter(function () {
+          return $(this).is(':checked');
+        });
 
-          $batchSizeInput.val(checked.length);
+        $batchSizeInput.val(checked.length);
+
+        $selectAll.prop('checked', checked.length === $checkboxes.length);
+      });
+
+      $selectAll.click(function () {
+        var isChecked = $selectAll.is(':checked');
+
+        if (isChecked) {
+          selectFirstCheckboxes($checkboxes.length);
+        } else {
+          selectFirstCheckboxes(0);
         }
-      );
+      });
+
+      function selectFirstCheckboxes(numberToSelect) {
+        $checkboxes
+          .prop('checked', false)
+          .slice(0, numberToSelect)
+          .prop('checked', true);
+
+        $batchSizeInput.val(numberToSelect);
+
+        $selectAll.prop('checked', numberToSelect >= $checkboxes.length);
+      }
     }
   };
 })(window.GOVUKAdmin.Modules);
