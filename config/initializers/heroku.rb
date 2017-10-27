8cb95e5dcb627844d685bd655830210d63b00680
Pablo Manrubia <pmanrubia@gmail.com>
2017-10-27 12:57:45 +0100

    Defaults to the first user in Database
    The first time a user connects to the application in Heroku will
    be assigned the first user of the database.

--- config/initializers/heroku.rb
+++ config/initializers/heroku.rb
@@ -1,6 +1,7 @@
 if Heroku.enabled?
   ApplicationController.class_eval do
     before_action :force_development!
+    before_action :set_default_user
 
     def authenticate_user!
       user_id = params.dig(:heroku, :user_id)
@@ -10,6 +11,10 @@ if Heroku.enabled?
 
     def current_user
       User.find(session[:heroku_user_id])
+    end
+
+    def set_default_user
+      session[:heroku_user_id] ||= User.first.id
     end
 
     private
