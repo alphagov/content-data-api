2351f11b72a97feb63d58d1d66fd87042b11855f
Pablo Manrubia <pmanrubia@gmail.com>
2017-10-30 09:48:31 +0000

    Create default users in Heroku for user testing

--- lib/heroku.rb
+++ lib/heroku.rb
@@ -6,12 +6,14 @@ class Heroku
   def self.create_users(organisation_id)
     organisation = Content::Item.find(organisation_id)
 
-    User.create!(
-      name: 'AT user',
-      email: 'uat@gds.com',
-      uid: SecureRandom.uuid,
-      organisation_slug: organisation.title.parameterize.underscore,
-      organisation_content_id: organisation.content_id,
-    )
+    10.times do |index|
+      User.create!(
+        name: "user-#{index}",
+        email: "email-#{index}@domain.gov.uk",
+        uid: "uid-#{index}",
+        organisation_slug: organisation.title.parameterize.underscore,
+        organisation_content_id: organisation.content_id,
+      )
+    end
   end
 end
