67be73c6ed5018851bf389cfd24d56d240907637
Kelvin Gan <gds@kelvingan.co.uk>
2017-11-08 23:48:57 +0000

    Refactor tests to not rely on HTML classes
    As suggested by @KyleMacPherson, we are using the `data-test` attribute
    to assign to our markup so that our feature tests can select elements.
    By not relying on classes, we are able to separate the concern of
    classes so that they can remain specific to styling. This means that if
    the styling and classes needed to change, the tests would not break.

--- spec/support/pages/audit/content_audit_tool/audit_content_page.rb
+++ spec/support/pages/audit/content_audit_tool/audit_content_page.rb
@@ -4,20 +4,20 @@ class AuditContentPage < SitePrism::Page
   set_url "/audits"
 
   section :filter_form, "form" do
-    element :search, ".search"
-    element :allocated_to, ".allocated-to"
-    element :audit_status, ".audit-status"
-    element :document_type, ".document-type"
-    element :organisations, ".organisation-select-wrapper"
-    element :organisations_select, "#organisations-select"
-    element :organisations_input, "#organisations"
-    element :add_organisations, ".add-organisation"
-    element :primary_orgs_label, "label[for='primary']"
-    element :primary_orgs, ".primary-orgs"
-    element :apply_filters, ".apply-filters"
+    element :search, "[data-test=search]"
+    element :allocated_to, "[data-test=allocated-to]"
+    element :audit_status, "[data-test=audit-status]"
+    element :organisations, "[data-test=organisations]"
+    element :organisations_select, "[data-test=organisations-select]"
+    element :organisations_input, "[data-test=organisations] input"
+    element :add_organisations, "[data-test=add-organisation]"
+    element :primary_orgs_label, "[data-test=primary-orgs-label]"
+    element :primary_orgs, "[data-test=primary-orgs]"
+    element :document_type, "[data-test=document-type]"
+    element :apply_filters, "[data-test=apply-filters]"
   end
 
-  element :list, ".filter-list"
-  element :listing, ".filter-list tbody tr"
+  element :list, "[data-test=filter-list]"
+  element :listing, "[data-test=filter-list] tbody tr"
   element :pagination, ".pagination"
 end
