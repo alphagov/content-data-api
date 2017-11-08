4d2567fc1a6b66f9252d87ebced8a510c569b043
Pablo Manrubia <pmanrubia@gmail.com>
2017-11-08 23:56:40 +0000

    Unify the way parameters are being processed
    When no organisation is selected, then an empty array is returned, in
    which case, will be blank and removed as the other parameters.

--- app/controllers/audits/base_controller.rb
+++ app/controllers/audits/base_controller.rb
@@ -5,7 +5,6 @@ module Audits
 
     def filter(override = {})
       options = filter_from_non_blankable_query_parameters
-        .merge(filter_from_blankable_query_parameters)
         .merge(override)
 
       Filter.new(options)
@@ -22,29 +21,17 @@ module Audits
         sort: Sort.column(params[:sort_by]),
         sort_direction: Sort.direction(params[:sort_by]),
         primary_org_only: params[:primary] == 'true',
+        organisations: params.fetch(:organisations, []).flatten.reject(&:blank?),
         title: params[:query],
       }
 
       options.delete_if { |_, v| v.blank? }
     end
 
-    def filter_from_blankable_query_parameters
-      {}.tap do |options|
-        options[:organisations] = organisations if params.key?(:organisations)
-      end
-    end
-
     def filter_params
       Audits::SerializeFilterToQueryParameters
         .new(filter)
         .call
-    end
-
-    def organisations
-      params
-        .fetch(:organisations, [])
-        .flatten
-        .reject(&:blank?)
     end
   end
 end
