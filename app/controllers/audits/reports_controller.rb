d563f9c7474fe16f72c7785a093ea99fb5ef4bee
Pablo Manrubia <pmanrubia@gmail.com>
2017-11-08 22:04:14 +0000

    Set default parameters in the ReportsController
    Removes the dependency with `BaseController` by being explicit about
    the default parameters in a `before_action`.
    Makes it more obvious the code that is involved in setting up the
    parameters, and remove the inheritance mechanism that is currently
    in place.

--- app/controllers/audits/reports_controller.rb
+++ app/controllers/audits/reports_controller.rb
@@ -1,14 +1,18 @@
 module Audits
   class ReportsController < BaseController
-    def show
-      @default_filter = {
-        allocated_to: current_user.uid,
-        audit_status: Audits::Audit::ALL,
-        organisations: [current_user.organisation_content_id],
-        primary_org_only: true,
-      }
+    before_action :set_default_parameters, only: :show
 
+    def show
       @monitor = ::Audits::Monitor.new(filter)
+    end
+
+  private
+
+    def set_default_parameters
+      params[:allocated_to] ||= current_user.uid
+      params[:audit_status] ||= Audits::Audit::ALL
+      params[:organisations] ||= [current_user.organisation_content_id]
+      params[:primary] ||= 'true'
     end
   end
 end
