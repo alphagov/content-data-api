67cc52fdccbf497ccaeb4541f01dc03ac1b2ee2e
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2017-11-08 10:31:09 +0000

    Add data-test attributes to navigation and form elements for ease of testing

--- app/helpers/menu_helper.rb
+++ app/helpers/menu_helper.rb
@@ -1,8 +1,8 @@
 module MenuHelper
-  def navigation_link(text, url, scope)
+  def navigation_link(text, url, scope, data_test)
     status = controller.controller_name == scope ? 'active' : ''
 
-    content_tag :li, role: "presentation", class: status do
+    content_tag :li, role: "presentation", class: status, data: { test: data_test } do
       link_to_unless_current text, url, aria_controls: "home", role: "tab" do
         link_to text, "#", aria_controls: "home", role: "tab"
       end
