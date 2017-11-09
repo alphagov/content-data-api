04768780181b28c9f06a39c341e19c3ebe356055
Pablo Manrubia <pmanrubia@gmail.com>
2017-11-09 00:41:37 +0000

    Renames filter method to `params_to_filter`
    It represents better what it does. It also allow us to remove the old
    method for filtering, along with the process of the parameters.

--- app/controllers/audits/base_controller.rb
+++ app/controllers/audits/base_controller.rb
@@ -1,13 +1,7 @@
 module Audits
   class BaseController < ApplicationController
     layout "audits"
-    helper_method :filter, :filter_params
-
-    def filter
-      options = filter_from_non_blankable_query_parameters
-
-      Filter.new(options)
-    end
+    helper_method :params_to_filter, :filter_params
 
     def params_to_filter
       options = {
@@ -28,22 +22,6 @@ module Audits
       end
 
   private
-
-    def filter_from_non_blankable_query_parameters
-      options = {
-        allocated_to: params[:allocated_to],
-        audit_status: params[:audit_status],
-        document_type: params[:document_type],
-        page: params[:page],
-        sort: Sort.column(params[:sort_by]),
-        sort_direction: Sort.direction(params[:sort_by]),
-        primary_org_only: params[:primary] == 'true',
-        organisations: params.fetch(:organisations, []).flatten.reject(&:blank?),
-        title: params[:query],
-      }
-
-      options.delete_if { |_, v| v.blank? }
-    end
 
     def filter_params
       options = {
