03c5f95ec80a4b67e9980b36ef2ea006c74d4926
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2017-12-05 15:07:01 +0000

    Assert my content item count presence on each page
    Add tests to assert that the `my content` tab counts applicable
    user content and displays it as expected on each page.

--- spec/features/audit/tabs/my_content_spec.rb
+++ spec/features/audit/tabs/my_content_spec.rb
@@ -2,7 +2,7 @@ RSpec.feature "`My content` tab", type: :feature do
   scenario 'assigning content items to user to audit' do
     given_i_am_an_auditor_belonging_to_an_organisation
     and_i_have_no_content_items_assigned_to_me
-    when_i_visit_the_assign_content_page
+    when_i_visit_the_my_content_page
     then_i_can_see_that_i_have_0_content_items_to_audit
     when_i_navigate_to_assign_content_page
     and_i_assign_2_content_items_to_myself
@@ -14,10 +14,31 @@ RSpec.feature "`My content` tab", type: :feature do
   scenario 'auditing content items' do
     given_i_am_an_auditor_belonging_to_an_organisation
     and_i_have_5_content_items_assigned_to_me
-    when_i_visit_the_assign_content_page
+    when_i_visit_the_my_content_page
     then_i_can_see_that_i_have_5_content_items_to_audit
     when_i_audit_one_of_my_content_items
     then_i_can_see_that_i_have_4_content_items_left_to_audit
+  end
+
+  scenario 'on the my content page' do
+    given_i_am_an_auditor_belonging_to_an_organisation
+    and_i_have_5_content_items_assigned_to_me
+    when_i_visit_the_my_content_page
+    then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_my_content_page
+  end
+
+  scenario 'on the assign content page' do
+    given_i_am_an_auditor_belonging_to_an_organisation
+    and_i_have_5_content_items_assigned_to_me
+    when_i_visit_the_assign_content_page
+    then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_assignment_page
+  end
+
+  scenario 'on the audit progress page' do
+    given_i_am_an_auditor_belonging_to_an_organisation
+    and_i_have_5_content_items_assigned_to_me
+    when_i_visit_the_audit_progress_page
+    then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_progress_page
   end
 
   def given_i_am_an_auditor_belonging_to_an_organisation
@@ -34,7 +55,7 @@ RSpec.feature "`My content` tab", type: :feature do
            primary_publishing_organisation: @organisation)
   end
 
-  def when_i_visit_the_assign_content_page
+  def when_i_visit_the_my_content_page
     @audit_content_page = ContentAuditTool.new.audit_content_page
     @audit_content_page.load
   end
@@ -96,5 +117,27 @@ RSpec.feature "`My content` tab", type: :feature do
 
   def then_i_can_see_that_i_have_4_content_items_left_to_audit
     expect(@audit_content_page).to have_my_content_tab(text: 'My content (4)')
+  end
+
+  def when_i_visit_the_audit_progress_page
+    @audit_report_page = ContentAuditTool.new.audit_report_page
+    @audit_report_page.load
+  end
+
+  def when_i_visit_the_assign_content_page
+    @audit_assignment_page = ContentAuditTool.new.audit_assignment_page
+    @audit_assignment_page.load
+  end
+
+  def then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_progress_page
+    expect(@audit_report_page).to have_my_content_tab(text: 'My content (5)')
+  end
+
+  def then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_assignment_page
+    expect(@audit_assignment_page).to have_my_content_tab(text: 'My content (5)')
+  end
+
+  def then_i_can_see_that_i_have_5_content_items_left_to_audit_from_the_my_content_page
+    expect(@audit_content_page).to have_my_content_tab(text: 'My content (5)')
   end
 end
