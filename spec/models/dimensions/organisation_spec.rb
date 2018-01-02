7b0a77a7173b115a87d3f11ba108255ff410a69c
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-01-02 16:50:51 +0000

    Validate uniqueness of Dimensions::Organisation
    Ensure that each instance of Dimensions::Organisation is unique
    by scoping uniqueness of one attribute against all the others. If all
    of the attributes are the same as another instance of a
    Dimensions::Organisation then the instance will not be considered
    unique. If the combination of attributes is unique then the instance
    will be considered unique.

--- spec/models/dimensions/organisation_spec.rb
+++ spec/models/dimensions/organisation_spec.rb
@@ -6,4 +6,5 @@ RSpec.describe Dimensions::Organisation, type: :model do
   it { is_expected.to validate_presence_of(:link) }
   it { is_expected.to validate_presence_of(:content_id) }
   it { is_expected.to validate_presence_of(:state) }
+  it { should validate_uniqueness_of(:title).scoped_to(:slug, :link, :content_id, :state) }
 end
