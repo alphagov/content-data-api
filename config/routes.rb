7bad7c7ee7cc36339c964988b0bbc9c423f63aaf
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-01-22 15:14:41 +0000

    Remove audits routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,27 +13,9 @@ Rails.application.routes.draw do
     end
   end
 
-
-  namespace :audits do
-    get '/', to: "allocations#index"
-    get '/my-content', to: "audits#index", as: 'my_content'
-    resource :report, only: :show
-    resource :guidance, only: :show
-
-    post :allocations, to: "allocations#destroy", constraints: ->(req) { req.parameters["allocate_to"] == "no_one" }
-    resources :allocations, only: %w(index create)
-  end
-
   get '/api/v1/metrics/:content_id', to: "metrics#show"
 
   if Rails.env.development?
     mount GovukAdminTemplate::Engine, at: "/style-guide"
-  end
-
-  # rack-proxy does not work with webmock, so disable it for tests: https://github.com/ncr/rack-proxy#warning
-  if Rails.env.test?
-    get "#{Proxies::IframeAllowingProxy::PROXY_BASE_PATH}*base_path", to: proc { [200, {}, ['Proxy disabled in Test environment']] }
-  else
-    mount Proxies::IframeAllowingProxy.new => Proxies::IframeAllowingProxy::PROXY_BASE_PATH
   end
 end
