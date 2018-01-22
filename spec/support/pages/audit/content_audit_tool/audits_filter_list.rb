614bcdf9738acf0960d2b1d7c9587bea5f36ede4
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-01-22 15:37:46 +0000

    Remove audit page object model
    Removing the audit code and relevant tests means the audit page object
    model is no longer needed.

--- spec/support/pages/audit/content_audit_tool/audits_filter_list.rb
+++
@@ -1,6 +0,0 @@
-require "site_prism/page"
-
-class AuditsFilterList < SitePrism::Page
-  element :list, "table[data-test-id=filter-list] tbody"
-  sections :listings, AuditListingSection, "table[data-test-id=filter-list] tbody tr"
-end
