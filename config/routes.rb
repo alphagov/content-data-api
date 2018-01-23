955a60413c1950fda98584ca7a85a8c8d4f46b71
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-01-23 09:42:58 +0000

    Remove additional audit views and stylesheets

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,6 @@ Rails.application.routes.draw do
     resources :items, only: %w(index show), param: :content_id
   end
 
-  resources :content_items, only: %w(index show), param: :content_id do
-    scope module: "audits" do
-      get :audit, to: "audits#show"
-      post :audit, to: "audits#save"
-      patch :audit, to: "audits#save"
-    end
-  end
-
   if Rails.env.development?
     mount GovukAdminTemplate::Engine, at: "/style-guide"
   end
