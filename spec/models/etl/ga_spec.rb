168c7007e96bc257797a5052fb1c60ab49b1ce8d
Pablo Manrubia <pmanrubia@gmail.com>
2018-02-11 11:42:40 +0000

    Load GA events in Facts Table
    I have split the process in two steps. The first one will import all
    the events from GA in batches of 10_000 events. I have chosen this number
    because it matches the maximum number of events in each GA request.
    All the events are stored in GA::Events table to be process in the second
    step.
    
    On the second step, in a single query, we update the facts metrics table.
    I have chosen to write custom SQL as the amount of records that we are
    handling is very big (around 600K), and loading into AR for processing,
    is taking almost 2 hours with a single process. The time is not a problem
    as we don’t have performance requirements so far, but we should’t have
    transactions that take so much time.
    
    By running the update query we reduce this process to 7-8 seconds, so we
    have a small transaction, and the database does all the heavy lifting,
    not the application.

---
+++ spec/models/etl/ga_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+require 'gds-api-adapters'
+
+RSpec.describe ETL::GA do
+  subject { described_class }
+
+  let(:item1) { create :dimensions_item, base_path: '/path1', latest: true }
+  let(:item2) { create :dimensions_item, base_path: '/path2', latest: true }
+
+  let(:date) { Date.new(2018, 2, 20) }
+  let(:dimensions_date) { Dimensions::Date.for(date) }
+
+  before { allow_any_instance_of(GoogleAnalyticsService).to receive(:find_in_batches).and_yield(ga_response) }
+
+  context 'When the base_path matches the GA path' do
+    it 'update the facts with the GA metrics' do
+      fact1 = create :facts_metric, dimensions_item: item1, dimensions_date: dimensions_date
+      fact2 = create :facts_metric, dimensions_item: item2, dimensions_date: dimensions_date
+
+      described_class.process(date: date)
+
+      expect(fact1.reload).to have_attributes(pageviews: 1, unique_pageviews: 1)
+      expect(fact2.reload).to have_attributes(pageviews: 2, unique_pageviews: 2)
+    end
+
+    it 'does not update metrics for other days' do
+      fact1 = create :facts_metric, dimensions_item: item1, dimensions_date: dimensions_date, pageviews: 20, unique_pageviews: 10
+
+      day_before = date - 1
+      described_class.process(date: day_before)
+
+      expect(fact1.reload).to have_attributes(pageviews: 20, unique_pageviews: 10)
+    end
+
+    it 'does not update metrics for other items' do
+      item = create :dimensions_item, base_path: '/non-matching-path', latest: true
+      fact = create :facts_metric, dimensions_item: item, dimensions_date: dimensions_date, pageviews: 99, unique_pageviews: 90
+
+      described_class.process(date: date)
+
+      expect(fact.reload).to have_attributes(pageviews: 99, unique_pageviews: 90)
+    end
+
+  end
+
+private
+
+  def ga_response
+    [
+      {
+        'page_path' => '/path1',
+        'pageviews' => 1,
+        'unique_pageviews' => 1,
+        'date' => '2018-02-20',
+      },
+      {
+        'page_path' => '/path2',
+        'pageviews' => 2,
+        'unique_pageviews' => 2,
+        'date' => '2018-02-20',
+      },
+    ]
+  end
+end
