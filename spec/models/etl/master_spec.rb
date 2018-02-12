9c0ca2ceff018db66a946f6bbf7ed018f4626e42
Pablo Manrubia <pmanrubia@gmail.com>
2018-02-12 14:11:39 +0100

    Ensure that process only applies to latest items
    We should create a Facts entry in the latest content items, so we avoid
    duplications and inconsistencies in the Data Warehouse.

--- spec/models/etl/master_spec.rb
+++ spec/models/etl/master_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe ETL::Master do
       dimensions_item: item,
     )
   end
+
+  it 'only create a Metrics Fact entry for Content Items with latest = `true`' do
+    create(:dimensions_item, latest: true, content_id: 'cid1')
+    create(:dimensions_item, latest: false, content_id: 'cid1')
+
+    subject.process
+
+    expect(Facts::Metric.count).to eq(1)
+  end
 end
