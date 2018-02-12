626a66e6dfa9e767276f1555b0fc9f2e09c90ad5
Pablo Manrubia <pmanrubia@gmail.com>
2018-02-12 13:42:33 +0100

    Remove tests that depends on Organisations
    We have already removed the Dimensions for Organisations, so this tests
    should have been removed as well.

--- spec/models/etl/master_spec.rb
+++ spec/models/etl/master_spec.rb
@@ -16,16 +16,6 @@ RSpec.describe ETL::Master do
     expect(Facts::Metric.count).to eq(2)
   end
 
-  it 'does not raise an exception if the content item has no organisation' do
-    expect(ETL::Items).to receive(:process) do
-      create :dimensions_item, latest: true
-    end
-
-    subject.process
-
-    expect(Facts::Metric.count).to eq(1)
-  end
-
   it 'creates a metrics fact with the associated dimensions' do
     expect(ETL::Items).to receive(:process) do
       create(:dimensions_item, latest: true, content_id: 'cid1')
@@ -37,19 +27,5 @@ RSpec.describe ETL::Master do
       dimensions_date: date,
       dimensions_item: Dimensions::Item.find_by(content_id: 'cid1'),
     )
-  end
-
-  it 'creates multiple items for multiple organisations' do
-    expect(ETL::Items).to receive(:process) do
-      create(:dimensions_item, latest: true, content_id: 'cid1')
-      create(:dimensions_item, latest: true, content_id: 'cid2')
-    end
-    subject.process
-
-    expect(Facts::Metric.count).to eq(2)
-    item1 = Dimensions::Item.find_by(content_id: 'cid1')
-    item2 = Dimensions::Item.find_by(content_id: 'cid2')
-    expect(Facts::Metric.first).to have_attributes(dimensions_item: item1, dimensions_date: date)
-    expect(Facts::Metric.second).to have_attributes(dimensions_item: item2, dimensions_date: date)
   end
 end
