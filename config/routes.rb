e2362ba331e8b25da219fcaada1d364261007050
Kelvin Gan <gds@kelvingan.co.uk>
2018-02-13 12:58:08 +0000

    Add audits redirect
    The route /audits was where the Audit Tool used to live in the Content
    Performance Manager. This now lives as its own service, so we need to
    redirect users to the correct location.
    
    Have added a request spec as redirects do not work with routing specs[1].
    
    [1] https://stackoverflow.com/questions/10842448/do-routing-specs-support-redirect-routes-rspec/10844398#10844398

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get '/api/v1/metrics/:content_id', to: "metrics#show"
 
+  get '/audits', to: redirect(Plek.find('content-audit-tool', status: 302))
+
   if Rails.env.development?
     mount GovukAdminTemplate::Engine, at: "/style-guide"
   end
