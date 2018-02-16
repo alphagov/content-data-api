86856a6136cdd70d9f8e50b40930ebcdd044f297
Pablo Manrubia <pmanrubia@gmail.com>
2018-02-16 19:15:46 +0000

    Remove Dropdown options for Themes and Subthemes

--- app/helpers/dropdown_helper.rb
+++ app/helpers/dropdown_helper.rb
@@ -14,41 +14,4 @@ module DropdownHelper
 
     options_for_select(organisation_options, selected)
   end
-
-  def allocation_options_for_select(selected = nil, include_anyone: true)
-    selected = current_user.uid if selected.nil?
-
-    additional_options = [
-      ['Me', current_user.uid],
-      ['No one', :no_one],
-    ]
-
-    additional_options << ['Anyone', :anyone] if include_anyone
-
-    allocation_options = FindOrganisationUsers
-                           .call(organisation_slug: current_user.organisation_slug)
-                           .pluck(:name, :uid)
-                           .map { |name, uid| [name.squish, uid] }
-                           .reject { |_, uid| uid == current_user.uid }
-                           .sort_by { |name, _| name }
-                           .unshift(*additional_options)
-
-    options_for_select(allocation_options, selected)
-  end
-
-  class ThemeOption < SimpleDelegator
-    def name
-      "All #{super}"
-    end
-
-    def value
-      "Theme_#{id}"
-    end
-  end
-
-  class SubthemeOption < SimpleDelegator
-    def value
-      "Subtheme_#{id}"
-    end
-  end
 end
