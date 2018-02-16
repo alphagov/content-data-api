162bde817a8af4d7e5939af781846de0e1ac74c4
Pablo Manrubia <pmanrubia@gmail.com>
2018-02-16 18:13:54 +0000

    Remove unused factories: group
    This model is no longer in use, so it can be safely removed.

--- spec/factories.rb
+++ spec/factories.rb
@@ -73,24 +73,6 @@ FactoryBot.define do
     end
   end
 
-  factory :group do
-    sequence(:slug) { |index| "slug-#{index}" }
-    sequence(:name) { |index| "name-#{index}" }
-    sequence(:group_type) { |index| "group-type-#{index}" }
-
-    trait :with_two_content_items do
-      after(:create) do |group|
-        group.content_items = create_list(:content_item, 2)
-      end
-    end
-
-    trait :with_one_content_item do
-      after(:create) do |group|
-        group.content_items << create(:content_item)
-      end
-    end
-  end
-
   factory :dimensions_date, class: Dimensions::Date do
     sequence(:date) { |i| i.days.ago.to_date }
 
