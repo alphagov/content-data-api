1e2e07b2118a63961315549e2501c9734fd17e8c
Kelvin Gan <gds@kelvingan.co.uk>
2018-02-19 15:14:08 +0000

    Populate metadata fields for Dimension::Items

--- app/domain/importers/content_details.rb
+++ app/domain/importers/content_details.rb
@@ -14,7 +14,26 @@ class Importers::ContentDetails
   def run
     response = items_service.fetch_raw_json(base_path)
     number_of_pdfs = Performance::Metrics::NumberOfPdfs.parse(response.to_h['details'])
+    metadata = format_metadata(response.to_h)
+    attributes =  metadata.merge(raw_json: response, number_of_pdfs: number_of_pdfs)
     item = Dimensions::Item.find_by(content_id: content_id, latest: true)
-    item.update_attributes(raw_json: response, number_of_pdfs: number_of_pdfs)
+    item.update_attributes(attributes)
+  end
+
+private
+
+  def format_metadata(formatted_response)
+    formatted_response.slice(
+      'content_id',
+      'title',
+      'document_type',
+      'content_purpose_supertype',
+      'first_published_at',
+      'public_updated_at',
+    )
+  end
+
+  def number_of_pdfs(formatted_response)
+    Performance::Metrics::NumberOfPdfs.parse(formatted_response['details'])
   end
 end
