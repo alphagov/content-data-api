da17ea4595b6b30147377118692a376ebfea5226
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-02-21 11:28:22 +0000

    Add parse method to count word attachments from response
    I have added another method, rather than using the existing run method,
    because the current CPM will be removed shortly and it is not worth unifying the codebase.

--- app/domain/performance/metrics/number_of_word_files.rb
+++ app/domain/performance/metrics/number_of_word_files.rb
@@ -13,5 +13,11 @@ module Performance::Metrics
       documents = NumberOfFiles.parse documents_string
       { number_of_word_files: NumberOfFiles.number_of_files(documents, DOC_XPATH) }
     end
+
+    def self.parse(details)
+      documents_string = NumberOfFiles.extract_documents(details)
+      documents = NumberOfFiles.parse documents_string
+      NumberOfFiles.number_of_files(documents, DOC_XPATH)
+    end
   end
 end
--- spec/domain/performance/metrics/number_of_word_files_spec.rb
+++ spec/domain/performance/metrics/number_of_word_files_spec.rb
@@ -25,20 +25,48 @@ module Performance
     let(:content_without_document_keys) { build(:content_item, details: {}) }
     let(:content_without_details_key) { build(:content_item) }
 
-    it "returns the number of word files present" do
-      expect(subject.new(content_with_word_files).run).to eq(number_of_word_files: 3)
+    context "#run" do
+      it "returns the number of word files present" do
+        expect(subject.new(content_with_word_files).run).to eq(number_of_word_files: 3)
+      end
+
+      it "returns 0 if no pdfs are present" do
+        expect(subject.new(content_without_word_files).run).to eq(number_of_word_files: 0)
+      end
+
+      it "returns 0 if no document keys are present" do
+        expect(subject.new(content_without_document_keys).run).to eq(number_of_word_files: 0)
+      end
+
+      it "returns 0 if the 'details' key is not present" do
+        expect(subject.new(content_without_details_key).run).to eq(number_of_word_files: 0)
+      end
     end
 
-    it "returns 0 if no pdfs are present" do
-      expect(subject.new(content_without_word_files).run).to eq(number_of_word_files: 0)
-    end
+    context "#parse" do
+      it "returns the number of word files present" do
+        details = {
+          'documents' => '<div class=\"attachment-details\">\n<a href=\"link.doc\">1</a>\n\n\n\n</div>'
+        }
 
-    it "returns 0 if no document keys are present" do
-      expect(subject.new(content_without_document_keys).run).to eq(number_of_word_files: 0)
-    end
+        expect(subject.parse(details)).to eq(1)
+      end
 
-    it "returns 0 if the 'details' key is not present" do
-      expect(subject.new(content_without_details_key).run).to eq(number_of_word_files: 0)
+      it "returns 0 if no word files are present" do
+        details = {
+          'documents' => '<div class=\"attachment-details\">\n<a href=\"link.txt\">1</a>\n\n\n\n</div>'
+        }
+
+        expect(subject.parse(details)).to eq(0)
+      end
+
+      it "returns 0 if no document keys are present" do
+        expect(subject.parse({})).to eq(0)
+      end
+
+      it "returns 0 if the details is nil" do
+        expect(subject.parse({})).to eq(0)
+      end
     end
   end
 end
