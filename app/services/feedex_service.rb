d9742b66e783b73899ba9d08403cceca2ec9d80a
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-02-27 12:14:36 +0000

    Implemented feedex service

--- app/services/feedex_service.rb
+++ app/services/feedex_service.rb
@@ -1,3 +1,34 @@
+require 'gds_api/support_api'
+
 class FeedexService
-  def find_in_batches(date); end
+  attr_reader :date, :batch_size, :support_api
+
+  def initialize(date, batch_size, support_api = GdsApi::SupportApi.new(Plek.new.find('support-api')))
+    @date = date
+    @batch_size = batch_size
+    @support_api = support_api
+  end
+
+  def find_in_batches
+    current_page = 1
+    loop do
+      response = support_api.feedback_by_day(date, current_page, @batch_size)
+      yield convert_results(response['results'])
+
+      break if response['pages'] == current_page
+      current_page += 1
+    end
+  end
+
+private
+
+  def convert_results(results)
+    results.map do |result|
+      {
+        date: date,
+        page_path: result['path'],
+        number_of_issues: result['count']
+      }
+    end
+  end
 end
