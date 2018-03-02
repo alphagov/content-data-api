2e2dc9c5cb36266b063fdb43d8d8b1deeadf9f3d
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-02 22:50:01 +0000

    Inline Module and Class name for Queries::Metrics

--- app/models/queries/metrics.rb
+++ app/models/queries/metrics.rb
@@ -1,17 +1,14 @@
-module Queries
-  class Metrics
-    def self.run(from:, to:, base_path: nil)
-      metrics = Facts::Metric.
-        joins(:dimensions_date).
-        joins(:dimensions_item).
-        where('dimensions_dates.date BETWEEN ? AND ?', from, to)
+module Queries::Metrics
+  def self.run(from:, to:, base_path: nil)
+    metrics = Facts::Metric.
+      joins(:dimensions_date).
+      joins(:dimensions_item).
+      where('dimensions_dates.date BETWEEN ? AND ?', from, to)
 
-      if base_path.present?
-        metrics = metrics.where('dimensions_items.base_path like (?)', base_path)
-      end
-
-      metrics
+    if base_path.present?
+      metrics = metrics.where('dimensions_items.base_path like (?)', base_path)
     end
+
+    metrics
   end
 end
-\
