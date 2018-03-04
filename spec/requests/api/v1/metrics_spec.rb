74f5c3e0151ba040a9048bc7ecc39d3da5a7002c
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-04 21:25:29 +0000

    DRY-UP API spec for :/api/v1/metrics/:content_id
    ```shell
    % rspec spec/requests/api --format documentation
    
    /api/v1/metrics/:content_id
      returns `bad request` for metrics not on the whitelist
      Daily metrics
        returns `feedex issues` between two dates
        returns `pageviews` values between two dates
      Content metrics
        returns the `number of pdfs` between two dates
        returns the `number of word documents` between two dates
    
    Finished in 0.94019 seconds (files took 0.52941 seconds to load)
    5 examples, 0 failures
    ```

--- spec/requests/api/v1/metrics_spec.rb
+++ spec/requests/api/v1/metrics_spec.rb
@@ -9,69 +9,38 @@ RSpec.describe '/api/v1/metrics/:content_id', type: :request do
 
   let!(:item) { create :dimensions_item, content_id: 'id1' }
 
-  let!(:metric1) { create :metric, dimensions_item: item, dimensions_date: day1 }
-  let!(:metric2) { create :metric, dimensions_item: item, dimensions_date: day2 }
-  let!(:metric3) { create :metric, dimensions_item: item, dimensions_date: day3 }
-  let!(:metric4) { create :metric, dimensions_item: item, dimensions_date: day4 }
-
   it 'returns `bad request` for metrics not on the whitelist' do
     get '/api/v1/metrics/id1', params: { metric: 'something-else', from: '2018-01-13', to: '2018-01-15' }
 
     expect(response.status).to eq(400)
   end
 
-  describe 'pageviews' do
+  describe 'Daily metrics' do
     before do
-      metric1.update pageviews: 10
-      metric2.update pageviews: 20
-      metric3.update pageviews: 30
-      metric4.update pageviews: 40
+      create :metric, dimensions_item: item, dimensions_date: day1, pageviews: 10, number_of_issues: 10
+      create :metric, dimensions_item: item, dimensions_date: day2, pageviews: 20, number_of_issues: 20
+      create :metric, dimensions_item: item, dimensions_date: day3, pageviews: 30, number_of_issues: 30
+      create :metric, dimensions_item: item, dimensions_date: day4, pageviews: 40, number_of_issues: 40
     end
 
-    it 'returns metric values between two dates' do
+    it 'returns `pageviews` values between two dates' do
       get '/api/v1/metrics/id1', params: { metric: 'pageviews', from: '2018-01-13', to: '2018-01-15' }
 
-      json = JSON.parse(response.body)
-      expect(json.deep_symbolize_keys).to eq(api_reponse)
+      json = JSON.parse(response.body).deep_symbolize_keys
+      expect(json).to eq(build_api_response('pageviews'))
     end
 
-    def api_reponse
-      {
-        metadata: {
-          metric: 'pageviews',
-          total: 60,
-          from: '2018-01-13',
-          to: '2018-01-15',
-          content_id: 'id1',
-        },
-        results: [
-          { content_id: 'id1', date: '2018-01-13', value: 10 },
-          { content_id: 'id1', date: '2018-01-14', value: 20 },
-          { content_id: 'id1', date: '2018-01-15', value: 30 },
-        ]
-      }
-    end
-  end
-
-  describe 'feedex issues' do
-    before do
-      metric1.update number_of_issues: 10
-      metric2.update number_of_issues: 20
-      metric3.update number_of_issues: 30
-      metric4.update number_of_issues: 40
-    end
-
-    it 'returns metric values between two dates' do
+    it 'returns `feedex issues` between two dates' do
       get '/api/v1/metrics/id1', params: { metric: 'number_of_issues', from: '2018-01-13', to: '2018-01-15' }
 
       json = JSON.parse(response.body)
-      expect(json.deep_symbolize_keys).to eq(api_reponse)
+      expect(json.deep_symbolize_keys).to eq(build_api_response('number_of_issues'))
     end
 
-    def api_reponse
+    def build_api_response(metric_name)
       {
         metadata: {
-          metric: 'number_of_issues',
+          metric: metric_name,
           total: 60,
           from: '2018-01-13',
           to: '2018-01-15',
@@ -86,59 +55,35 @@ RSpec.describe '/api/v1/metrics/:content_id', type: :request do
     end
   end
 
-  describe 'number_of_pdfs' do
+  describe 'Content metrics' do
     before do
-      old_version = create :dimensions_item, content_id: 'id1', number_of_pdfs: 30, latest: false
-      metric1.update dimensions_item: old_version
+      item1_old = create :dimensions_item, content_id: 'id1', number_of_pdfs: 30, number_of_word_files: 30, latest: false
+      item.update number_of_pdfs: 20, number_of_word_files: 20
 
-      item.update number_of_pdfs: 20
+      create :metric, dimensions_item: item1_old, dimensions_date: day1
+      create :metric, dimensions_item: item, dimensions_date: day2
+      create :metric, dimensions_item: item, dimensions_date: day3
+      create :metric, dimensions_item: item, dimensions_date: day4
     end
 
-    it 'returns metric values between two dates' do
+    it 'returns the `number of pdfs` between two dates' do
       get '/api/v1/metrics/id1', params: { metric: 'number_of_pdfs', from: '2018-01-13', to: '2018-01-15' }
 
       json = JSON.parse(response.body)
-      expect(json.deep_symbolize_keys).to eq(api_reponse)
+      expect(json.deep_symbolize_keys).to eq(api_reponse('number_of_pdfs'))
     end
 
-    def api_reponse
-      {
-        metadata: {
-          metric: 'number_of_pdfs',
-          total: 70,
-          from: '2018-01-13',
-          to: '2018-01-15',
-          content_id: 'id1',
-        },
-        results: [
-          { content_id: 'id1', date: '2018-01-13', value: 30 },
-          { content_id: 'id1', date: '2018-01-14', value: 20 },
-          { content_id: 'id1', date: '2018-01-15', value: 20 },
-        ]
-      }
-    end
-  end
-
-  describe 'number_of_word_files' do
-    before do
-      old_version = create :dimensions_item, content_id: 'id1', number_of_word_files: 30, latest: false
-      metric1.update dimensions_item: old_version
-
-      item.update number_of_word_files: 20
-    end
-
-    it 'returns metric values between two dates' do
+    it 'returns the `number of word documents` between two dates' do
       get '/api/v1/metrics/id1', params: { metric: 'number_of_word_files', from: '2018-01-13', to: '2018-01-15' }
 
-
       json = JSON.parse(response.body)
-      expect(json.deep_symbolize_keys).to eq(api_reponse)
+      expect(json.deep_symbolize_keys).to eq(api_reponse('number_of_word_files'))
     end
 
-    def api_reponse
+    def api_reponse(metric_name)
       {
         metadata: {
-          metric: 'number_of_word_files',
+          metric: metric_name,
           total: 70,
           from: '2018-01-13',
           to: '2018-01-15',
