f8a958e4f9c05c9315db11df4a54bf53f281e5b7
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-04 11:29:52 +0000

    DRY-UP RSpec test to include other metrics

--- spec/requests/api/v1/metrics_spec.rb
+++ spec/requests/api/v1/metrics_spec.rb
@@ -2,69 +2,57 @@ require 'rails_helper'
 RSpec.describe '/api/v1/metrics/:content_id', type: :request do
   before { create(:user) }
 
-  context 'with valid parmeters' do
-    it 'List a metric for a content item between two dates' do
-      day1 = create(:dimensions_date, date: Date.new(2018, 1, 13))
-      day2 = create(:dimensions_date, date: Date.new(2018, 1, 14))
-      day3 = create(:dimensions_date, date: Date.new(2018, 1, 15))
-      day4 = create(:dimensions_date, date: Date.new(2018, 1, 16))
-      item1 = create(:dimensions_item, content_id: 'id1')
+  let!(:day1) { create :dimensions_date, date: Date.new(2018, 1, 13) }
+  let!(:day2) { create :dimensions_date, date: Date.new(2018, 1, 14) }
+  let!(:day3) { create :dimensions_date, date: Date.new(2018, 1, 15) }
+  let!(:day4) { create :dimensions_date, date: Date.new(2018, 1, 16) }
 
-      create(:metric, dimensions_item: item1, dimensions_date: day1, pageviews: 20, unique_pageviews: 10)
-      create(:metric, dimensions_item: item1, dimensions_date: day2, pageviews: 10, unique_pageviews: 5)
-      create(:metric, dimensions_item: item1, dimensions_date: day3, pageviews: 20, unique_pageviews: 10)
-      create(:metric, dimensions_item: item1, dimensions_date: day4, pageviews: 20, unique_pageviews: 10)
+  let!(:item) { create :dimensions_item, content_id: 'id1' }
 
-      query_params = {
-        metric: 'pageviews',
-        from: Date.new(2018, 1, 13),
-        to: Date.new(2018, 1, 15),
-      }
-      get '/api/v1/metrics/id1', params: query_params
+  let!(:metric1) { create :metric, dimensions_item: item, dimensions_date: day1 }
+  let!(:metric2) { create :metric, dimensions_item: item, dimensions_date: day2 }
+  let!(:metric3) { create :metric, dimensions_item: item, dimensions_date: day3 }
+  let!(:metric4) { create :metric, dimensions_item: item, dimensions_date: day4 }
 
-      json = JSON.parse(response.body)
-      expect(json.deep_symbolize_keys).to eq(pageviews_response)
+  it 'returns `bad request` for metrics not on the whitelist' do
+    get '/api/v1/metrics/id1', params: { metric: 'something-else', from: '2018-01-13', to: '2018-01-15' }
+
+    expect(response.status).to eq(400)
+  end
+
+  describe 'pageviews' do
+    before do
+      metric1.update pageviews: 10
+      metric2.update pageviews: 20
+      metric3.update pageviews: 30
+      metric4.update pageviews: 40
     end
 
-    def pageviews_response
+    it 'returns metric values between two dates' do
+      get '/api/v1/metrics/id1', params: { metric: 'pageviews', from: '2018-01-13', to: '2018-01-15' }
+
+      json = JSON.parse(response.body)
+      expect(json.deep_symbolize_keys).to eq(api_reponse)
+    end
+
+    def api_reponse
       {
         metadata: {
           metric: 'pageviews',
-          total: 50,
+          total: 60,
           from: '2018-01-13',
           to: '2018-01-15',
           content_id: 'id1',
         },
         results: [
-          {
-            content_id: 'id1',
-            date: '2018-01-13',
-            value: 20,
-          },
-          {
-            content_id: 'id1',
-            date: '2018-01-14',
-            value: 10,
-          },
-          {
-            content_id: 'id1',
-            date: '2018-01-15',
-            value: 20,
-          }
+          { content_id: 'id1', date: '2018-01-13', value: 10 },
+          { content_id: 'id1', date: '2018-01-14', value: 20 },
+          { content_id: 'id1', date: '2018-01-15', value: 30 },
         ]
       }
     end
   end
 
-  context 'with invalid parameters' do
-    it 'returns bad request for metrics not on the whitelist' do
-      query_params = {
-        metric: 'something-else',
-        from: Date.new(2018, 1, 13),
-        to: Date.new(2018, 1, 15),
-      }
-      get '/api/v1/metrics/id1.json', params: query_params
-      expect(response.status).to eq(400)
     end
   end
 end
