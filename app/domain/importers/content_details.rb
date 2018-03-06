61b81f9c45550439a05f3ab613948e491d84a3d5
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-06 16:14:05 +0000

    Divide data item details acquisition to extract Job
    It is a bit better to pull quality metrics via a Sidekiq job so we don’t
    it is more robust and reliable.

--- app/domain/importers/content_details.rb
+++ app/domain/importers/content_details.rb
@@ -16,12 +16,13 @@ class Importers::ContentDetails
 
   def run
     item = Dimensions::Item.find_by(content_id: content_id, latest: true)
+
     item_raw_json = items_service.fetch_raw_json(base_path)
-    item.update_attributes(raw_json: item_raw_json)
     metadata = format_response(item_raw_json)
-    item.update_attributes(metadata)
-      # quality_metrics = content_quality_service.run(item.get_content)
-      # item.update_attributes(metadata.merge(quality_metrics))
+    item.update_attributes(metadata.merge! raw_json: item_raw_json)
+
+    quality_metrics = content_quality_service.run(item.get_content)
+    item.update_attributes(quality_metrics)
   rescue GdsApi::HTTPGone
     item.gone!
   rescue GdsApi::HTTPNotFound
