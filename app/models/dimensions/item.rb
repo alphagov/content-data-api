879020557f639c6730ba0e061dfa90233eb02999
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-03-13 11:37:02 +0000

    refactor body content extraction

--- app/models/dimensions/item.rb
+++ app/models/dimensions/item.rb
@@ -39,50 +39,13 @@ class Dimensions::Item < ApplicationRecord
 
 private
 
-  VALID_SCHEMA_TYPES = %w[
-    answer
-    case_study
-    consultation
-    corporate_information_page
-    detailed_guide
-    document_collection
-    fatality_notice
-    help
-    hmrc_manual_section
-    html_publication
-    manual
-    manual_section
-    news_article
-    publication
-    service_manual_guide
-    simple_smart_answer
-    specialist_document
-    speech
-    statistical_data_set
-    take_part
-    topical_event_about_page
-    transaction
-    working_group
-    world_location_news_article
-  ].freeze
-
   def extract_by_schema_type(json)
     schema = json.dig("schema_name")
     if Parsers::ContentExtractors.for_schema(schema)
       html = Parsers::ContentExtractors.for_schema(schema).parse(json)
       return parse_html(html)
     end
-
-    if schema.nil? || !VALID_SCHEMA_TYPES.include?(schema)
-      raise InvalidSchemaError, "Schema does not exist: #{schema}"
-    end
-
-    html = extract_main(json)
-    parse_html(html)
-  end
-
-  def extract_main(json)
-    json.dig("details", "body")
+    raise InvalidSchemaError, "Schema does not exist: #{schema}"
   end
 
   def parse_html(html)
