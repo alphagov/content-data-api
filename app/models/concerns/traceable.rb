26064774bd9c87bd30a670bf367e24645b36a623
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-03-16 09:48:29 +0000

    Log Any Exceptions Raised
    Raise exception if no block given to Traceable#time
    Delete unused ETL::Dates class and it's test

--- app/models/concerns/traceable.rb
+++ app/models/concerns/traceable.rb
@@ -5,11 +5,16 @@ module Concerns::Traceable
 
   included do
     def time(process:)
+      raise ArgumentError "No block was given to Traceable#time" unless block_given?
       started = Time.now
       logger.info "Process: '#{process}' started at #{started}"
       yield
       ended = Time.now
       logger.info "Process: '#{process}' ended at #{ended}, duration: #{ended - started}"
+    rescue StandardError => ex
+      logger.error(ex.message)
+      logger.error(ex.backtrace.inspect)
+      raise ex
     end
 
     def log(message:)
