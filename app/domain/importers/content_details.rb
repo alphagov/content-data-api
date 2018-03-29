176c4c1094c023311bafaf8f5e084ad0a416f581
Pablo Manrubia <pmanrubia@gmail.com>
2018-03-29 11:47:29 +0100

    Remove logging from ImportDetails job
    It is not adding much value, and it outputs a huge amount of logs when
    preloading the DW

--- app/domain/importers/content_details.rb
+++ app/domain/importers/content_details.rb
@@ -1,8 +1,7 @@
 require 'odyssey'
 
 class Importers::ContentDetails
-  include Concerns::Traceable
-  attr_reader :items_service, :id,
+  attr_reader :items_service, :id
 
   def self.run(*args)
     new(*args).run
@@ -16,15 +15,13 @@ class Importers::ContentDetails
   def run
     item = nil
     begin
-      time(process: :content_details) do
-        item = Dimensions::Item.find(id)
-        item_raw_json = items_service.fetch_raw_json(item.base_path)
-        attributes = Metadata::Parser.parse(item_raw_json)
-        needs_quality_metrics = item.quality_metrics_required?(attributes)
-        item.update_attributes(attributes)
+      item = Dimensions::Item.find(id)
+      item_raw_json = items_service.fetch_raw_json(item.base_path)
+      attributes = Metadata::Parser.parse(item_raw_json)
+      needs_quality_metrics = item.quality_metrics_required?(attributes)
+      item.update_attributes(attributes)
 
-        ImportQualityMetricsJob.perform_async(item.id) if needs_quality_metrics
-      end
+      ImportQualityMetricsJob.perform_async(item.id) if needs_quality_metrics
     rescue GdsApi::HTTPGone
       item.gone!
       handle_gone base_path: item.base_path
@@ -33,7 +30,7 @@ class Importers::ContentDetails
     end
   end
 
-private
+  private
 
   def handle_not_found(base_path:)
     log process: :content_details, message: "NotFound when retrieving '#{base_path}' from content store"
