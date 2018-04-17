b6bf1c4a371739006de79e61c77d5d2689d660a2
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-04-17 17:02:26 +0100

    Add missing tests
    Add tests that were removed during the directory changes

---
+++ spec/etl/items/importers/content_details_spec.rb
@@ -0,0 +1,139 @@
+RSpec.describe Items::Importers::ContentDetails do
+  let(:base_path) { '/base_path' }
+  let(:content_id) { 'content_id' }
+
+
+
+  context 'Import contents' do
+    subject { Items::Importers::ContentDetails.new(latest_dimension_item.id) }
+    let(:locale) { 'en' }
+    let!(:existing_content_hash) { 'ContentHashContentHash' }
+    let!(:parsed_content_hash) { 'NewContentHash' }
+    let!(:latest_dimension_item_fr) do
+      create(:dimensions_item,
+        content_id: content_id, base_path: base_path, locale: 'fr',
+        raw_json: nil, content_hash: existing_content_hash)
+    end
+    let!(:latest_dimension_item) do
+      create(:dimensions_item,
+        content_id: content_id, base_path: base_path, locale: 'en',
+        raw_json: nil, content_hash: existing_content_hash)
+    end
+    let!(:older_dimension_item) { create(:dimensions_item, content_id: content_id, base_path: base_path, latest: false, raw_json: nil) }
+    let(:raw_json) { { 'details' => 'the-json' } }
+
+    before do
+      allow(subject.items_service).to receive(:fetch_raw_json).and_return(raw_json)
+      allow(Items::Jobs::ImportQualityMetricsJob).to receive(:perform_async)
+      allow(Item::Metadata::Parser).to receive(:parse).and_return(
+        raw_json: raw_json,
+        number_of_pdfs: 99,
+        number_of_word_files: 94,
+        content_id: '09hjasdfoj234',
+        locale: locale,
+        title: 'A guide to coding',
+        document_type: 'answer',
+        content_purpose_document_supertype: 'guide',
+        first_published_at: Time.new(2012, 10, 3, 13, 19, 55),
+        public_updated_at: Time.new(2015, 6, 3, 11, 13, 44),
+        primary_organisation_title: 'Home Office',
+        primary_organisation_content_id: 'cont-id-1',
+        primary_organisation_withdrawn: false,
+        content_hash: parsed_content_hash,
+      )
+    end
+
+    it 'leaves the older versions alone' do
+      subject.run
+
+      expect(older_dimension_item.reload.raw_json).to eq nil
+    end
+
+    it 'updates the latest item attributes' do
+      subject.run
+
+      expect(latest_dimension_item.reload).to have_attributes(
+        raw_json: raw_json,
+        number_of_pdfs: 99,
+        number_of_word_files: 94,
+        content_id: '09hjasdfoj234',
+        title: 'A guide to coding',
+        document_type: 'answer',
+        content_purpose_document_supertype: 'guide',
+        first_published_at: Time.new(2012, 10, 3, 13, 19, 55),
+        public_updated_at: Time.new(2015, 6, 3, 11, 13, 44),
+        primary_organisation_title: 'Home Office',
+        primary_organisation_content_id: 'cont-id-1',
+        primary_organisation_withdrawn: false,
+        content_hash: parsed_content_hash,
+      )
+    end
+
+    context "when the locale is 'en'" do
+      let(:locale) { 'en' }
+
+      context 'and the content has changed' do
+        let(:parsed_content_hash) { 'ADifferentContentHash' }
+        it 'triggers a quality metrics job' do
+          subject.run
+          expect(Items::Jobs::ImportQualityMetricsJob).to have_received(:perform_async).with(latest_dimension_item.id)
+          expect(Items::Jobs::ImportQualityMetricsJob).not_to have_received(:perform_async).with(latest_dimension_item_fr.id)
+        end
+      end
+
+      context 'and the content has not changed' do
+        let(:parsed_content_hash) { existing_content_hash }
+        it "doesn't run a quality metrics job" do
+          subject.run
+          expect(Items::Jobs::ImportQualityMetricsJob).not_to have_received(:perform_async)
+        end
+      end
+    end
+
+    context "when the locale is not 'en' and the content has changed" do
+      let(:locale) { 'fr' }
+      let(:parsed_content_hash) { 'ADifferentContentHash' }
+      it "doesn't run a quality metrics job" do
+        subject.run
+        expect(Items::Jobs::ImportQualityMetricsJob).not_to have_received(:perform_async).with(latest_dimension_item.id)
+        expect(Items::Jobs::ImportQualityMetricsJob).not_to have_received(:perform_async).with(latest_dimension_item_fr.id)
+      end
+    end
+  end
+
+  context 'when GdsApi::HTTPGone is raised' do
+    subject { Items::Importers::ContentDetails.new(existing_content_item.id) }
+    let(:locale) { 'en' }
+    let!(:existing_content_item) { create :dimensions_item, content_id: content_id, status: 'something', locale: locale }
+
+    before :each do
+      expect(subject.items_service).to receive(:fetch_raw_json).and_raise(GdsApi::HTTPGone.new(410))
+
+      subject.run
+    end
+
+    it 'should set the status to gone' do
+      expect(existing_content_item.reload.status).to eq('gone')
+    end
+  end
+
+  context 'when GdsApi::HTTPNotFound is raised' do
+    subject { Items::Importers::ContentDetails.new(existing_content_item.id) }
+    let(:locale) { 'en' }
+    let!(:existing_content_item) do
+      create :dimensions_item, content_id: content_id, status: 'something', raw_json: { existing: 'content' }
+    end
+
+    before :each do
+      expect(subject.items_service).to receive(:fetch_raw_json).and_raise(GdsApi::HTTPNotFound.new(404))
+
+      subject.run
+    end
+
+    it 'should set leave everything alone' do
+      existing_content_item.reload
+      expect(existing_content_item.status).to eq('something')
+      expect(existing_content_item.raw_json).to eq('existing' => 'content')
+    end
+  end
+end
---
+++ spec/etl/items/importers/quality_metrics_spec.rb
@@ -0,0 +1,55 @@
+RSpec.describe Items::Importers::QualityMetrics do
+  let(:dimensions_item) { create :dimensions_item }
+  subject { Items::Importers::QualityMetrics.new(dimensions_item.id) }
+
+  context 'import quality metrics' do
+    before do
+      allow_any_instance_of(Dimensions::Item).to receive(:get_content).and_return('the-entire-body')
+      allow(subject.content_quality_service).to receive(:run).with('the-entire-body').and_return(
+        readability_score: 1,
+        contractions_count: 2,
+        equality_count: 3,
+        indefinite_article_count: 4,
+        passive_count: 5,
+        profanities_count: 6,
+        redundant_acronyms_count: 7,
+        repeated_words_count: 8,
+        simplify_count: 9,
+        spell_count: 10
+      )
+    end
+
+    it 'updates the metadata fields for dimensions item' do
+      subject.run
+      expect(dimensions_item.reload).to have_attributes(
+        spell_count: 10,
+        readability_score: 1,
+        contractions_count: 2,
+        equality_count: 3,
+        passive_count: 5,
+        indefinite_article_count: 4,
+        profanities_count: 6,
+        redundant_acronyms_count: 7,
+        repeated_words_count: 8,
+        simplify_count: 9,
+      )
+    end
+
+    it 'does not update fields if InvalidSchemaError is raised' do
+      allow_any_instance_of(Dimensions::Item).to receive(:get_content).and_raise(InvalidSchemaError)
+      subject.run
+      expect(dimensions_item.reload).to have_attributes(
+        spell_count: nil,
+        readability_score: nil,
+        contractions_count: nil,
+        equality_count: nil,
+        passive_count: nil,
+        indefinite_article_count: nil,
+        profanities_count: nil,
+        redundant_acronyms_count: nil,
+        repeated_words_count: nil,
+        simplify_count: nil
+      )
+    end
+  end
+end
