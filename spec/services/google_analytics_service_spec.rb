b57f2a6179a8ec30c70a6406811ab7d55618a8e9
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-04-18 17:01:21 +0100

    Test for find_user_feedback_in_batches

--- spec/services/google_analytics_service_spec.rb
+++ spec/services/google_analytics_service_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe GA::Service do
       end
     end
 
-    context 'when called with a block' do
+    context 'when #find_in_batches is called with a block' do
       it 'should yield successive report data' do
         arg1 = [
           a_hash_including(
@@ -54,6 +54,52 @@ RSpec.describe GA::Service do
           .to yield_successive_args(arg1, arg2)
       end
     end
+
+    describe "#find_user_feedback_in_batches" do
+      let(:date) { Date.new(2018, 2, 20) }
+
+      before do
+        allow(subject.client).to receive(:fetch_all) do
+          [
+            build_report_data(
+              build_report_row(dimensions: %w(/foo ffNoClick), metrics: %w(10))
+            ),
+            build_report_data(
+              build_report_row(dimensions: %w(/foo ffYesClick), metrics: %w(2))
+            ),
+            build_report_data(
+              build_report_row(dimensions: %w(/bar ffNoClick), metrics: %w(3))
+            ),
+            build_report_data(
+              build_report_row(dimensions: %w(/bar ffYesClick), metrics: %w(44))
+            ),
+          ]
+        end
+      end
+
+      context 'when #find_in_batches is called with a block' do
+        it 'should yield successive report data' do
+          arg1 = [
+            a_hash_including(
+              'page_path' => '/foo',
+              'is_this_useful_yes' => 2,
+              'is_this_useful_no' => 10,
+              'date' => '2018-02-20',
+            ),
+          ]
+          arg2 = [
+            a_hash_including(
+              'page_path' => '/cool',
+              'is_this_useful_yes' => 44,
+              'is_this_useful_no' => 3,
+              'date' => '2018-02-20',
+            )
+          ]
+
+          expect { |probe| subject.find_in_batches(date: date, batch_size: 2, &probe) }
+            .to yield_successive_args(arg1, arg2)
+        end
+      end
 
     private
 
