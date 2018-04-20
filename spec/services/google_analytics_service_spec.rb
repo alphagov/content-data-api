0b6944f00214f26c22bbe41e3b5b75a462aa99da
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-04-20 13:49:48 +0100

    Add logic to fetch and format user feedback data

--- spec/services/google_analytics_service_spec.rb
+++ spec/services/google_analytics_service_spec.rb
@@ -54,68 +54,68 @@ RSpec.describe GA::Service do
           .to yield_successive_args(arg1, arg2)
       end
     end
+  end
 
-    describe "#find_user_feedback_in_batches" do
-      let(:date) { Date.new(2018, 2, 20) }
+  describe "#find_user_feedback_in_batches" do
+    let(:date) { Date.new(2018, 2, 20) }
 
-      before do
-        allow(subject.client).to receive(:fetch_all) do
-          [
-            build_report_data(
-              build_report_row(dimensions: %w(/foo ffNoClick), metrics: %w(10))
-            ),
-            build_report_data(
-              build_report_row(dimensions: %w(/foo ffYesClick), metrics: %w(2))
-            ),
-            build_report_data(
-              build_report_row(dimensions: %w(/bar ffNoClick), metrics: %w(3))
-            ),
-            build_report_data(
-              build_report_row(dimensions: %w(/bar ffYesClick), metrics: %w(44))
-            ),
-          ]
-        end
+    before do
+      allow(subject.client).to receive(:fetch_all) do
+        [
+          build_report_data(
+            build_report_row(dimensions: %w(/foo ffNoClick), metrics: %w(10))
+          ),
+          build_report_data(
+            build_report_row(dimensions: %w(/foo ffYesClick), metrics: %w(2))
+          ),
+          build_report_data(
+            build_report_row(dimensions: %w(/bar ffNoClick), metrics: %w(3))
+          ),
+          build_report_data(
+            build_report_row(dimensions: %w(/bar ffYesClick), metrics: %w(44))
+          ),
+        ]
       end
-
-      context 'when #find_in_batches is called with a block' do
-        it 'should yield successive report data' do
-          arg1 = [
-            a_hash_including(
-              'page_path' => '/foo',
-              'is_this_useful_yes' => 2,
-              'is_this_useful_no' => 10,
-              'date' => '2018-02-20',
-            ),
-          ]
-          arg2 = [
-            a_hash_including(
-              'page_path' => '/cool',
-              'is_this_useful_yes' => 44,
-              'is_this_useful_no' => 3,
-              'date' => '2018-02-20',
-            )
-          ]
-
-          expect { |probe| subject.find_in_batches(date: date, batch_size: 2, &probe) }
-            .to yield_successive_args(arg1, arg2)
-        end
-      end
-
-    private
-
-    def build_report_data(*report_rows)
-      Google::Apis::AnalyticsreportingV4::ReportData.new(rows: report_rows)
     end
 
-    def build_report_row(dimensions:, metrics:)
-      Google::Apis::AnalyticsreportingV4::ReportRow.new(
-        dimensions: dimensions,
-        metrics: metrics.map do |metric|
-          Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
-            values: Array(metric)
+    context 'when #find_in_batches is called with a block' do
+      it 'should yield successive report data' do
+        arg1 = [
+          a_hash_including(
+            'page_path' => '/foo',
+            'is_this_useful_yes' => 2,
+            'is_this_useful_no' => 10,
+            'date' => '2018-02-20',
+          ),
+        ]
+        arg2 = [
+          a_hash_including(
+            'page_path' => '/bar',
+            'is_this_useful_yes' => 44,
+            'is_this_useful_no' => 3,
+            'date' => '2018-02-20',
           )
-        end
-      )
+        ]
+        expect { |probe| subject.find_user_feedback_in_batches(date: date, batch_size: 1, &probe) }
+          .to yield_successive_args(arg1, arg2)
+      end
     end
+  end
+
+  private
+
+  def build_report_data(*report_rows)
+    Google::Apis::AnalyticsreportingV4::ReportData.new(rows: report_rows)
+  end
+
+  def build_report_row(dimensions:, metrics:)
+    Google::Apis::AnalyticsreportingV4::ReportRow.new(
+      dimensions: dimensions,
+      metrics: metrics.map do |metric|
+        Google::Apis::AnalyticsreportingV4::DateRangeValues.new(
+          values: Array(metric)
+        )
+      end
+    )
   end
 end
