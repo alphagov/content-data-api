560f008aaed87f7cc6ab7cb049f3009756a04d5f
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-04-25 11:29:52 +0100

    Fix 'Taxon' schema
    Handle cases where 'child_taxons' key is not found.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -199,14 +199,23 @@ RSpec.describe Item::Content::Parser do
         expect(subject.extract_content(json.deep_stringify_keys)).to eq("Announcement 25 December 2017 closed")
       end
 
-      it "returns content json if schema_name is 'taxon'" do
-        json = { schema_name: "taxon",
-          description: "Blogs",
-          links: { child_taxons: [
-            { title: "One", description: "first" },
-            { title: "Two", description: "second" }
-          ] } }
-        expect(subject.extract_content(json.deep_stringify_keys)).to eq("Blogs One first Two second")
+      describe "Taxon" do
+        it "returns description if json does not have any child_taxons" do
+          json = { schema_name: "taxon",
+            description: "Blogs",
+            links: {} }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Blogs")
+        end
+
+        it "returns content json if schema_name is 'taxon'" do
+          json = { schema_name: "taxon",
+            description: "Blogs",
+            links: { child_taxons: [
+              { title: "One", description: "first" },
+              { title: "Two", description: "second" }
+            ] } }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Blogs One first Two second")
+        end
       end
 
       describe "ServiceManualStandard" do
