41e4ae72c36b0b5661e2b677be8667b9befce7e9
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-04-25 11:18:36 +0100

    Fix 'ServiceManualServiceToolkit' schema
    Handle cases where 'collections' or 'links' keys are not found.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -206,23 +206,48 @@ RSpec.describe Item::Content::Parser do
         expect(subject.extract_content(json.deep_stringify_keys)).to eq('sm title the main body ch1 title ch1 desc ch2 title ch2 desc')
       end
 
-      it "returns content if schema name is 'service_manual_service_toolkit'" do
-        json = {
-          schema_name: 'service_manual_service_toolkit',
-          details: {
-            collections: [
-              {
-                title: 'main title 1',
-                description: 'main desc 1',
-                links: [
-                  { title: 'title link 1', description: 'desc link 1' },
-                  { title: 'title link 2', description: 'desc link 2' }
-                ]
-              }
-            ]
+      describe "ServiceManualServiceToolkit" do
+        it "returns nil if json does not have 'collection' key" do
+          json = {
+            schema_name: 'service_manual_service_toolkit',
+            details: {}
           }
-        }
-        expect(subject.extract_content(json.deep_stringify_keys)).to eq('main title 1 main desc 1 title link 1 desc link 1 title link 2 desc link 2')
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq(nil)
+        end
+
+        it "returns 'title' and 'description' if json does not have 'links' key" do
+          json = {
+            schema_name: 'service_manual_service_toolkit',
+            details: {
+              collections: [
+                {
+                  title: 'main title 1',
+                  description: 'main desc 1'
+                }
+              ]
+            }
+          }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq('main title 1 main desc 1')
+        end
+
+        it "returns json content" do
+          json = {
+            schema_name: 'service_manual_service_toolkit',
+            details: {
+              collections: [
+                {
+                  title: 'main title 1',
+                  description: 'main desc 1',
+                  links: [
+                    { title: 'title link 1', description: 'desc link 1' },
+                    { title: 'title link 2', description: 'desc link 2' }
+                  ]
+                }
+              ]
+            }
+          }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq('main title 1 main desc 1 title link 1 desc link 1 title link 2 desc link 2')
+        end
       end
 
       describe "Contact" do
