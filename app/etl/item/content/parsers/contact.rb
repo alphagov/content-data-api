15be4c11d1ed4ffcc4d4818609a0c74ed95d9f32
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-04-25 10:49:17 +0100

    Fix 'Contact' schema
    Handle cases where 'phone_numbers', 'post_addresses' or 'email_addresses' keys are not found.

--- app/etl/item/content/parsers/contact.rb
+++ app/etl/item/content/parsers/contact.rb
@@ -2,9 +2,9 @@ class Item::Content::Parsers::Contact
   def parse(json)
     html = []
     html << json.dig('details', 'description')
-    html << get_email(json)
-    html << get_postal(json)
-    html << get_phone(json)
+    html << get_email(json) unless get_email(json).nil?
+    html << get_postal(json) unless get_postal(json).nil?
+    html << get_phone(json) unless get_phone(json).nil?
     html.join(" ")
   end
 
@@ -16,31 +16,40 @@ private
 
   def get_phone(json)
     html = []
-    json.dig('details', 'phone_numbers').each do |phone|
-      html << phone['title']
-      html << phone['description']
+    phones = json.dig('details', 'phone_numbers')
+    unless phones.nil?
+      phones.each do |phone|
+        html << phone['title']
+        html << phone['description']
+      end
     end
-    html << json.dig('details', 'more_info_phone_number')
-    html
+    more_info = json.dig('details', 'more_info_phone_number')
+    html << more_info unless more_info.nil?
   end
 
   def get_postal(json)
     html = []
-    json.dig('details', 'post_addresses').each do |addr|
-      html << addr['title']
-      html << addr['description']
+    postal = json.dig('details', 'post_addresses')
+    unless postal.nil?
+      postal.each do |addr|
+        html << addr['title']
+        html << addr['description']
+      end
     end
-    html << json.dig('details', 'more_info_post_address')
-    html
+    more_info = json.dig('details', 'more_info_post_address')
+    html << more_info unless more_info.nil?
   end
 
   def get_email(json)
     html = []
-    json.dig('details', 'email_addresses').each do |addr|
-      html << addr['title']
-      html << addr['description']
+    emails = json.dig('details', 'email_addresses')
+    unless emails.nil?
+      emails.each do |addr|
+        html << addr['title']
+        html << addr['description']
+      end
     end
-    html << json.dig('details', 'more_info_email_address')
-    html
+    more_info = json.dig('details', 'more_info_email_address')
+    html << more_info unless more_info.nil?
   end
 end
