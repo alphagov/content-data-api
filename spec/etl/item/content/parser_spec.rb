3ad22bb91dfff89047f422976950c466601ce530
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-04-25 11:27:19 +0100

    Fix 'FinderEmailSignup' schema
    Handle cases where 'email_signup_choice' key is not found.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -137,15 +137,24 @@ RSpec.describe Item::Content::Parser do
         end
       end
 
-      it "returns content json if schema_name is 'finder_email_signup'" do
-        json = { schema_name: "finder_email_signup",
-          description: "Use buttons",
-          details: { email_signup_choice:
-            [
-              { radio_button_name: "Yes" },
-              { radio_button_name: "No" }
-            ] } }
-        expect(subject.extract_content(json.deep_stringify_keys)).to eq("Yes No Use buttons")
+      describe "FinderEmailSignup" do
+        it "returns description if json does not have 'email_signup_choice' key" do
+          json = { schema_name: "finder_email_signup",
+            description: "Use buttons",
+            details: {} }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Use buttons")
+        end
+
+        it "returns content json" do
+          json = { schema_name: "finder_email_signup",
+            description: "Use buttons",
+            details: { email_signup_choice:
+              [
+                { radio_button_name: "Yes" },
+                { radio_button_name: "No" }
+              ] } }
+          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Yes No Use buttons")
+        end
       end
 
       it "returns content json if schema_name is 'location_transaction'" do
