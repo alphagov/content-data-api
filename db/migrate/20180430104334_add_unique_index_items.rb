2da631a42181c53c4d93f5a010b9bd67a98d5042
Pablo Manrubia <pmanrubia@gmail.com>
2018-04-30 21:01:15 +0100

    Use partial unique index for unique base_paths
    We could have multiple base_paths when the `latest` attribute is false, 
    but only one when `latest` is true.
    
    This index guarantees that our database is consistent with the way we
    model the data. I have decided to add a constraint in the database as 
    the performance of the operations is much faster, and it is likely that
    other processes that might not depend on AR, perform operations on the 
    database creating situations with inaccurate data that are not easy to 
    detect.
    
    For example, we could insert `pageviews` to the same `base_path` multiple
    times making all calculations to be inaccurate.

--- db/migrate/20180430104334_add_unique_index_items.rb
+++ db/migrate/20180430104334_add_unique_index_items.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexItems < ActiveRecord::Migration[5.1]
   def change
-    add_index :dimensions_items, %i(latest base_path), unique: true
+    add_index :dimensions_items, %i(latest base_path), unique: true, where: "latest = 'true'"
   end
 end
