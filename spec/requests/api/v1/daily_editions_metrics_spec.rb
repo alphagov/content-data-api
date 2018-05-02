4caa6c5c00c9c11db69b4caaac4c7491aa9e3748
Pablo Manrubia <pmanrubia@gmail.com>
2018-05-02 11:41:29 +0100

    Add readability score to API test

--- spec/requests/api/v1/daily_editions_metrics_spec.rb
+++ spec/requests/api/v1/daily_editions_metrics_spec.rb
@@ -11,16 +11,16 @@ RSpec.describe '/api/v1/metrics/', type: :request do
   let!(:content_id) { SecureRandom.uuid }
 
   let!(:item) { create :dimensions_item, content_id: content_id, locale: 'en' }
-  let!(:item_fr) { create :dimensions_item, content_id: content_id, locale: 'de' }
+  let!(:item_non_english) { create :dimensions_item, content_id: content_id, locale: 'de' }
 
   before do
     item1_old = create :dimensions_item,
-      content_id: content_id, locale: 'en', number_of_pdfs: 30, number_of_word_files: 30, latest: false
-    item.update number_of_pdfs: 20, number_of_word_files: 20
-    item_fr.update number_of_pdfs: 200, number_of_word_files: 100
+      content_id: content_id, locale: 'en', number_of_pdfs: 30, number_of_word_files: 30, readability_score: 123, latest: false
+    item.update number_of_pdfs: 20, number_of_word_files: 20, readability_score: 5
+    item_non_english.update number_of_pdfs: 200, number_of_word_files: 100
 
     create :metric, dimensions_item: item1_old, dimensions_date: day1
-    create :metric, dimensions_item: item_fr, dimensions_date: day2, pageviews: 10, feedex_comments: 10
+    create :metric, dimensions_item: item_non_english, dimensions_date: day2, pageviews: 10, feedex_comments: 10
     create :metric, dimensions_item: item, dimensions_date: day2
     create :metric, dimensions_item: item, dimensions_date: day3
     create :metric, dimensions_item: item, dimensions_date: day4
@@ -38,6 +38,19 @@ RSpec.describe '/api/v1/metrics/', type: :request do
 
     json = JSON.parse(response.body)
     expect(json.deep_symbolize_keys).to eq(api_reponse('number_of_word_files'))
+  end
+
+  it 'returns the `readability score` between two dates' do
+    get "/api/v1/metrics/readability_score/#{content_id}/time-series", params: { from: '2018-01-13', to: '2018-01-15' }
+
+    json = JSON.parse(response.body)
+    expect(json.deep_symbolize_keys).to eq({
+      readability_score: [
+        { date: '2018-01-13', value: 123 },
+        { date: '2018-01-14', value: 5 },
+        { date: '2018-01-15', value: 5 },
+      ]
+    })
   end
 
   def api_reponse(metric_name)
