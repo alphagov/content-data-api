525db4f17fb3640cbb3584099375caa18765b8f5
Kyle Macpherson <kyle.macpherson@digital.cabinet-office.gov.uk>
2018-05-09 15:01:47 +0100

    Add logic to delete all ga events in ETL process

--- app/etl/ga/views_processor.rb
+++ app/etl/ga/views_processor.rb
@@ -38,7 +38,7 @@ private
     date_to_s = date.strftime("%F")
 
     conn.execute(load_metrics_query(date_to_s))
-    conn.execute(clean_up_query)
+    clean_up_events!
   end
 
   def load_metrics_query(date_to_s)
@@ -58,19 +58,8 @@ private
     SQL
   end
 
-  def clean_up_query
-    date_to_s = date.strftime("%F")
-    <<~SQL
-      DELETE FROM events_gas
-      WHERE date = '#{date_to_s}' AND
-            process_name = #{Events::GA.process_names['views']} AND
-        page_path in (
-           SELECT base_path
-           FROM dimensions_items, facts_metrics
-           WHERE dimensions_items.id = facts_metrics.dimensions_item_id
-           AND facts_metrics.dimensions_date_id = '#{date_to_s}'
-        )
-    SQL
+  def clean_up_events!
+    Events::GA.delete_all
   end
 
   attr_reader :date
