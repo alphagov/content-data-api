d1bb530571f47c665dc5246b0a63df54148fb28f
chao-xian <gds@kelvingan.co.uk>
2018-05-11 16:34:02 +0100

    Refactor metrics controller to use Metric helper class

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -12,7 +12,7 @@ class Api::MetricsController < Api::BaseController
   end
 
   def index
-    items = Rails.configuration.metrics.map { |k, v| v.merge(metric_id: k) }.sort_by { |item| item[:name] }
+    items = Metric.all_metrics.map { |k, v| v.merge(metric_id: k) }.sort_by { |item| item[:name] }
     render json: items
   end
 
@@ -24,7 +24,7 @@ private
       .by_base_path(format_base_path_param)
       .by_locale('en')
 
-    if facts_metrics.include?(metric)
+    if Metric.is_content_metric?(metric)
       series
         .with_edition_metrics
         .order('dimensions_dates.date asc')
@@ -55,25 +55,5 @@ private
         invalid_params: metric_params.errors.to_hash
       )
     end
-  end
-
-  def facts_metrics
-    %w(
-      number_of_pdfs
-      number_of_word_files
-      readability_score
-      contractions_count
-      equality_count
-      indefinite_article_count
-      passive_count
-      profanities_count
-      redundant_acronyms_count
-      repeated_words_count
-      simplify_count
-      spell_count
-      string_length
-      sentence_count
-      word_count
-    )
   end
 end
