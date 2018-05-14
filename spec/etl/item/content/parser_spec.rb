7bfd56e350ffd6f4107bd910a1a044d342f99cf6
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 11:05:41 +0100

    Extract tests for Need Parser
    The tests for schema_name 'need' have been been moved to separate
    file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -129,18 +129,6 @@ RSpec.describe Item::Content::Parser do
         end
       end
 
-      it "returns content if schema name is 'need'" do
-        json = {
-          schema_name: 'need',
-          details: {
-            role: 'the role',
-            goal: 'the goal',
-            benefit: 'the benefit'
-          }
-        }
-        expect(subject.extract_content(json.deep_stringify_keys)).to eq('the role the goal the benefit')
-      end
-
       describe "TravelAdviceIndex" do
         it "returns nil if json does not have children array" do
           json = { schema_name: "travel_advice_index",
