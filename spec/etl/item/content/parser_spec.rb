3147d3e778364ca6b562af95171b8f32525af4a6
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 10:08:33 +0100

    Extract tests for FinderEmailSignup Parser
    The tests for schema_name 'finder_email_signup' have been moved
    to a separate file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -3,26 +3,6 @@ RSpec.describe Item::Content::Parser do
 
   describe "#extract_content" do
     context "when valid schema" do
-      describe "FinderEmailSignup" do
-        it "returns description if json does not have 'email_signup_choice' key" do
-          json = { schema_name: "finder_email_signup",
-            description: "Use buttons",
-            details: {} }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Use buttons")
-        end
-
-        it "returns content json" do
-          json = { schema_name: "finder_email_signup",
-            description: "Use buttons",
-            details: { email_signup_choice:
-              [
-                { radio_button_name: "Yes" },
-                { radio_button_name: "No" }
-              ] } }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Yes No Use buttons")
-        end
-      end
-
       it "returns content json if schema_name is 'local_transaction'" do
         json = { schema_name: "local_transaction",
           details: { introduction: "Greetings", need_to_know: "A Name",
