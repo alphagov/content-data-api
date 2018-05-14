8c282415f8d222debff02dd1ff78a8b2da636028
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 09:48:59 +0100

    Extract tests for BodyContent Parser
    The tests for BodyContent have now been moved to a separate file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -3,54 +3,6 @@ RSpec.describe Item::Content::Parser do
 
   describe "#extract_content" do
     context "when valid schema" do
-      it 'returns content json from :body for all valid formats' do
-        valid_types = %w[
-          answer
-          calendar
-          case_study
-          consultation
-          corporate_information_page
-          detailed_guide
-          document_collection
-          fatality_notice
-          help_page
-          hmrc_manual_section
-          html_publication
-          manual
-          manual_section
-          news_article
-          organisation
-          publication
-          service_manual_guide
-          simple_smart_answer
-          specialist_document
-          speech
-          statistical_data_set
-          take_part
-          topical_event_about_page
-          working_group
-          world_location_news_article
-        ].freeze
-        valid_types.each do |schema|
-          json = build_raw_json(schema_name: schema, body: "<p>Body for #{schema}</p>")
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Body for #{schema}"), "Incorrect body for schema: '#{schema}'"
-        end
-      end
-
-      it 'returns nil if details.body does NOT exist' do
-        valid_schema_json = { schema_name: 'answer', details: {} }
-        expect(subject.extract_content(valid_schema_json.deep_stringify_keys)).to eq(nil)
-      end
-
-      it 'does not fail with unicode characters' do
-        json = build_raw_json(
-          body: %{\u003cdiv class="govspeak"\u003e\u003cp\u003eLorem ipsum dolor sit amet.},
-          schema_name: 'case_study'
-        )
-
-        expect(subject.extract_content(json.deep_stringify_keys)).to eq('Lorem ipsum dolor sit amet.')
-      end
-
       describe "FinderEmailSignup" do
         it "returns description if json does not have 'email_signup_choice' key" do
           json = { schema_name: "finder_email_signup",
@@ -437,15 +389,6 @@ RSpec.describe Item::Content::Parser do
 
         expected = "Learn to drive a car: step by step It's easy Step 1 Enter vehicle A B C Step 2 Adjust seat Z Y X"
         expect(subject.extract_content(json.deep_stringify_keys)).to eql(expected)
-      end
-
-      def build_raw_json(body:, schema_name:)
-        {
-          schema_name: schema_name,
-          details: {
-            body: body
-          }
-        }
       end
     end
 
