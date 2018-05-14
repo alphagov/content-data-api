4be242c88d99be173699ca8470be63d054185e45
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 10:28:42 +0100

    Extract tests for Finder Parser
    These tests for schema_name 'finder' have been moved to a separate
    file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -321,16 +321,6 @@ RSpec.describe Item::Content::Parser do
         expect(subject.extract_content(json.deep_stringify_keys)).to eql(expected)
       end
 
-      it "returns content json if schema_name is 'finder'" do
-        json = { schema_name: "finder", title: "Contact HMRC",
-          links: { children: [
-            { title: "Personal Tax", description: "Email, write or phone us" },
-            { title: "Child Benefit", description: "Tweet us" }
-          ] } }
-        expected = "Contact HMRC Personal Tax Email, write or phone us Child Benefit Tweet us"
-        expect(subject.extract_content(json.deep_stringify_keys)).to eql(expected)
-      end
-
       it "returns content json if schema_name is 'mainstream_browse_page'" do
         json = { schema_name: "mainstream_browse_page", title: "Travel Abroad",
           description: "Go abroad",
