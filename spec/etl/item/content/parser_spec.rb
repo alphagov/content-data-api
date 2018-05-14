6ec7657f33483b68d2c08911f4d096c16b1102d3
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 11:30:33 +0100

    Extract tests for StepByStep Parser
    The tests for schema_name 'step_by_step_nav' have been moved to a
    separate file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -171,19 +171,6 @@ RSpec.describe Item::Content::Parser do
         expected = "Service Manual Digital Service Standard Design Naming your service Technology Security and Maintenance"
         expect(subject.extract_content(json.deep_stringify_keys)).to eql(expected)
       end
-
-      it "returns content json if schema_name is 'step_by_step_nav'" do
-        json = { schema_name: "step_by_step_nav", details: { step_by_step_nav: {
-          title: "Learn to drive a car: step by step", introduction: "It's easy",
-          steps: [{ title: "Step 1",
-            contents: [{ text: "Enter vehicle" }, contents: [{ text: "A" }, { text: "B" }, { text: "C" }]] },
-                  { title: "Step 2",
-              contents: [{ text: "Adjust seat" }, contents: [{ text: "Z" }, { text: "Y" }, { text: "X" }]] }]
-          } } }
-
-        expected = "Learn to drive a car: step by step It's easy Step 1 Enter vehicle A B C Step 2 Adjust seat Z Y X"
-        expect(subject.extract_content(json.deep_stringify_keys)).to eql(expected)
-      end
     end
 
     context "when invalid schema" do
