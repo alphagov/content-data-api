de04677899ef8648ec964fde19c278714bd506e5
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 12:12:32 +0100

    Extract tests for ServiceManualTopic Parser
    The tests for schema_name 'service_manual_topic' have been moved
    to a separate file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -3,28 +3,6 @@ RSpec.describe Item::Content::Parser do
 
   describe "#extract_content" do
     context "when valid schema" do
-      describe "ServiceManualTopic" do
-        it "returns description if json does not have 'groups' key" do
-          json = { schema_name: "service_manual_topic",
-            description: "Blogs",
-            details: {} }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Blogs")
-        end
-
-        it "returns content json" do
-          json = { schema_name: "service_manual_topic",
-            description: "Blogs",
-            details: { groups:
-              [
-                { name: "Design",
-                  description: "thinking" },
-                { name: "Performance",
-                  description: "analysis" }
-              ] } }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq("Blogs Design thinking Performance analysis")
-        end
-      end
-
       it "returns content json if schema_name is 'service_sign_in'" do
         json = { schema_name: "service_sign_in",
           details: {
