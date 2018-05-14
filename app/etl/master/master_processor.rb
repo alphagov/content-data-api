4d2a53420a85ee866c6591312c0197251d92dcfb
Pablo Manrubia <pmanrubia@gmail.com>
2018-05-14 11:19:02 +0100

    Remove unused code in Master processor

--- app/etl/master/master_processor.rb
+++ app/etl/master/master_processor.rb
@@ -12,19 +12,15 @@ class Master::MasterProcessor
   def process
     raise DuplicateDateError if Dimensions::Date.exists?(date)
     time(process: :master) do
-      Master::MetricsProcessor.process(date: dimensions_date.date)
-      GA::ViewsProcessor.process(date: dimensions_date.date)
-      GA::UserFeedbackProcessor.process(date: dimensions_date.date)
-      GA::InternalSearchProcessor.process(date: dimensions_date.date)
-      Feedex::Processor.process(date: dimensions_date.date)
+      Master::MetricsProcessor.process(date: date)
+      GA::ViewsProcessor.process(date: date)
+      GA::UserFeedbackProcessor.process(date: date)
+      GA::InternalSearchProcessor.process(date: date)
+      Feedex::Processor.process(date: date)
     end
   end
 
 private
-
-  def dimensions_date
-    @dimensions_date ||= Dimensions::Date.for(date)
-  end
 
   attr_reader :date
 
