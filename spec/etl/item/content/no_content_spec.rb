4a0d42421c7e695249dc905cb5956cd40afaead9
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 16:02:18 +0100

    Add schemas to ignore
    There are a number of schemas that we do not want to extract data
    from as they will not give us any useful content for quality metrics.
    
    This commit adds the following schemas to the list that we do not
    want to extract content from: organisations_homepage, topic,
    world_location, placeholder_organisation, placeholder_world_location,
    placeholder_working_group, placeholder_topical_event,
    placeholder_policy_area and placeholder_ministerial_role.
    
    Now that we have added these schemas we should not be seeing any
    InvalidSchemaErrors for any of these schemas.

--- spec/etl/item/content/no_content_spec.rb
+++ spec/etl/item/content/no_content_spec.rb
@@ -8,14 +8,23 @@ RSpec.describe Item::Content::Parser do
       external_content
       generic
       homepage
+      organisations_homepage
       person
       placeholder_corporate_information_page
+      placeholder_ministerial_role
+      placeholder_organisation
+      placeholder_policy_area
+      placeholder_topical_event
+      placeholer_working_group
+      placeholder_world_location
       placehold_worldwide_organisation
       placeholder_person
       placeholder
       policy
-      special_route
       redirect
+      special_route
+      topic
+      world_location
       vanish
     ]
     no_content_schemas.each do |schema|
