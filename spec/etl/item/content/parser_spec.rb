efa102cb5840ce01a3436720a9df00c182ea3c24
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-14 12:02:02 +0100

    Extract tests for ServiceManualServiceToolkit Parser
    The tests for schema_name 'service_manual_service_toolkit' have
    been moved to a separate file.

--- spec/etl/item/content/parser_spec.rb
+++ spec/etl/item/content/parser_spec.rb
@@ -32,50 +32,6 @@ RSpec.describe Item::Content::Parser do
         end
       end
 
-      describe "ServiceManualServiceToolkit" do
-        it "returns nil if json does not have 'collection' key" do
-          json = {
-            schema_name: 'service_manual_service_toolkit',
-            details: {}
-          }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq(nil)
-        end
-
-        it "returns 'title' and 'description' if json does not have 'links' key" do
-          json = {
-            schema_name: 'service_manual_service_toolkit',
-            details: {
-              collections: [
-                {
-                  title: 'main title 1',
-                  description: 'main desc 1'
-                }
-              ]
-            }
-          }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq('main title 1 main desc 1')
-        end
-
-        it "returns json content" do
-          json = {
-            schema_name: 'service_manual_service_toolkit',
-            details: {
-              collections: [
-                {
-                  title: 'main title 1',
-                  description: 'main desc 1',
-                  links: [
-                    { title: 'title link 1', description: 'desc link 1' },
-                    { title: 'title link 2', description: 'desc link 2' }
-                  ]
-                }
-              ]
-            }
-          }
-          expect(subject.extract_content(json.deep_stringify_keys)).to eq('main title 1 main desc 1 title link 1 desc link 1 title link 2 desc link 2')
-        end
-      end
-
       it "returns content json if schema_name is 'service_sign_in'" do
         json = { schema_name: "service_sign_in",
           details: {
