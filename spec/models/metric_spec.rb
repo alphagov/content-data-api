bcb9414684445dd516a9f403cd1ed1605f42aebb
Pablo Manrubia <pmanrubia@gmail.com>
2018-05-18 15:28:15 +0100

    DRY-up metric_spec.rb

--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -1,83 +1,80 @@
 RSpec.describe Metric do
-  let(:content_metric) { "word_count" }
-  let(:performance_metric) { "pageviews" }
+  EDITION_METRICS =
+    %w(
+      contractions_count
+      equality_count
+      indefinite_article_count
+      number_of_pdfs
+      number_of_word_files
+      passive_count
+      profanities_count
+      readability_score
+      redundant_acronyms_count
+      repeated_words_count
+      sentence_count
+      simplify_count
+      spell_count
+      string_length
+      word_count
+    ).freeze
 
-  it "returns a list of all metrics" do
-    metrics = Metric.all_metrics
+  DAILY_METRICS =
+    %w(
+      avg_time_on_page
+      bounce_rate
+      entrances
+      exits
+      feedex_comments
+      is_this_useful_no
+      is_this_useful_yes
+      number_of_internal_searches
+      pageviews
+      unique_pageviews
+    ).freeze
 
-    expect(metrics.length).to eq(25)
-    a_metric = metrics.first
-    expect(a_metric).to be_an_instance_of(Metric)
+  describe '.all_metrics' do
+    it "returns a list of all metrics" do
+      metrics = Metric.all_metrics
+
+      expect(metrics.length).to eq(25)
+      a_metric = metrics.first
+      expect(a_metric).to be_an_instance_of(Metric)
+    end
   end
 
-  describe '.edition_metrics' do
-    it 'return the edition metrics' do
-      metrics = Metric.edition_metrics
-      expect(metrics.map(&:name)).to match_array(%w(
-        number_of_pdfs
-        number_of_word_files
-        readability_score
-        contractions_count
-        equality_count
-        indefinite_article_count
-        passive_count
-        profanities_count
-        redundant_acronyms_count
-        repeated_words_count
-        simplify_count
-        spell_count
-        string_length
-        sentence_count
-        word_count
-      ))
+  describe '.metric_names' do
+    it 'return all metric names' do
+      names = Metric.metric_names
+
+      expect(names).to match_array(EDITION_METRICS + DAILY_METRICS)
     end
   end
 
   describe '.is_edition_metric?' do
-    %w(
-      number_of_pdfs
-      number_of_word_files
-      readability_score
-      contractions_count
-      equality_count
-      indefinite_article_count
-      passive_count
-      profanities_count
-      redundant_acronyms_count
-      repeated_words_count
-      simplify_count
-      spell_count
-      string_length
-      sentence_count
-      word_count
-    ).each do |metric|
+    EDITION_METRICS.each do |metric|
       it "returns true for #{metric}" do
         expect(Metric.is_edition_metric?(metric)).to be_truthy
       end
     end
 
-    %w(
-    pageviews
-    unique_pageviews
-    feedex_comments
-    is_this_useful_no
-    is_this_useful_yes
-    number_of_internal_searches
-    entrances
-    exits
-    bounce_rate
-    avg_time_on_page
-    ).each do |metric|
+    DAILY_METRICS.each do |metric|
       it "returns false for #{metric}" do
         expect(Metric.is_edition_metric?(metric)).to be_falsey
       end
     end
   end
 
-  it "returns a list of content metrics" do
-    metrics = Metric.edition_metrics_names
+  describe '.edition_metrics_names' do
+    it 'return the edition metrics names' do
+      names = Metric.edition_metrics.map(&:name)
+      expect(names).to match_array(EDITION_METRICS)
+    end
+  end
 
-    expect(metrics).to include(content_metric)
-    expect(metrics).to_not include(performance_metric)
+  describe '.edition_metrics' do
+    it 'return the edition metrics' do
+      metrics = Metric.edition_metrics
+      expect(metrics.map(&:name)).to match_array(EDITION_METRICS)
+    end
   end
 end
