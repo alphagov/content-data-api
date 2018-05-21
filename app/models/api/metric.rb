d3c26206a805258e32d68f2c3280378ac7b0379d
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-05-21 12:51:39 +0100

    Update API to filter by base_path
    We are now able to obtain metrics by querying the base_path instead
    of having to know the content_id of the base_path by which to filter.

--- app/models/api/metric.rb
+++ app/models/api/metric.rb
@@ -1,25 +1,23 @@
 class Api::Metric
   DATE_REGEX = /\A\d\d\d\d-\d\d-\d\d\Z/
-  CONTENT_ID_REGEX = /\A[0-9A-F]{8}-[0-9A-F]{4}-[1-5][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}\Z/i
 
   private_constant :DATE_REGEX
-  private_constant :CONTENT_ID_REGEX
 
   include ActiveModel::Validations
 
-  attr_reader :metric, :from, :to, :content_id
+  attr_reader :metric, :from, :to, :content_id, :base_path
 
   validates :metric, presence: true, inclusion: { in: Rails.configuration.valid_metric_names }
   validates :from, presence: true, format: { with: DATE_REGEX, message: "Dates should use the format YYYY-MM-DD" }
   validates :to, presence: true, format: { with: DATE_REGEX, message: "Dates should use the format YYYY-MM-DD" }
-  validates :content_id, presence: true, format: { with: CONTENT_ID_REGEX, message: "Content ID must be a UUID." }
+  validates :base_path, presence: true
   validate :from_before_to, if: :have_a_date_range?
 
   def initialize(params)
     @metric = params[:metric]
     @from = params[:from]
     @to = params[:to]
-    @content_id = params[:content_id]
+    @base_path = params[:base_path]
   end
 
 private
