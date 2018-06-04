75d769e0be1117c89d030e9308a2b57f7fe1f5f9
Mat Moore <matmoore@users.noreply.github.com>
2018-06-04 16:38:12 +0000

    Add preload logic into the bulk import consumer
    Since the preload process is a lot simpler than the regular processing,
    I've separated it from the MessageProcessor class.
    
    Both will need to be refactored as part of handling multipart documents, this
    is just a first step.

--- app/streams/publishing_api/bulk_import_consumer.rb
+++ app/streams/publishing_api/bulk_import_consumer.rb
@@ -1,10 +1,30 @@
 class PublishingAPI::BulkImportConsumer
   def process(message)
-    # TODO: this is just a stubbed consumer to stop Icinga alerts
-    # PublishingApiMessageProcessor.new(message).process
+    content_item = PublishingAPI::ContentItem.parse_message(message.payload)
+    metadata = content_item.metadata
+
+    # When preloading data, we always create an item
+    item = Dimensions::Item.create_empty(
+      content_id: content_item.content_id,
+      base_path: content_item.base_path,
+      locale: content_item.locale,
+      payload_version: content_item.payload_version,
+      raw_json: message,
+      **metadata
+    )
+
+    # Do the rest of the import
+    Items::Importers::ContentDetails.run(item.id, current_date.day, current_date.month, current_date.year)
+
     message.ack
   rescue StandardError => e
     GovukError.notify(e)
     message.discard
+  end
+
+private
+
+  def current_date
+    Time.zone.now.to_date
   end
 end
