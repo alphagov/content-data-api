ab8e64df6bf082952bcea6db5206420ea1ddc6d0
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-04 15:10:22 +0100

    Use module to define the PublishingAPI

--- app/streams/publishing_api/consumer.rb
+++ app/streams/publishing_api/consumer.rb
@@ -1,9 +1,11 @@
-class PublishingAPI::Consumer
-  def process(message)
-    PublishingAPI::MessageProcessor.new(message).process
-    message.ack
-  rescue StandardError => e
-    GovukError.notify(e)
-    message.discard
+module PublishingAPI
+  class Consumer
+    def process(message)
+      MessageProcessor.new(message).process
+      message.ack
+    rescue StandardError => e
+      GovukError.notify(e)
+      message.discard
+    end
   end
 end
