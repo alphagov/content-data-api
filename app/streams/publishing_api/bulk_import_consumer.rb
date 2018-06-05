0569834822f841e13cc6069bde580585ec679961
Mat Moore <matmoore@users.noreply.github.com>
2018-06-05 11:10:50 +0000

    Test the bulk consumer behaviour

--- app/streams/publishing_api/bulk_import_consumer.rb
+++ app/streams/publishing_api/bulk_import_consumer.rb
@@ -4,14 +4,20 @@ class PublishingAPI::BulkImportConsumer
     metadata = content_item.metadata
 
     # When preloading data, we always create an item
-    item = Dimensions::Item.create_empty(
-      content_id: content_item.content_id,
-      base_path: content_item.base_path,
-      locale: content_item.locale,
-      payload_version: content_item.payload_version,
-      raw_json: message,
-      **metadata
-    )
+    begin
+      item = Dimensions::Item.create(
+        latest: true,
+        content_id: content_item.content_id,
+        base_path: content_item.base_path,
+        locale: content_item.locale,
+        publishing_api_payload_version: content_item.payload_version,
+        raw_json: message,
+        **metadata
+      )
+    rescue ActiveRecord::RecordNotUnique
+      logger.info "Ignoring duplicate item with content id #{content_item.content_id}. Payload version is #{content_item.payload_version}"
+      return
+    end
 
     # Do the rest of the import
     Items::Importers::ContentDetails.run(item.id, current_date.day, current_date.month, current_date.year)
