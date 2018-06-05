ea9517ee50ee05a1016756c6a5504eb8383dcd05
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-05 18:07:41 +0100

    Move filtering scopes into the Items dimension
    It seems consistent to have all the scopes related to filtering items
    in the Items dimension.

--- app/models/dimensions/item.rb
+++ app/models/dimensions/item.rb
@@ -4,6 +4,12 @@ class Dimensions::Item < ApplicationRecord
   has_one :facts_edition, class_name: "Facts::Edition", foreign_key: :dimensions_item_id
   validates :content_id, presence: true
 
+  scope :by_base_path, ->(base_path) { where('base_path like (?)', base_path) }
+  scope :by_content_id, ->(content_id) { where(content_id: content_id) }
+  scope :by_organisation_id, ->(organisation_id) { where(primary_organisation_content_id: organisation_id) }
+  scope :by_document_type, ->(document_type) { where('document_type like (?)', document_type) }
+  scope :by_locale, ->(locale) { where(locale: locale) }
+
   def self.by_natural_key(content_id:, locale:)
     find_by(content_id: content_id, locale: locale, latest: true)
   end
