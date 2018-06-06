3fe9747e2cb5c262b2febd23bc578b63a0b0dabb
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-06 23:45:42 +0100

    Extract metadata when transforming the event
    The earlier we transform the message into a Domain Object the better.
    This will allow us to easily change and evolve the application with
    changes in the source.

--- app/streams/publishing_api/content_item.rb
+++ app/streams/publishing_api/content_item.rb
@@ -1,40 +1,37 @@
 class PublishingAPI::ContentItem
-  attr_reader :content_id
-  attr_reader :base_path
-  attr_reader :payload_version
-  attr_reader :locale
-  attr_reader :metadata
-  attr_reader :details
-  attr_reader :links
+  include ActiveModel::Model
+
+  attr_accessor :base_path
+  attr_accessor :content_id
+  attr_accessor :content_purpose_document_supertype
+  attr_accessor :content_purpose_subgroup
+  attr_accessor :content_purpose_supergroup
+  attr_accessor :details
+  attr_accessor :document_type
+  attr_accessor :first_published_at
+  attr_accessor :links
+  attr_accessor :locale
+  attr_accessor :payload_version
+  attr_accessor :public_updated_at
+  attr_accessor :title
+
 
   def self.parse_message(message_payload)
-    content_id = message_payload.fetch('content_id')
-    base_path = message_payload.fetch('base_path')
-    payload_version = message_payload.fetch('payload_version')
-    locale = message_payload['locale'] || 'en'
-    metadata = Item::Metadata::Parsers::Metadata.parse(message_payload)
-    details = message_payload.fetch('details')
-    links = message_payload.fetch('expanded_links')
-
     new(
-      content_id: content_id,
-      base_path: base_path,
-      payload_version: payload_version,
-      locale: locale,
-      links: links,
-      details: details,
-      metadata: metadata,
+      base_path: message_payload.fetch('base_path'),
+      content_id: message_payload.fetch('content_id'),
+      content_purpose_document_supertype: message_payload['content_purpose_document_supertype'],
+      content_purpose_subgroup: message_payload['content_purpose_subgroup'],
+      content_purpose_supergroup: message_payload['content_purpose_supergroup'],
+      details: message_payload.fetch('details'),
+      document_type: message_payload['document_type'],
+      first_published_at: message_payload['first_published_at'],
+      links: message_payload.fetch('expanded_links'),
+      locale: message_payload['locale'] || 'en',
+      payload_version: message_payload.fetch('payload_version'),
+      public_updated_at: message_payload['public_updated_at'],
+      title: message_payload['title']
     )
-  end
-
-  def initialize(content_id:, base_path:, payload_version:, locale:, links:, details:, metadata:)
-    @content_id = content_id
-    @base_path = base_path
-    @payload_version = payload_version
-    @locale = locale
-    @links = links
-    @details = details
-    @metadata = metadata
   end
 
 private
--- spec/streams/publishing_api/content_item_spec.rb
+++ spec/streams/publishing_api/content_item_spec.rb
@@ -1,15 +1,21 @@
 RSpec.describe PublishingAPI::ContentItem do
   it "parses a valid message queue message" do
     payload = GovukSchemas::RandomExample.for_schema(notification_schema: "detailed_guide")
-    item = PublishingAPI::ContentItem.parse_message(payload)
+    content_item = PublishingAPI::ContentItem.parse_message(payload)
 
-    expect(item.content_id).to eq(payload['content_id'])
-    expect(item.base_path).to eq(payload['base_path'])
-    expect(item.locale).to eq(payload['locale'])
-    expect(item.payload_version).to eq(payload['payload_version'])
-    expect(item.details).to eq(payload['details'])
-    expect(item.links).to eq(payload['expanded_links'])
-    expect(item.metadata[:title]).to eq(payload['title'])
+    expect(content_item.base_path).to eq(payload['base_path'])
+    expect(content_item.content_id).to eq(payload['content_id'])
+    expect(content_item.content_purpose_document_supertype).to eq(payload['content_purpose_document_supertype'])
+    expect(content_item.content_purpose_subgroup).to eq(payload['content_purpose_subgroup'])
+    expect(content_item.content_purpose_supergroup).to eq(payload['content_purpose_supergroup'])
+    expect(content_item.details).to eq(payload['details'])
+    expect(content_item.document_type).to eq(payload['document_type'])
+    expect(content_item.first_published_at).to eq(payload['first_published_at'])
+    expect(content_item.links).to eq(payload['expanded_links'])
+    expect(content_item.locale).to eq(payload['locale'])
+    expect(content_item.payload_version).to eq(payload['payload_version'])
+    expect(content_item.public_updated_at).to eq(payload['public_updated_at'])
+    expect(content_item.title).to eq(payload['title'])
   end
 
   it "rejects an invalid message queue message" do
