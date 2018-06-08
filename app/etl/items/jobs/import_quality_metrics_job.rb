17469d31e01cc68a673b312b8d0fd69305fa89b2
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-08 21:54:32 +0100

    Do not use Job to retrieve Quality Metrics
    We are not in a rush to retrieve Quality metrics, and for the time
    being I would rather to call the method directly and raise an error
    in Sentry in case of issues.
    
    Of course, the process won’t stop

--- app/etl/items/jobs/import_quality_metrics_job.rb
+++
@@ -1,7 +0,0 @@
-class Items::Jobs::ImportQualityMetricsJob < Items::Jobs::ApplicationJob
-  sidekiq_options queue: 'quality_metrics'
-
-  def run(*args)
-    Items::Importers::QualityMetrics.run(*args)
-  end
-end
