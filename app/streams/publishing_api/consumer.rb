cce7215dd993c63a3c7ade78a999d7e698b200a9
Andrien Ricketts <andrien.ricketts@digital.cabinet-office.gov.uk>
2018-06-18 16:13:59 +0100

    Prevent Content Items with no 'base_path' being stored
    We need to specific fields from content items in the
    Data Warehouse. One of the mandatory fields is 'base_path'.
    This commit ensures that we only store the content items
    where there is a 'base_path' field, otherwise we ignore it.

--- app/streams/publishing_api/consumer.rb
+++ app/streams/publishing_api/consumer.rb
@@ -1,12 +1,24 @@
 module PublishingAPI
   class Consumer
     def process(message)
+      if is_invalid_message?(message)
+        GovukError.notify(StandardError.new, extra: { payload: message })
+        message.discard
+        return
+      end
+
       PublishingAPI::MessageHandler.process(message)
 
       message.ack
     rescue StandardError => e
       GovukError.notify(e)
       message.discard
+    end
+
+  private
+
+    def is_invalid_message?(message)
+      !message.payload['base_path'].present?
     end
   end
 end
--- spec/integration/streams/errors_spec.rb
+++ spec/integration/streams/errors_spec.rb
@@ -7,14 +7,35 @@ RSpec.describe PublishingAPI::Consumer do
 
   it_behaves_like 'a message queue processor'
 
-  context 'when an error happens' do
-    let!(:message) { build :message }
+  let!(:message) { build :message }
 
-    before { expect(PublishingAPI::MessageHandler).to receive(:process).and_raise(StandardError.new("An error")) }
+  context 'when an error happens' do
+    before {
+      expect(PublishingAPI::MessageHandler).to receive(:process).and_raise(StandardError.new)
+    }
 
     it "logs the error" do
       expect(GovukError).to receive(:notify).with(instance_of(StandardError))
 
+      expect { subject.process(message) }.to_not raise_error
+    end
+
+    it "discards the message" do
+      expect(message).to receive(:discard)
+
+      subject.process(message)
+    end
+  end
+
+  context "when message has missing mandatory field 'base_path'" do
+    before {
+      allow(PublishingAPI::MessageHandler).to receive(:process).and_raise(StandardError)
+    }
+
+    it "logs the error" do
+      message.payload.delete('base_path')
+
+      expect(GovukError).to receive(:notify).with(StandardError.new, extra: { payload: message })
       expect { subject.process(message) }.to_not raise_error
     end
 
