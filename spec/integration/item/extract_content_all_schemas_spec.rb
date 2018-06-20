38ffec6b052a1de9a66b8e8c2b2602f51c6e80a5
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-20 10:50:04 +0100

    Fix sample generation with Govuk schemas
    It generates valid samples for each schema. I have also DRY-ed the code

--- spec/integration/item/extract_content_all_schemas_spec.rb
+++ spec/integration/item/extract_content_all_schemas_spec.rb
@@ -1,32 +1,13 @@
 RSpec.describe 'Process parser', type: :integration do
   context 'with a list of content schemas' do
     it 'extracts the content valid schemas' do
-      schemas = valid_schemas
-      schemas.each do |schema|
-        expect(Item::Content::Parser.extract_content(schema)).to be_a(String).or eq(nil)
-      end
-    end
+      schemas = GovukSchemas::Schema.all(schema_type: "notification").values
 
-    it 'returns nil for unused schemas' do
-      schemas = invalid_schemas
       schemas.each do |schema|
-        expect(Item::Content::Parser.extract_content(schema)).to be_nil
+        sample = GovukSchemas::RandomExample.new(schema: schema).payload
+        expect(Item::Content::Parser.extract_content(sample)).to be_a(String).or eq(nil)
       end
     end
   end
 
-  def get_schema_collection
-    schemas = GovukSchemas::Schema.all(schema_type: "notification")
-    schemas.values
-  end
-
-  def valid_schemas
-    schemas = get_schema_collection
-
-    schemas.each do |schema|
-      random_example = GovukSchemas::RandomExample.new(schema: schema).payload
-      schemas.delete(schema) if invalid_schema_list.include?(random_example.dig("schema_name"))
-    end
-    schemas
-  end
 end
