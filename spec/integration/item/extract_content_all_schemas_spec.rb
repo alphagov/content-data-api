681b808c17a32dcc1ebfca13bd777f4cd6cba95d
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-20 11:01:31 +0100

    Split Rspec test into multiple test cases
    It is better to run the test once per schema, than to validate all the
    schemas in the same test.
    
    By having more tests we achieve a more granular approach to an issue
    with a concrete schema, plus the RSpec process does not give the feeling
    of being blocked.

--- spec/integration/item/extract_content_all_schemas_spec.rb
+++ spec/integration/item/extract_content_all_schemas_spec.rb
@@ -1,13 +1,12 @@
 RSpec.describe 'Process parser', type: :integration do
   context 'with a list of content schemas' do
-    it 'extracts the content valid schemas' do
-      schemas = GovukSchemas::Schema.all(schema_type: "notification").values
-
-      schemas.each do |schema|
-        sample = GovukSchemas::RandomExample.new(schema: schema).payload
+    schemas = GovukSchemas::Schema.all(schema_type: "notification")
+    schemas.each do |schema, value|
+      schema_name = schema.split('/')[-3]
+      it "extracts the content for: #{schema_name}" do
+        sample = GovukSchemas::RandomExample.new(schema: value).payload
         expect(Item::Content::Parser.extract_content(sample)).to be_a(String).or eq(nil)
       end
     end
   end
-
 end
