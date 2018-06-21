7e5c78db2c118122588741e639daee1e3a760627
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-21 12:23:52 +0100

    DRY-up the parser for Licence
    By renaming the parsed content to `body` we make it clearer the
    information that it holds. I have also simplified the expression
    to retrieve the content by content type as per suggestion[1]
    
    [1]: https://github.com/alphagov/content-performance-manager/pull/756/commits/a1fe426e637f23056550d410be831b543dc08121#r196812481

--- app/etl/item/content/parsers/licence.rb
+++ app/etl/item/content/parsers/licence.rb
@@ -1,26 +1,17 @@
 class Item::Content::Parsers::Licence
   def parse(json)
-    parsed = json.dig("details", "licence_overview")
-    if parsed.present? && parsed.is_a?(Array)
-      parsed = Hash[*parsed.map(&:values).flatten].fetch("text/html", nil)
+    body = json.dig("details", "licence_overview")
+    return unless body.present?
+
+    if body.is_a?(Array)
+      body_by_content_type = body.map(&:values).to_h
+      body = body_by_content_type.fetch('text/html', nil)
     end
 
-    return nil unless parsed.present?
-    parsed
+    body
   end
 
   def schemas
     ['licence']
   end
 end
-
-
-# body = json.dig("details", "body")
-# return unless body.present?
-#
-# if body.is_a?(Array)
-#   body_by_content_type = body.map(&:values).to_h
-#   body = body_by_content_type.fetch('text/html', nil)
-# end
-#
-# body
