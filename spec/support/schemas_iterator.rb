762af52e69fa5fe28d1602f6b14c4f323b9ff497
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-21 12:16:10 +0100

    Renames parameter to improve readability

--- spec/support/schemas_iterator.rb
+++ spec/support/schemas_iterator.rb
@@ -1,8 +1,8 @@
 module SchemasIterator
   def self.each_schema
     schemas = GovukSchemas::Schema.all(schema_type: "notification")
-    schemas.each do |schema, value|
-      schema_name = schema.split('/')[-3]
+    schemas.each do |schema_name, value|
+      schema_name = schema_name.split('/')[-3]
       yield schema_name, value
     end
   end
