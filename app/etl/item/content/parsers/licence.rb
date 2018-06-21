73f7bf585150bf37c6dc6d59bf0e9a3a879fd70a
Pablo Manrubia <pmanrubia@gmail.com>
2018-06-21 12:22:50 +0100

    Add RSpec to ensure we handle empty Body in Licences
    Improve the coverage of the test for the Licence parser, so we are sure
    that we are able to handle empty strings and empty arrays

--- app/etl/item/content/parsers/licence.rb
+++ app/etl/item/content/parsers/licence.rb
@@ -13,3 +13,14 @@ class Item::Content::Parsers::Licence
     ['licence']
   end
 end
+
+
+# body = json.dig("details", "body")
+# return unless body.present?
+#
+# if body.is_a?(Array)
+#   body_by_content_type = body.map(&:values).to_h
+#   body = body_by_content_type.fetch('text/html', nil)
+# end
+#
+# body
--- spec/etl/item/content/licence_spec.rb
+++ spec/etl/item/content/licence_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe Item::Content::Parser do
     expect(subject.extract_content(json.deep_stringify_keys)).to eq('licence expired')
   end
 
+  it 'returns nil if details.body is an empty string' do
+    json = { schema_name: 'licence', details: { licence_overview: '' } }
+    expect(subject.extract_content(json.deep_stringify_keys)).to eq(nil)
+  end
+
+  it 'returns nil if details.body is an empty array' do
+    valid_schema_json = { schema_name: 'licence', details: { licence_overview: [] } }
+    expect(subject.extract_content(valid_schema_json.deep_stringify_keys)).to eq(nil)
+  end
+
   it "returns licence_overview for content provided as array for each content type" do
     json = { schema_name: 'licence',
       details: { licence_overview:
