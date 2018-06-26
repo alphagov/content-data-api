b53817967740bfc2ab5e1bfe0521525ff5f1e687
Mat Moore <matmoore@users.noreply.github.com>
2018-06-26 10:32:12 +0000

    Remove duplication from MessageAdapter
    `to_dimensions_items` is very similar for multi-part and single-part
    documents. The only fields that change are titles, base paths, and content.
    So inline this branch and factor out the common bits.

--- app/streams/publishing_api/message_adapter.rb
+++ app/streams/publishing_api/message_adapter.rb
@@ -16,62 +16,49 @@ module PublishingAPI
     end
 
     def to_dimension_items
-      items = []
       if has_multiple_parts?
-        parts.each do |part|
-          items << Dimensions::Item.new(
+        parts.map do |part|
+          Dimensions::Item.new(
             base_path: base_path_for_part(part),
-            content_id: message.payload.fetch('content_id'),
-            publishing_api_payload_version: message.payload.fetch('payload_version'),
-            document_type: message.payload.fetch('document_type'),
-            locale: message.payload['locale'],
             title: title_for_part(part),
-            content_purpose_document_supertype: message.payload['content_purpose_document_supertype'],
-            content_purpose_supergroup: message.payload['content_purpose_supergroup'],
-            content_purpose_subgroup: message.payload['content_purpose_subgroup'],
-            first_published_at: parse_time('first_published_at'),
-            primary_organisation_content_id: primary_organisation['content_id'],
-            primary_organisation_title: primary_organisation['title'],
-            primary_organisation_withdrawn: primary_organisation['withdrawn'],
-            public_updated_at: parse_time('public_updated_at'),
-            schema_name: message.payload.fetch('schema_name'),
-            latest: true,
-            raw_json: message.payload,
-            content: Item::Content::Parser.extract_content(message.payload, subpage: part['slug'])
+            content: Item::Content::Parser.extract_content(message.payload, subpage: part['slug']),
+            **attributes
           )
         end
       else
-        items << to_dimension_item(message)
+        [
+          Dimensions::Item.new(
+            base_path: base_path,
+            title: title,
+            content: Item::Content::Parser.extract_content(message.payload),
+            **attributes
+          )
+        ]
       end
-
-      items
     end
 
   private
 
     attr_reader :message
 
-    def to_dimension_item(item_message)
-      Dimensions::Item.new(
-        content_id: item_message.payload.fetch('content_id'),
-        base_path: item_message.payload.fetch('base_path'),
-        publishing_api_payload_version: item_message.payload.fetch('payload_version'),
-        document_type: item_message.payload.fetch('document_type'),
-        locale: item_message.payload['locale'],
-        title: item_message.payload['title'],
-        content_purpose_document_supertype: item_message.payload['content_purpose_document_supertype'],
-        content_purpose_supergroup: item_message.payload['content_purpose_supergroup'],
-        content_purpose_subgroup: item_message.payload['content_purpose_subgroup'],
+    def attributes
+      {
+        content_id: content_id,
+        publishing_api_payload_version: message.payload.fetch('payload_version'),
+        document_type: message.payload.fetch('document_type'),
+        locale: message.payload['locale'],
+        content_purpose_document_supertype: message.payload['content_purpose_document_supertype'],
+        content_purpose_supergroup: message.payload['content_purpose_supergroup'],
+        content_purpose_subgroup: message.payload['content_purpose_subgroup'],
         first_published_at: parse_time('first_published_at'),
         primary_organisation_content_id: primary_organisation['content_id'],
         primary_organisation_title: primary_organisation['title'],
         primary_organisation_withdrawn: primary_organisation['withdrawn'],
         public_updated_at: parse_time('public_updated_at'),
-        schema_name: item_message.payload.fetch('schema_name'),
-        latest: true,
-        raw_json: item_message.payload,
-        content: Item::Content::Parser.extract_content(item_message.payload)
-      )
+        schema_name: message.payload.fetch('schema_name'),
+        raw_json: message.payload,
+        latest: true
+      }
     end
 
     def primary_organisation
@@ -89,6 +76,10 @@ module PublishingAPI
 
     def base_path
       message.payload.fetch('base_path')
+    end
+
+    def title
+      message.payload['title']
     end
 
     def base_path_for_part(part)
