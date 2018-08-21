74110f333c79b279ddfb7838abaa72cac7207dd1
Pablo Manrubia <pmanrubia@gmail.com>
2018-08-21 18:47:50 +0100

    Add describe block to Promotion#valid?

--- spec/streams/publishing_api/promotion_spec.rb
+++ spec/streams/publishing_api/promotion_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe PublishingAPI::Promotion do
-  ATTRIBUTE_NAMES =
-    %w(
+  describe '#valid?' do
+    ATTRIBUTE_NAMES =
+      %w(
       analytics_identifier
       content_purpose_document_supertype
       content_purpose_subgroup
@@ -25,30 +26,31 @@ RSpec.describe PublishingAPI::Promotion do
       update_type
     ).freeze
 
-  ATTRIBUTE_NAMES.each do |attribute_name|
-    it "returns true if the #{attribute_name} has changed" do
-      item1 = build :dimensions_item, attribute_name => 'value-1'
-      item2 = build :dimensions_item, attribute_name => 'value-2'
+    ATTRIBUTE_NAMES.each do |attribute_name|
+      it "returns true if the #{attribute_name} has changed" do
+        item1 = build :dimensions_item, attribute_name => 'value-1'
+        item2 = build :dimensions_item, attribute_name => 'value-2'
+
+        promotion = PublishingAPI::Promotion.new(item1, item2)
+        expect(promotion.valid?).to be true
+      end
+    end
+
+    it 'does not include the latest attribute' do
+      item1 = build :dimensions_item, latest: true
+      item2 = build :dimensions_item, item1.attributes.dup
+      item2.latest = false
+
+      promotion = PublishingAPI::Promotion.new(item1, item2)
+      expect(promotion.valid?).to be false
+    end
+
+    it 'returns true if old_item is nil' do
+      item1 = build :dimensions_item
+      item2 = nil
 
       promotion = PublishingAPI::Promotion.new(item1, item2)
       expect(promotion.valid?).to be true
     end
-  end
-
-  it 'does not include the latest attribute' do
-    item1 = build :dimensions_item, latest: true
-    item2 = build :dimensions_item, item1.attributes.dup
-    item2.latest = false
-
-    promotion = PublishingAPI::Promotion.new(item1, item2)
-    expect(promotion.valid?).to be false
-  end
-
-  it 'returns true if old_item is nil' do
-    item1 = build :dimensions_item
-    item2 = nil
-
-    promotion = PublishingAPI::Promotion.new(item1, item2)
-    expect(promotion.valid?).to be true
   end
 end
