92b2aca80efa9b14bbdb0f0b82ef37c4ddb94f72
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-08-22 12:42:23 +0000

    Begin refactoring message handling into single/multipart

--- app/streams/publishing_api/message_handler.rb
+++ app/streams/publishing_api/message_handler.rb
@@ -9,36 +9,15 @@ class PublishingAPI::MessageHandler
 
   def process
     return if PublishingAPI::MessageValidator.is_old_message?(message)
+    if PublishingAPI::MultipartMessage.is_multipart?(message)
+      PublishingAPI::MultipartHandler.new(message).process
+    else
+      PublishingAPI::SingleItemHandler.new(message).process
+    end
 
-    item_handlers.each(&:process!)
   end
 
 private
 
   attr_reader :message
-
-  def item_handlers
-    adapter = PublishingAPI::MessageAdapter.new(message)
-    new_items = adapter.new_dimension_items
-
-    old_items = Dimensions::Item.existing_latest_items(
-      adapter.content_id,
-      adapter.locale,
-      new_items.map(&:base_path)
-    )
-
-    result = new_items.map do |new_item|
-      old_item = Dimensions::Item.find_by(base_path: new_item.base_path, latest: true)
-      PublishingAPI::ItemHandler.new(
-        old_item: old_item,
-        new_item: new_item,
-      )
-    end
-
-    if new_items.length < old_items.length
-      old_items.each(&:deprecate!)
-    end
-
-    result
-  end
 end
