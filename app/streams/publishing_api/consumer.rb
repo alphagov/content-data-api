ca405cbdefe50925290693ddfa6e1819600c8e13
Pablo Manrubia <pmanrubia@gmail.com>
2018-09-04 16:20:25 +0100

    Do not process `republish` events
    It is better to count them via the Grafana monitor and the process them
    once we are sure that everything is working as expected.

--- app/streams/publishing_api/consumer.rb
+++ app/streams/publishing_api/consumer.rb
@@ -1,12 +1,22 @@
 module PublishingAPI
   class Consumer
     def process(message)
-      MessageProcessorJob.perform_later(message.payload)
+      MessageProcessorJob.perform_later(message.payload) if valid_routing_key?(message)
 
       message.ack
     rescue StandardError => e
       GovukError.notify(e)
       message.discard
     end
+
+  private
+
+    def valid_routing_key?(message)
+      routing_key = message.delivery_info.routing_key
+
+      ROUTING_KEYS.any? { |suffix| routing_key.ends_with?(suffix) }
+    end
+
+    ROUTING_KEYS = %w(links major minor unpublish bulk.data-warehouse).freeze
   end
 end
