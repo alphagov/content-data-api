8967907cbceddfa48d32003489f112ea9ea4e21f
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-09-20 14:20:38 +0000

    Use content_uuid to group data for /content
    The query that returns data for the /content
    endpoint now uses the content_uuid column
    to join the latest Dimensions::Item to all
    previous editions/metrics for those editions.
    
    This means we will get metrics for all editions
    even if the base_path, title, document_type have
    changed.

--- spec/domain/reports/content_spec.rb
+++ spec/domain/reports/content_spec.rb
@@ -2,6 +2,8 @@ RSpec.describe Reports::Content do
   include ItemSetupHelpers
 
   let(:primary_org_id) { '96cad973-92dc-41ea-a0ff-c377908fee74' }
+  let(:content_uuid) { SecureRandom.uuid }
+  let(:another_content_uuid) { SecureRandom.uuid }
 
   before do
     create :user
@@ -20,6 +22,8 @@ RSpec.describe Reports::Content do
           title: 'the title',
           document_type: 'news_story',
           primary_organisation_content_id: primary_org_id,
+          latest: false,
+          content_uuid: content_uuid,
         })
 
       create_metric(base_path: '/path/1', date: '2018-01-02',
@@ -33,20 +37,97 @@ RSpec.describe Reports::Content do
           title: 'the title',
           document_type: 'news_story',
           primary_organisation_content_id: primary_org_id,
+          latest: true,
+          content_uuid: content_uuid,
+        })
+
+      create_metric(base_path: '/path/2', date: '2018-01-02',
+        daily: {
+          unique_pageviews: 20,
+          is_this_useful_yes: 10,
+          is_this_useful_no: 10,
+          number_of_internal_searches: 7
+        },
+        item: {
+          title: 'title 2',
+          document_type: 'press_release',
+          primary_organisation_content_id: primary_org_id,
+          latest: true,
+          content_uuid: another_content_uuid,
         })
     end
 
     it 'returns the correct data' do
-      expect(described_class.retrieve(from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id)).to match_array(
+      results = described_class.retrieve(from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id)
+      expect(results).to eq(
         [
-          base_path: '/path/1',
-          title: 'the title',
-          unique_pageviews: 233,
-          document_type: 'news_story',
-          satisfaction_score: 0.8,
-          satisfaction_score_responses: 250,
-          number_of_internal_searches: 220
+          {
+            base_path: '/path/1',
+            title: 'the title',
+            unique_pageviews: 233,
+            document_type: 'news_story',
+            satisfaction_score: 0.8,
+            satisfaction_score_responses: 250,
+            number_of_internal_searches: 220
+          },
+          {
+            base_path: '/path/2',
+            title: 'title 2',
+            unique_pageviews: 20,
+            document_type: 'press_release',
+            satisfaction_score: 0.5,
+            satisfaction_score_responses: 20,
+            number_of_internal_searches: 7
+          }
         ]
+      )
+    end
+  end
+
+
+  context 'the attributes we are displaying have changed over time' do
+    before do
+      create_metric(base_path: '/old/base/path', date: '2018-01-01',
+        daily: {
+          unique_pageviews: 100,
+          is_this_useful_yes: 10,
+          is_this_useful_no: 10,
+          number_of_internal_searches: 15,
+        },
+        item: {
+          title: 'old title',
+          document_type: 'news_story',
+          primary_organisation_content_id: primary_org_id,
+          latest: false,
+          content_uuid: content_uuid,
+        })
+      create_metric(base_path: '/new/base/path', date: '2018-01-01',
+        daily: {
+          unique_pageviews: 100,
+          is_this_useful_yes: 50,
+          is_this_useful_no: 50,
+          number_of_internal_searches: 5,
+        },
+        item: {
+          title: 'new title',
+          document_type: 'press_release',
+          primary_organisation_content_id: primary_org_id,
+          latest: true,
+          content_uuid: content_uuid,
+        })
+    end
+
+    it 'returns metrics from all versions with metadata from the latest version' do
+      results = described_class.retrieve(from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id)
+      expect(results.count).to eq(1)
+      expect(results.first).to eq(
+        base_path: '/new/base/path',
+        title: 'new title',
+        unique_pageviews: 200,
+        document_type: 'press_release',
+        satisfaction_score: 0.5,
+        satisfaction_score_responses: 120,
+        number_of_internal_searches: 20
       )
     end
   end
@@ -59,7 +140,11 @@ RSpec.describe Reports::Content do
           is_this_useful_yes: 0,
           is_this_useful_no: 0,
         },
-        item: { title: 'the title', primary_organisation_content_id: primary_org_id })
+        item: {
+          title: 'the title',
+          primary_organisation_content_id: primary_org_id,
+          content_uuid: content_uuid,
+        })
     end
 
     it 'returns the nil for the satisfaction_score' do
@@ -79,7 +164,9 @@ RSpec.describe Reports::Content do
           is_this_useful_yes: 0,
           is_this_useful_no: 0,
         },
-        item: { title: 'the title', primary_organisation_content_id: primary_org_id })
+        item: { title: 'the title',
+          primary_organisation_content_id: primary_org_id,
+          content_uuid: content_uuid, })
     end
 
     it 'returns a empty array' do
