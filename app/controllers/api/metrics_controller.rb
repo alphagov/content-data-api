b44f92873aeb6cbc70d662cc1b3084fd357eeb71
Pablo Manrubia <pmanrubia@gmail.com>
2018-09-24 13:16:44 +0100

    Remove parameter validation when listing metrics

--- app/controllers/api/metrics_controller.rb
+++ app/controllers/api/metrics_controller.rb
@@ -1,20 +1,6 @@
 class Api::MetricsController < Api::BaseController
-  before_action :validate_params!, except: :index
-
   def index
     items = Metric.find_all
     render json: items
-  end
-
-private
-
-  def validate_params!
-    unless api_request.valid?
-      error_response(
-        "validation-error",
-        title: "One or more parameters is invalid",
-        invalid_params: api_request.errors.to_hash
-      )
-    end
   end
 end
