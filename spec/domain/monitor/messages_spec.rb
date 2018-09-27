9af02b423bd4b154b6d98d9c3e558c3e25496f23
chao-xian <gds@kelvingan.co.uk>
2018-09-27 12:42:23 +0100

    Fix messsage processing job to call monitoring
    We also refactor slightly to accept just the routing key in the
    monitoring message.

--- spec/domain/monitor/messages_spec.rb
+++ spec/domain/monitor/messages_spec.rb
@@ -4,38 +4,32 @@ RSpec.describe Monitor::Messages do
   end
 
   it 'increments major if routing key ends .major' do
-    message = build(:message, routing_key: 'news_story.major')
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.major")
-    Monitor::Messages.run(message)
+    Monitor::Messages.run('news_story.major')
   end
 
   it 'increments minor if routing key ends .minor' do
-    message = build(:message, routing_key: 'news_story.minor')
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.minor")
-    Monitor::Messages.run(message)
+    Monitor::Messages.run('news_story.minor')
   end
 
   it 'increments links if routing key ends .links' do
-    message = build(:message, routing_key: 'news_story.links')
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.links")
-    Monitor::Messages.run(message)
+    Monitor::Messages.run('news_story.links')
   end
 
   it 'increments republish if routing key ends .republish' do
-    message = build(:message, routing_key: 'news_story.republish')
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.republish")
-    Monitor::Messages.run(message)
+    Monitor::Messages.run('news_story.republish')
   end
 
   it 'increments unpublish if routing key ends .unpublish' do
-    message = build(:message, routing_key: 'news_story.unpublish')
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.unpublish")
-    Monitor::Messages.run(message)
+    Monitor::Messages.run('news_story.unpublish')
   end
 
-  it 'increments discarded if routing key ends .discarded' do
-    message = build(:message, routing_key: 'news_story.discarded')
+  it '.increment_discarded increments discarded' do
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.discarded")
-    Monitor::Messages.run(message)
+    Monitor::Messages.increment_discarded
   end
 end
