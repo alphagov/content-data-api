d48a36e9f74480ba31b3da6fe89fbe24c079926c
Pete Goddard <pete@artofsoftware.co.uk>
2018-10-03 12:27:17 +0100

    Refactor facts_spec.rb and find_series_spec.rb

--- spec/domain/reports/find_series_spec.rb
+++ spec/domain/reports/find_series_spec.rb
@@ -1,9 +1,7 @@
 RSpec.describe Reports::FindSeries do
-  include ItemSetupHelpers
   context "all" do
     it "returns a series of all metrics" do
-      today = Date.today
-      create_metric(base_path: '/path/1', date: today)
+      create :metric, date: Date.today
 
       expect(described_class.new.run.one?).to eq(true)
     end
@@ -11,9 +9,10 @@ RSpec.describe Reports::FindSeries do
 
   context "between" do
     it "returns metrics for a given date range" do
-      metric1 = create_metric(base_path: '/the/path', date: '2018-5-13')
-      metric2 = create_metric(base_path: '/the/path', date: '2018-5-14')
-      create_metric(base_path: '/the/path', date: '2018-5-24')
+      edition = create :edition, base_path: '/the/path', date: '2018-5-13'
+      metric1 = create :metric, edition: edition, date: '2018-5-13'
+      metric2 = create :metric, edition: edition, date: '2018-5-14'
+      create :metric, edition: edition, date: '2018-5-24'
       series = described_class.new.between(from: '2018-5-13', to: '2018-5-14').run
 
       expect(series).to_not be_nil
@@ -27,9 +26,11 @@ RSpec.describe Reports::FindSeries do
       today = Date.today
       tomorrow = today + 1
 
-      metric1 = create_metric base_path: '/path/1', date: today, edition: { words: 10_000 }
-      metric2 = create_metric base_path: '/path/2', date: tomorrow, edition: { words: 20_000 }
-      metric3 = create_metric base_path: '/path/3', date: tomorrow
+      edition1 = create :edition, base_path: '/path/1', date: today, facts: { words: 10_000 }
+      metric1 = create :metric, date: today, edition: edition1
+      edition2 = create :edition, base_path: '/path/2', date: tomorrow, facts: { words: 20_000 }
+      metric2 = create :metric, date: tomorrow, edition: edition2
+      metric3 = create :metric, date: tomorrow
 
       series = described_class.new.run
 
@@ -39,17 +40,15 @@ RSpec.describe Reports::FindSeries do
 
   context "by_organisation" do
     it "returns a series of metrics filtered by organisation" do
-      day0 = create(:dimensions_date, date: Date.new(2018, 1, 12))
-      day1 = create(:dimensions_date, date: Date.new(2018, 1, 13))
-      day2 = create(:dimensions_date, date: Date.new(2018, 1, 14))
-      item1 = create(:dimensions_item, primary_organisation_content_id: 'org-1')
-      item2 = create(:dimensions_item, primary_organisation_content_id: 'org-2')
-      create(:facts_edition, dimensions_item: item1, dimensions_date: day0)
+      day0 = Date.new(2018, 1, 12)
+      day1 = Date.new(2018, 1, 13)
+      day2 = Date.new(2018, 1, 14)
+      edition1 = create :edition, date: day0, primary_organisation_content_id: 'org-1'
+      edition2 = create :edition, date: day1, primary_organisation_content_id: 'org-2'
 
-
-      metric1 = create(:metric, dimensions_item: item1, dimensions_date: day0)
-      metric2 = create(:metric, dimensions_item: item1, dimensions_date: day1)
-      create(:metric, dimensions_item: item2, dimensions_date: day2)
+      metric1 = create(:metric, edition: edition1, date: day0)
+      metric2 = create(:metric, edition: edition1, date: day1)
+      create(:metric, edition: edition2, date: day2)
 
       series = described_class.new.by_organisation_id('org-1').run
 
@@ -57,11 +56,7 @@ RSpec.describe Reports::FindSeries do
     end
 
     it 'ignores parameters when blank' do
-      item = create(:dimensions_item)
-      metric = create(:metric, dimensions_item: item)
-      today = Dimensions::Date.find_or_create(Date.today)
-      create(:facts_edition, dimensions_item: item,
-        dimensions_date: today)
+      metric = create(:metric, date: Date.today)
 
       expect(described_class.new.by_organisation_id('').run).to match_array([metric])
     end
@@ -69,18 +64,17 @@ RSpec.describe Reports::FindSeries do
 
   context "by_base_path" do
     it "returns a series of metrics for a base path" do
-      day0 = create(:dimensions_date, date: Date.new(2018, 1, 12))
-      day1 = create(:dimensions_date, date: Date.new(2018, 1, 13))
-      day2 = create(:dimensions_date, date: Date.new(2018, 1, 14))
-      item1 = create(:dimensions_item, base_path: '/path1')
-      item2 = create(:dimensions_item, base_path: '/path2')
-      create(:facts_edition, dimensions_item: item1, dimensions_date: day0)
+      day0 = Date.new(2018, 1, 12)
+      day1 = Date.new(2018, 1, 13)
+      day2 = Date.new(2018, 1, 14)
+      edition1 = create(:edition, date: day0, base_path: '/path1')
+      edition2 = create(:edition, date: day0, base_path: '/path2')
 
-      metric1 = create(:metric, dimensions_item: item1, dimensions_date: day0)
-      metric2 = create(:metric, dimensions_item: item1, dimensions_date: day1)
-      metric3 = create(:metric, dimensions_item: item1, dimensions_date: day2)
-      create(:metric, dimensions_item: item2, dimensions_date: day1)
-      create(:metric, dimensions_item: item2, dimensions_date: day2)
+      metric1 = create :metric, edition: edition1, date: day0
+      metric2 = create :metric, edition: edition1, date: day1
+      metric3 = create :metric, edition: edition1, date: day2
+      create :metric, edition: edition2, date: day1
+      create :metric, edition: edition2, date: day2
 
       results = described_class.new.by_base_path('/path1').run
 
@@ -88,11 +82,7 @@ RSpec.describe Reports::FindSeries do
     end
 
     it 'ignores parameters when blank' do
-      item = create(:dimensions_item)
-      metric = create(:metric, dimensions_item: item)
-      today = Dimensions::Date.find_or_create(Date.today)
-      create(:facts_edition, dimensions_item: item,
-        dimensions_date: today)
+      metric = create(:metric, date: Date.today)
 
       expect(described_class.new.by_base_path('').run).to match_array([metric])
     end
@@ -100,20 +90,19 @@ RSpec.describe Reports::FindSeries do
 
   describe '#content_items' do
     it 'return the content items included in the report' do
-      day0 = create(:dimensions_date, date: Date.new(2018, 1, 12))
-      day1 = create(:dimensions_date, date: Date.new(2018, 1, 13))
-      day2 = create(:dimensions_date, date: Date.new(2018, 1, 14))
-      item1 = create(:dimensions_item, base_path: '/path1')
-      item2 = create(:dimensions_item, base_path: '/path2')
-      create(:facts_edition, dimensions_item: item1, dimensions_date: day0)
+      day0 = Date.new(2018, 1, 12)
+      day1 = Date.new(2018, 1, 13)
+      day2 = Date.new(2018, 1, 14)
+      edition1 = create(:edition, base_path: '/path1', date: day0)
+      edition2 = create(:edition, base_path: '/path2', date: day0)
 
-      create(:metric, dimensions_item: item1, dimensions_date: day0)
-      create(:metric, dimensions_item: item1, dimensions_date: day1)
-      create(:metric, dimensions_item: item1, dimensions_date: day2)
-      create(:metric, dimensions_item: item2, dimensions_date: day1)
-      create(:metric, dimensions_item: item2, dimensions_date: day2)
+      create(:metric, edition: edition1, date: day0)
+      create(:metric, edition: edition1, date: day1)
+      create(:metric, edition: edition1, date: day2)
+      create(:metric, edition: edition2, date: day1)
+      create(:metric, edition: edition2, date: day2)
 
-      expect(described_class.new.content_items).to match_array([item1, item2])
+      expect(described_class.new.content_items).to match_array([edition1, edition2])
     end
   end
 end
