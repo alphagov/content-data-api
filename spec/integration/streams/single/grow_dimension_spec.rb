6a7ab21048d684c0bd4bd7cef32201a010899c90
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-10 15:04:38 +0100

    Remove `locale` from test (not needed)

--- spec/integration/streams/single/grow_dimension_spec.rb
+++ spec/integration/streams/single/grow_dimension_spec.rb
@@ -36,8 +36,10 @@ RSpec.describe PublishingAPI::Consumer do
       subject.process(message)
       subject.process(message2)
     }.to change(Dimensions::Edition, :count).by(1)
-    expect(Dimensions::Edition.first).to have_attributes(publishing_api_payload_version: 2)
-    expect(Dimensions::Edition.first).to have_attributes(latest: true)
+    expect(Dimensions::Edition.first).to have_attributes(
+      publishing_api_payload_version: 2,
+      latest: true
+)
   end
 
   it 'does not grow the dimension if the event carries no changes in an attribute' do
@@ -82,23 +84,21 @@ RSpec.describe PublishingAPI::Consumer do
       message = build :message, base_path: '/base-path', attributes: message_attributes
       message.payload['details']['body'] = '<p>some content</p>'
       subject.process(message)
-      latest_edition = Dimensions::Edition.where(base_path: '/base-path', latest: true).first
+      latest_edition = Dimensions::Edition.latest.find_by(base_path: '/base-path')
       expect(latest_edition).to have_attributes(expected_attributes(content_id: message.payload['content_id']))
     end
 
     it 'assigns the same warehouse_item_id to the new edition' do
       content_id = SecureRandom.uuid
-      message = build :message,
-        base_path: '/base-path',
-        content_id: content_id,
-        attributes: { 'payload_version' => 2 }
-      message2 = build :message,
-        content_id: content_id,
-        payload: message.payload.deep_dup
+
+      message = build :message, base_path: '/base-path', content_id: content_id, attributes: { 'payload_version' => 2 }
+      subject.process(message)
+
+      message2 = build :message, content_id: content_id, payload: message.payload.deep_dup
       message2.payload['payload_version'] = 4
       message2.payload['details']['body'] = '<p>different content</p>'
-      subject.process(message)
       subject.process(message2)
+
       warehouse_ids = Dimensions::Edition.where(base_path: '/base-path').pluck(:warehouse_item_id)
       expect(warehouse_ids.uniq).to eq(["#{content_id}:en"])
     end
@@ -106,33 +106,21 @@ RSpec.describe PublishingAPI::Consumer do
 
   context 'when the base path has changed' do
     let(:content_id) { SecureRandom.uuid }
-    let(:locale) { 'en' }
-    let(:warehouse_item_id) { "#{content_id}:#{locale}" }
+    let(:warehouse_item_id) { "#{content_id}:en" }
     let(:new_base_path) { '/new/base/path' }
 
     it 'creates the new edition with the warehouse_item_id of the old edition' do
       create :edition,
-        base_path: '/old/base/path',
-        content_id: content_id,
-        locale: locale,
-        warehouse_item_id: warehouse_item_id,
-        latest: true,
-        publishing_api_payload_version: 2
-      message = build :message,
-        base_path: new_base_path,
-        content_id: content_id,
-        locale: locale,
-        payload_version: 3
+             base_path: '/old/base/path',
+             content_id: content_id,
+             warehouse_item_id: warehouse_item_id,
+             publishing_api_payload_version: 2
+      message = build :message, base_path: new_base_path, content_id: content_id, payload_version: 3
 
       subject.process(message)
 
-      new_edition = Dimensions::Edition.find_by(content_id: content_id,
-        locale: locale,
-        latest: true)
-      expect(new_edition).to have_attributes(
-        warehouse_item_id: warehouse_item_id,
-        base_path: new_base_path
-      )
+      new_edition = Dimensions::Edition.latest.find_by(content_id: content_id)
+      expect(new_edition).to have_attributes(warehouse_item_id: warehouse_item_id, base_path: new_base_path)
     end
   end
 end
