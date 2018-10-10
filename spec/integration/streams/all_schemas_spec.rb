06ddf2512fc90ba581f0a894fa7bc0e5c9f9b007
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-10 14:10:10 +0100

    Remove before block and set expectation before running test
    This improves readability because the before block feels a bit awkward.

--- spec/integration/streams/all_schemas_spec.rb
+++ spec/integration/streams/all_schemas_spec.rb
@@ -4,20 +4,16 @@ RSpec.describe 'Process all schemas' do
 
   let(:subject) { PublishingAPI::Consumer.new }
 
-  before do
-    allow(GovukError).to receive(:notify)
-  end
-
   SchemasIterator.each_schema do |schema_name, schema|
     unless %w{travel_advice guide}.include?(schema_name) || schema_name.include?('placeholder')
       %w{major minor links republish unpublish}.each do |update_type|
         it "handles event for: `#{schema_name}` with no errors for a `#{update_type}` update" do
+          expect(GovukError).not_to receive(:notify)
+
           payload = SchemasIterator.payload_for(schema_name, schema)
           message = build(:message, payload: payload, routing_key: "#{schema_name}.#{update_type}")
 
           subject.process(message)
-
-          expect(GovukError).not_to have_received(:notify)
         end
       end
     end
