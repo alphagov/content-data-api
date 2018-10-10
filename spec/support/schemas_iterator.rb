feee51abf8d44c8dd6cf79ef1968e6fc50409297
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-10 12:57:47 +0100

    Memoize the schemas iterator

--- spec/support/schemas_iterator.rb
+++ spec/support/schemas_iterator.rb
@@ -1,9 +1,12 @@
 module SchemasIterator
   def self.each_schema
-    schemas = GovukSchemas::Schema.all(schema_type: "notification")
-    schemas.each do |schema_name, value|
+    all_schemas.each do |schema_name, value|
       schema_name = schema_name.split('/')[-3]
       yield schema_name, value
     end
+  end
+
+  def self.all_schemas
+    @all_schemas ||= GovukSchemas::Schema.all(schema_type: "notification")
   end
 end
