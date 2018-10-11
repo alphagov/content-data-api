fbbe13f4740b355811fbc7cf18480bee4388d31c
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-11 12:25:28 +0100

    Remove unused scope: with_edition_metrics

--- app/models/facts/metric.rb
+++ app/models/facts/metric.rb
@@ -7,10 +7,6 @@ class Facts::Metric < ApplicationRecord
   validates :dimensions_date, presence: true
   validates :dimensions_edition, presence: true
 
-  scope :with_edition_metrics, -> do
-    joins(dimensions_edition: :facts_edition)
-  end
-
   scope :for_yesterday, -> { where(dimensions_date: Dimensions::Date.find_or_create(Date.yesterday)) }
   scope :from_day_before_to, ->(date) { where(dimensions_date: Dimensions::Date.between(date - 1, date)) }
 end
