2cd5c9af3efefca195ae5b8d69c3eb249db3f74f
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-11 12:24:17 +0100

    Remove unused method: csv_fields in Facts::Metric

--- app/models/facts/metric.rb
+++ app/models/facts/metric.rb
@@ -13,40 +13,4 @@ class Facts::Metric < ApplicationRecord
 
   scope :for_yesterday, -> { where(dimensions_date: Dimensions::Date.find_or_create(Date.yesterday)) }
   scope :from_day_before_to, ->(date) { where(dimensions_date: Dimensions::Date.between(date - 1, date)) }
-
-  def self.csv_fields
-    %i[
-      date
-      content_id
-      base_path
-      locale
-      title
-      description
-      document_type
-      schema_name
-      content_purpose_document_supertype
-      content_purpose_supergroup
-      content_purpose_subgroup
-      first_published_at
-      public_updated_at
-      pviews
-      primary_organisation_title
-      primary_organisation_content_id
-      upviews
-      feedex
-      pdf_count
-      doc_count
-      readability
-      useful_yes
-      useful_no
-      searches
-      words
-      chars
-      sentences
-      entrances
-      exits
-      bounce_rate
-      avg_page_time
-    ]
-  end
 end
