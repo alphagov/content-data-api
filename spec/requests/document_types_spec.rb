0b0235ed90d866e198ce7543dd4916229ec123ce
Pete Goddard <pete@artofsoftware.co.uk>
2018-10-19 16:11:53 +0100

    Add spec for caching on /organisations and /document_types

--- spec/requests/document_types_spec.rb
+++ spec/requests/document_types_spec.rb
@@ -11,4 +11,33 @@ RSpec.describe '/document_types' do
     json = JSON.parse(response.body).deep_symbolize_keys
     expect(json).to eq(document_types: %w(guide manual))
   end
+
+  describe "an API response" do
+    it "should be cacheable until the end of the day" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 0, 0, 0)) do
+        get "/document_types"
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=3600, public"
+      end
+    end
+
+    it "expires at 1am" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 1, 0, 0)) do
+        get '/document_types'
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=0, public"
+      end
+    end
+
+    it "can be cached for up to a day" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 1, 0, 1)) do
+        get '/document_types'
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=86399, public"
+      end
+    end
+  end
 end
--- spec/requests/organisations_spec.rb
+++ spec/requests/organisations_spec.rb
@@ -16,4 +16,33 @@ RSpec.describe '/organisations' do
       ]
     )
   end
+
+  describe "an API response" do
+    it "should be cacheable until the end of the day" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 0, 0, 0)) do
+        get '/organisations'
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=3600, public"
+      end
+    end
+
+    it "expires at 1am" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 1, 0, 0)) do
+        get '/organisations'
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=0, public"
+      end
+    end
+
+    it "can be cached for up to a day" do
+      Timecop.freeze(Time.zone.local(2020, 1, 1, 1, 0, 1)) do
+        get '/organisations'
+
+        expect(response.headers['ETag']).to be_present
+        expect(response.headers['Cache-Control']).to eq "max-age=86399, public"
+      end
+    end
+  end
 end
