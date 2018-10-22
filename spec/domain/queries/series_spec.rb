16675c47cc4ef90e9b5b18e3c6304cd7aed501ab
Sean Rankine <sean.rankine@me.com>
2018-10-22 11:16:34 +0100

    Sort time series returned from db
    The API needs to respond with the time series in chronological
    order. Perviously data was just returned as ordered within the
    database. The PR adds sorts the time series data.

--- spec/domain/queries/series_spec.rb
+++ spec/domain/queries/series_spec.rb
@@ -7,26 +7,22 @@ RSpec.describe Queries::Series do
   let!(:edition) { create :edition, date: day1, content_id: content_id, base_path: base_path, locale: 'en' }
 
   before do
+    create :metric, edition: edition, date: day3, pviews: 30
     create :metric, edition: edition, date: day1, pviews: 10
     create :metric, edition: edition, date: day2, pviews: 20
-    create :metric, edition: edition, date: day3, pviews: 30
   end
 
-  it 'return the time series' do
+  it 'return the time series in order' do
     series = Queries::Series.new('pviews', Facts::Metric.all)
-    expect(series.time_series).to eq expected_values
+    expect(series.time_series).to eq([
+      { date: "2018-01-13", value: 10 },
+      { date: "2018-01-14", value: 20 },
+      { date: "2018-01-15", value: 30 }
+    ])
   end
 
   it 'return the total value for time period' do
     series = Queries::Series.new('pviews', Facts::Metric.all)
     expect(series.total).to eq 60
-  end
-
-  def expected_values
-    [
-      { date: "2018-01-13", value: 10 },
-      { date: "2018-01-14", value: 20 },
-      { date: "2018-01-15", value: 30 }
-    ]
   end
 end
