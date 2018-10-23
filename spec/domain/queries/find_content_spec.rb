ace2060d772e5cf305b90319141084ceb677702a
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-23 15:09:38 +0100

    DRY-up RSpec test for FindContent

--- spec/domain/queries/find_content_spec.rb
+++ spec/domain/queries/find_content_spec.rb
@@ -3,7 +3,14 @@ RSpec.describe Queries::FindContent do
   let(:warehouse_item_id) { '87d87ac6-e5b5-4065-a8b5-b7a43db648d2' }
   let(:another_warehouse_item_id) { 'ebf0dd2f-9d99-48e3-84d0-e94a2108ef45' }
 
-  let(:filter) { { from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id, document_type: nil } }
+  let(:filter) do
+    {
+      from: '2018-01-01',
+      to: '2018-02-01',
+      organisation_id: primary_org_id,
+      document_type: nil
+    }
+  end
 
   before do
     create :user
@@ -18,20 +25,9 @@ RSpec.describe Queries::FindContent do
         document_type: 'news_story',
         organisation_id: primary_org_id,
         warehouse_item_id: warehouse_item_id
-      create :metric,
-        edition: edition,
-        date: '2018-01-01',
-        upviews: 100,
-        useful_yes: 50,
-        useful_no: 20,
-        searches: 20
-      create :metric,
-        edition: edition,
-        date: '2018-01-02',
-        upviews: 133,
-        useful_yes: 150,
-        useful_no: 30,
-        searches: 200
+
+      create :metric, edition: edition, date: '2018-01-01', upviews: 100, useful_yes: 50, useful_no: 20, searches: 20
+      create :metric, edition: edition, date: '2018-01-02', upviews: 133, useful_yes: 150, useful_no: 30, searches: 200
 
       other_edition = create :edition,
         base_path: '/path/2',
@@ -40,20 +36,8 @@ RSpec.describe Queries::FindContent do
         document_type: 'press_release',
         organisation_id: primary_org_id,
         warehouse_item_id: another_warehouse_item_id
-      create :metric,
-        edition: other_edition,
-        date: '2018-01-01',
-        upviews: 5,
-        useful_yes: 5,
-        useful_no: 4,
-        searches: 4
-      create :metric,
-        edition: other_edition,
-        date: '2018-01-02',
-        upviews: 15,
-        useful_yes: 5,
-        useful_no: 6,
-        searches: 3
+      create :metric, edition: other_edition, date: '2018-01-01', upviews: 5, useful_yes: 5, useful_no: 4, searches: 4
+      create :metric, edition: other_edition, date: '2018-01-02', upviews: 15, useful_yes: 5, useful_no: 6, searches: 3
     end
 
     it 'aggregates the data by content item' do
@@ -86,20 +70,12 @@ RSpec.describe Queries::FindContent do
   context 'the attributes we are displaying have changed over time' do
     before do
       old_edition = create :edition,
-        base_path: '/old/base/path',
         date: '2018-01-01',
-        title: 'old title',
-        document_type: 'news_story',
         organisation_id: primary_org_id,
         latest: false,
         warehouse_item_id: warehouse_item_id
-      create :metric,
-        edition: old_edition,
-        date: '2018-01-02',
-        upviews: 100,
-        useful_yes: 10,
-        useful_no: 10,
-        searches: 15
+      create :metric, edition: old_edition, date: '2018-01-02', upviews: 100, useful_yes: 10, useful_no: 10, searches: 15
+
       new_edition = create :edition,
         replaces: old_edition,
         base_path: '/new/base/path',
@@ -107,47 +83,38 @@ RSpec.describe Queries::FindContent do
         title: 'new title',
         document_type: 'press_release',
         organisation_id: primary_org_id
-      create :metric,
-        edition: new_edition,
-        date: '2018-01-02',
-        upviews: 100,
-        useful_yes: 50,
-        useful_no: 50,
-        searches: 5
+      create :metric, edition: new_edition, date: '2018-01-02', upviews: 100, useful_yes: 50, useful_no: 50, searches: 5
     end
 
     it 'returns aggregated metrics from all versions with metadata from the latest version' do
       results = described_class.call(filter: filter)
-      expect(results.count).to eq(1)
-      expect(results.first).to eq(
-        base_path: '/new/base/path',
-        title: 'new title',
-        upviews: 200,
-        document_type: 'press_release',
-        satisfaction: 0.5,
-        satisfaction_score_responses: 120,
-        searches: 20
-      )
+      expect(results).to eq([
+        {
+          base_path: '/new/base/path',
+          title: 'new title',
+          upviews: 200,
+          document_type: 'press_release',
+          satisfaction: 0.5,
+          satisfaction_score_responses: 120,
+          searches: 20,
+        }])
     end
 
     it 'returns items matching the document_type of the latest version' do
-      by_document_type = { from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id, document_type: 'press_release' }
-      results = described_class.call(filter: by_document_type)
-      expect(results.count).to eq(1)
-      expect(results.first).to eq(
+      results = described_class.call(filter: filter.merge(document_type: 'press_release'))
+      expect(results).to eq([{
         base_path: '/new/base/path',
         title: 'new title',
         upviews: 200,
         document_type: 'press_release',
         satisfaction: 0.5,
         satisfaction_score_responses: 120,
-        searches: 20
-                               )
+        searches: 20,
+      }])
     end
 
     it 'does not return items matching the document_type of older versions' do
-      by_document_type = { from: '2018-01-01', to: '2018-02-01', organisation_id: primary_org_id, document_type: 'news_story' }
-      results = described_class.call(filter: by_document_type)
+      results = described_class.call(filter: filter.merge(document_type: 'news_story'))
       expect(results.count).to eq(0)
     end
   end
@@ -158,10 +125,10 @@ RSpec.describe Queries::FindContent do
         date: '2018-01-01',
         organisation_id: primary_org_id
       create :metric,
-             edition: edition,
-             date: '2018-01-01',
-             useful_yes: 0,
-             useful_no: 0
+        edition: edition,
+        date: '2018-01-01',
+        useful_yes: 0,
+        useful_no: 0
     end
 
     it 'returns the nil for the satisfaction' do
@@ -174,9 +141,7 @@ RSpec.describe Queries::FindContent do
   end
 
   context 'when no metrics in the date range' do
-    before do
-      create :edition, date: '2018-02-01'
-    end
+    before { create :edition, date: '2018-02-01' }
 
     it 'returns a empty array' do
       results = described_class.call(filter: filter)
