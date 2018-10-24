bb0ba6d1f0e29f04a611387aa58ca1d5d7083567
Pablo Manrubia <pmanrubia@gmail.com>
2018-10-24 13:18:56 +0100

    Add support for nil attributes in ContentRequestAPI

--- spec/domain/api/content_request_spec.rb
+++ spec/domain/api/content_request_spec.rb
@@ -1,17 +1,35 @@
 RSpec.describe Api::ContentRequest do
-  it '#to_filter' do
-    request = Api::ContentRequest.new(
-      document_type: 'guide',
-      organisation_id: 'the-id',
-      from: '2018-01-01',
-      to: '2018-01-31'
-    )
+  describe '#to_filter' do
+    it 'returns a hash with the parameters' do
+      request = Api::ContentRequest.new(
+        document_type: 'guide',
+        organisation_id: 'the-id',
+        from: '2018-01-01',
+        to: '2018-01-31',
+      )
 
-    expect(request.to_filter).to eq(
-      document_type: 'guide',
-      organisation_id: 'the-id',
-      from: '2018-01-01',
-      to: '2018-01-31'
-    )
+      expect(request.to_filter).to eq(
+        document_type: 'guide',
+        organisation_id: 'the-id',
+        from: '2018-01-01',
+        to: '2018-01-31',
+      )
+    end
+
+    it 'includes missing parameters' do
+      request = Api::ContentRequest.new(
+        document_type: nil,
+        organisation_id: nil,
+        from: nil,
+        to: nil,
+      )
+
+      expect(request.to_filter).to eq(
+        document_type: nil,
+        organisation_id: nil,
+        from: nil,
+        to: nil,
+      )
+    end
   end
 end
