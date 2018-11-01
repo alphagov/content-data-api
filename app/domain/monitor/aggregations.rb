d7e95960e525c781d235173a601a67b0346a524a
Pablo Manrubia <pmanrubia@gmail.com>
2018-11-01 09:55:30 +0000

    Add StatsD Monitor for aggregations

---
+++ app/domain/monitor/aggregations.rb
@@ -0,0 +1,32 @@
+class Monitor::Aggregations
+  def self.run(*args)
+    new(*args).run
+  end
+
+  def run
+    statsd_for_all_monthly_aggregations!
+    statsd_for_current_month!
+  end
+
+private
+
+  def statsd_for_all_monthly_aggregations!
+    path = path_for('all')
+    count = Aggregations::MonthlyMetric.count
+
+    GovukStatsd.count(path, count)
+  end
+
+  def statsd_for_current_month!
+    path = path_for('current')
+    count = Aggregations::MonthlyMetric
+              .where(dimensions_month: Dimensions::Month.current)
+              .count
+
+    GovukStatsd.count(path, count)
+  end
+
+  def path_for(item)
+    "monitor.aggregations.#{item}"
+  end
+end
---
+++ spec/domain/monitor/aggregations_spec.rb
@@ -0,0 +1,26 @@
+RSpec.describe Monitor::Aggregations do
+  around do |example|
+    Timecop.freeze(Date.new(2018, 3, 15)) { example.run }
+  end
+
+  let(:yesterday) { '2018-03-14' }
+
+  before { allow(GovukStatsd).to receive(:count) }
+
+  it 'sends StatsD counter for all the aggregations' do
+    expect(GovukStatsd).to receive(:count).with("monitor.aggregations.all", 2)
+
+    create_list :monthly_metric, 2
+
+    subject.run
+  end
+
+  it 'sends StatsD counter for `daily` metrics' do
+    expect(GovukStatsd).to receive(:count).with("monitor.aggregations.current", 3)
+
+    create_list :monthly_metric, 2, month: '2018-01'
+    create_list :monthly_metric, 3, month: '2018-03'
+
+    subject.run
+  end
+end
