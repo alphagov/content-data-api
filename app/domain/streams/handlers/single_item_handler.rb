289299e03c925d1bb21715696d7aea728d08be0b
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2018-11-15 16:57:03 +0000

    Improve SingleItemHandler for messages with no locale
    Some messages from the Publishing API currently don't include a
    locale, messages with a document_type of gone or redirect for example.
    
    This commit changes the SingleItemHandler to not depend on the locale
    for finding the old edition, falling back to the combination of the
    content_id, base_path, latest attributes if the locale isn't contained
    in the message.

--- app/domain/streams/handlers/single_item_handler.rb
+++ app/domain/streams/handlers/single_item_handler.rb
@@ -10,12 +10,16 @@ class Streams::Handlers::SingleItemHandler < Streams::Handlers::BaseHandler
   attr_reader :attrs, :old_edition
 
   def process
-    update_editions [attrs: attrs, old_edition: find_old_edition(attrs[:content_id], attrs[:locale])]
+    update_editions [attrs: attrs, old_edition: find_old_edition(attrs[:content_id], attrs[:locale], attrs[:base_path])]
   end
 
 private
 
-  def find_old_edition(content_id, locale)
-    Dimensions::Edition.find_by(content_id: content_id, locale: locale, latest: true)
+  def find_old_edition(content_id, locale, base_path)
+    if locale
+      Dimensions::Edition.find_by(content_id: content_id, locale: locale, latest: true)
+    else
+      Dimensions::Edition.find_by(content_id: content_id, base_path: base_path, latest: true)
+    end
   end
 end
---
+++ spec/domain/streams/handlers/single_item_handler_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe Streams::Handlers::SingleItemHandler do
+  let(:content_id) { SecureRandom.uuid }
+  let(:base_path) { '/base-path' }
+  let(:locale) { 'en' }
+
+  let(:old_edition) do
+    create(
+      :edition,
+      content_id: content_id,
+      base_path: base_path,
+      locale: 'en',
+      latest: true
+    )
+  end
+
+  subject { described_class.new(attrs) }
+
+  context 'when attrs match the old edition' do
+    let(:attrs) do
+      old_edition
+        .attributes
+        .slice('content_id', 'base_path', 'locale')
+        .symbolize_keys
+    end
+
+    context 'with a single english edition' do
+      it 'finds the old english edition when no attributes change' do
+        expect(subject).to receive(:update_editions).with(
+          [attrs: attrs, old_edition: old_edition]
+        )
+
+        subject.process
+      end
+    end
+
+    context 'with a single welsh edition' do
+      let(:base_path) { '/base-path.cy' }
+      let(:locale) { 'cy' }
+
+      it 'finds the old welsh edition when no attributes change' do
+        expect(subject).to receive(:update_editions).with(
+          [attrs: attrs, old_edition: old_edition]
+        )
+
+        subject.process
+      end
+    end
+  end
+
+  context 'when attrs differ from the old edition' do
+    context 'in the case of a redirect or gone with no locale' do
+      let(:attrs) do
+        {
+          content_id: content_id,
+          base_path: base_path
+        }
+      end
+
+      it 'finds the old edition' do
+        expect(subject).to receive(:update_editions).with(
+          [attrs: attrs, old_edition: old_edition]
+        )
+
+        subject.process
+      end
+    end
+  end
+end
