066d94853bd917f4c96cc28c4a5a7e093fdd1ba9
Sean Rankine <sean.rankine@me.com>
2018-11-20 12:56:58 +0000

    Revert "Merge pull request #1003 from alphagov/fix-redirect-constraint-errors"
    This reverts commit caecc748995de224fcfc9885d4d4e8c61ffe84c3, reversing
    changes made to eecff182f0abe4cd821942672ceef2606bdd5f2e.

--- app/domain/streams/messages/factory.rb
+++ app/domain/streams/messages/factory.rb
@@ -1,9 +1,7 @@
 module Streams::Messages
   class Factory
     def self.build(payload)
-      if RedirectMessage.is_redirect?(payload)
-        RedirectMessage.new(payload)
-      elsif MultipartMessage.is_multipart?(payload)
+      if MultipartMessage.is_multipart?(payload)
         MultipartMessage.new(payload)
       else
         SingleItemMessage.new(payload)
