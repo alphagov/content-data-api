1ee135b6b365068c5c67b1c58e54adee757b54b9
Sean Rankine <sean.rankine@me.com>
2018-11-22 10:59:13 +0000

    Add NOT NULL constraint to locales
    This adds a DB migrations to add a NOT NULL constraint for
    locales, however does not remove existing NULLs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_07_170246) do
+ActiveRecord::Schema.define(version: 2018_11_20_164728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2018_11_07_170246) do
     t.string "primary_organisation_title"
     t.string "organisation_id"
     t.boolean "primary_organisation_withdrawn"
-    t.string "locale"
+    t.string "locale", null: false
     t.bigint "publishing_api_payload_version", null: false
     t.string "content_purpose_supergroup"
     t.string "content_purpose_subgroup"
