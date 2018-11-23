b1fb57b61261b42aba4443294bf165923d6c7ba8
Sean Rankine <sean.rankine@me.com>
2018-11-23 15:55:19 +0000

    Revert "Merge pull request #1020 from alphagov/remove-unique-constratins"
    This reverts commit 1cec25df42d83dc6259d30ed27b4e44224bf43f2, reversing
    changes made to 25320dbbef49cb271d04eff9c9dbe8cf1fcf917e.

--- db/migrate/20181123142256_remove_constraints_base_path_latest.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConstraintsBasePathLatest < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :dimensions_editions, name: :index_dimensions_editions_on_latest_and_base_path
-  end
-end
--- db/migrate/20181123142713_remove_constraints_warehouse_item_id_latest.rb
+++
@@ -1,5 +0,0 @@
-class RemoveConstraintsWarehouseItemIdLatest < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :dimensions_editions, name: :index_dimensions_editions_on_latest_and_warehouse_item_id
-  end
-end
