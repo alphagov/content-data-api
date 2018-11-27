c62ba13179d26848e2807f81d20ae7848acd1180
Pablo Manrubia <pmanrubia@gmail.com>
2018-11-27 13:49:50 +0000

    Fix Lint issues

--- db/migrate/20181122152546_remove_default_zeros_from_aggregations_monthly_metrics.rb
+++ db/migrate/20181122152546_remove_default_zeros_from_aggregations_monthly_metrics.rb
@@ -1,6 +1,6 @@
 class RemoveDefaultZerosFromAggregationsMonthlyMetrics < ActiveRecord::Migration[5.2]
-  COLS = %i[pviews upviews feedex searches exits entrances bounces bounce_rate avg_page_time page_time]
-  
+  COLS = %i[pviews upviews feedex searches exits entrances bounces bounce_rate avg_page_time page_time].freeze
+
   def up
     COLS.each do |col|
       change_column_default(:aggregations_monthly_metrics, col, nil)
