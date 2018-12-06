3d35e164f8b022b769fabfb957a61432e3c5f7b0
Sean Rankine <sean.rankine@me.com>
2018-12-06 11:52:18 +0000

    Fix time period for past 3 months view
    The past 3 months views previously it included an extra day at the
    beginning of the period. Updated tests to check for correct
    boundaries.

---
+++ db/migrate/20181205165620_update_aggregations_search_last_three_months_to_version_3.rb
@@ -0,0 +1,5 @@
+class UpdateAggregationsSearchLastThreeMonthsToVersion3 < ActiveRecord::Migration[5.2]
+  def change
+    update_view :aggregations_search_last_three_months, version: 3, revert_to_version: 2, materialized: true
+  end
+end
