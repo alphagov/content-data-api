205ea059912667fbe59db5b2402013f9f1fdab96
Pete Goddard <peter.goddard@digital.cabinet-office.gov.uk>
2018-12-13 09:32:36 +0000

    Document import data into VM

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_06_120629) do
+ActiveRecord::Schema.define(version: 2018_12_12_113107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
