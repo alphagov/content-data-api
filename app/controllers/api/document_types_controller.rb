0860465a571b3319d0c54a2952c8da1b029f2536
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-01-04 11:15:37 +0000

    Fix indentation in DocumentTypes controller

--- app/controllers/api/document_types_controller.rb
+++ app/controllers/api/document_types_controller.rb
@@ -1,6 +1,6 @@
 class Api::DocumentTypesController < Api::BaseController
   def index
-      @document_types = DocumentType.find_all
-      render json: @document_types
+    @document_types = DocumentType.find_all
+    render json: @document_types
   end
 end
