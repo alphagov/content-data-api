001921fcd13bacb8a1924fbeb11608b155140a01
Pete Goddard <pete@artofsoftware.co.uk>
2019-01-11 17:09:12 +0000

    Introduce Aggregations::Base
    To DRY the aggregations code.

--- app/models/aggregations/search_last_six_months.rb
+++ app/models/aggregations/search_last_six_months.rb
@@ -1,6 +1,2 @@
-class Aggregations::SearchLastSixMonths < ApplicationRecord
-  def self.refresh
-    Aggregations::MaterializedView.prepare
-    Scenic.database.refresh_materialized_view(table_name, concurrently: true, cascade: false)
-  end
+class Aggregations::SearchLastSixMonths < Aggregations::Base
 end
