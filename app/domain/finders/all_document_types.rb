1b81c031a0818d4e793cace8bbb25188e0ad0b76
Pablo Manrubia <pmanrubia@gmail.com>
2019-01-17 13:08:25 +0000

    DRY-up Finder for all DocumentTypes

--- app/domain/finders/all_document_types.rb
+++ app/domain/finders/all_document_types.rb
@@ -4,27 +4,23 @@ class Finders::AllDocumentTypes
   end
 
   def run
-    all_document_types = ActiveRecord::Base.connection
-                           .execute(distinct_document_types_sql)
-                           .field_values('document_type')
-
-    document_types = all_document_types.reject { |t| DocumentType::IGNORED_TYPES.include? t }
-
-    document_types.map do |document_type|
-      DocumentType.new(id: document_type, name: document_type.humanize)
-    end
+    ActiveRecord::Base.connection
+      .execute(query)
+      .field_values('document_type')
+      .reject { |dt| DocumentType::IGNORED_TYPES.include? dt }
+      .map { |dt| DocumentType.new(id: dt, name: dt.humanize) }
   end
 
 private
 
-  def distinct_document_types_sql
+  def query
     <<-SQL
       WITH RECURSIVE temp AS (
         (SELECT document_type FROM dimensions_editions WHERE latest ORDER BY document_type LIMIT 1)
-        UNION ALL
-        SELECT (SELECT document_type FROM dimensions_editions WHERE document_type > temp.document_type AND latest ORDER BY document_type LIMIT 1)
-        FROM temp
-        WHERE temp.document_type IS NOT NULL
+          UNION ALL
+         SELECT (SELECT document_type FROM dimensions_editions WHERE document_type > temp.document_type AND latest ORDER BY document_type LIMIT 1)
+         FROM temp
+         WHERE temp.document_type IS NOT NULL
       )
       SELECT document_type FROM temp WHERE document_type IS NOT NULL;
     SQL
