5dee045233566c9193e75250a558d040f221f886
Kelvin Gan <kelvin.gan@digital.cabinet-office.gov.uk>
2019-02-05 15:47:05 +0000

    Add migration to update reading time on latest items
    Previous commit added reading time field and calculation. This commit
    updates the Facts::Edition reading_time field for latest
    Dimensions::Edition content items. We don't need to update items in the
    past as we don't calculate time series data from this metric. This also
    has the benefit of speeding up the migration as it is a lot of data to
    run through otherwise.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_150122) do
+ActiveRecord::Schema.define(version: 2019_02_04_171432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
