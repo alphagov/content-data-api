d3e61864886e13d051328f1bbaeedb9539e5dd1d
Pablo Manrubia <pmanrubia@gmail.com>
2019-02-06 11:31:17 +0000

    Add RSpec context for Feedex service
    This is a preparatory commit, as I am planning to handle the `no results`
    response next.

--- spec/domain/etl/feedex/service_spec.rb
+++ spec/domain/etl/feedex/service_spec.rb
@@ -3,93 +3,95 @@ RSpec.describe Etl::Feedex::Service do
   let(:subject) { described_class.new(date, 20, support_api) }
 
   describe '#find_in_batches' do
-    let(:date) { Date.new(2018, 2, 1) }
-    let(:response_1) do
-      {
-        'results' => [
-          { 'path' => '/path/1', 'count' => 3 },
-          { 'path' => '/path/2', 'count' => 1 }
-        ],
-        'pages' => 3,
-        'current_page' => 1
-      }
-    end
-    let(:response_2) do
-      {
-        'results' => [
-          { 'path' => '/path/3', 'count' => 5 },
-          { 'path' => '/path/4', 'count' => 2 }
-        ],
-        'pages' => 3,
-        'current_page' => 2
-      }
-    end
-    let(:response_3) do
-      {
-        'results' => [
-          { 'path' => '/path/5', 'count' => 1 },
-          { 'path' => '/path/6', 'count' => 7 }
-        ],
-        'pages' => 3,
-        'current_page' => 3
-      }
-    end
+    context 'when there are results' do
+      let(:date) { Date.new(2018, 2, 1) }
+      let(:response_1) do
+        {
+          'results' => [
+            { 'path' => '/path/1', 'count' => 3 },
+            { 'path' => '/path/2', 'count' => 1 }
+          ],
+          'pages' => 3,
+          'current_page' => 1
+        }
+      end
+      let(:response_2) do
+        {
+          'results' => [
+            { 'path' => '/path/3', 'count' => 5 },
+            { 'path' => '/path/4', 'count' => 2 }
+          ],
+          'pages' => 3,
+          'current_page' => 2
+        }
+      end
+      let(:response_3) do
+        {
+          'results' => [
+            { 'path' => '/path/5', 'count' => 1 },
+            { 'path' => '/path/6', 'count' => 7 }
+          ],
+          'pages' => 3,
+          'current_page' => 3
+        }
+      end
 
-    before :each do
-      allow(support_api).to receive(:feedback_by_day)
-        .with(date, 1, 20).and_return(response_1)
-      allow(support_api).to receive(:feedback_by_day)
-        .with(date, 2, 20).and_return(response_2)
-      allow(support_api).to receive(:feedback_by_day)
-        .with(date, 3, 20).and_return(response_3)
-    end
+      before :each do
+        allow(support_api).to receive(:feedback_by_day)
+                                .with(date, 1, 20).and_return(response_1)
+        allow(support_api).to receive(:feedback_by_day)
+                                .with(date, 2, 20).and_return(response_2)
+        allow(support_api).to receive(:feedback_by_day)
+                                .with(date, 3, 20).and_return(response_3)
+      end
 
-    it 'makes the correct request and yields the first page' do
-      expected_date = Date.parse('01/02/2018')
+      it 'makes the correct request and yields the first page' do
+        expected_date = Date.parse('01/02/2018')
 
-      expect { |b| subject.find_in_batches(&b) }.to yield_successive_args(
-        [
-          {
-            date: expected_date,
-            page_path: '/path/1',
-            feedex_comments: 3
-          },
-          {
-            date: expected_date,
-            page_path: '/path/2',
-            feedex_comments: 1
-          }
-        ],
-        [
-          {
-            date: expected_date,
-            page_path: '/path/3',
-            feedex_comments: 5
-          },
-          {
-            date: expected_date,
-            page_path: '/path/4',
-            feedex_comments: 2
-          }
-        ],
-        [
-          {
-            date: expected_date,
-            page_path: '/path/5',
-            feedex_comments: 1
-          },
-          {
-            date: expected_date,
-            page_path: '/path/6',
-            feedex_comments: 7
-          }
-        ]
-      )
+        expect { |b| subject.find_in_batches(&b) }.to yield_successive_args(
+          [
+            {
+              date: expected_date,
+              page_path: '/path/1',
+              feedex_comments: 3
+            },
+            {
+              date: expected_date,
+              page_path: '/path/2',
+              feedex_comments: 1
+            }
+          ],
+          [
+            {
+              date: expected_date,
+              page_path: '/path/3',
+              feedex_comments: 5
+            },
+            {
+              date: expected_date,
+              page_path: '/path/4',
+              feedex_comments: 2
+            }
+          ],
+          [
+            {
+              date: expected_date,
+              page_path: '/path/5',
+              feedex_comments: 1
+            },
+            {
+              date: expected_date,
+              page_path: '/path/6',
+              feedex_comments: 7
+            }
+          ]
+        )
 
-      expect(support_api).to have_received(:feedback_by_day).with(expected_date, 1, 20)
-      expect(support_api).to have_received(:feedback_by_day).with(expected_date, 2, 20)
-      expect(support_api).to have_received(:feedback_by_day).with(expected_date, 3, 20)
-      expect(support_api).not_to have_received(:feedback_by_day).with(expected_date, 4, 20)
+        expect(support_api).to have_received(:feedback_by_day).with(expected_date, 1, 20)
+        expect(support_api).to have_received(:feedback_by_day).with(expected_date, 2, 20)
+        expect(support_api).to have_received(:feedback_by_day).with(expected_date, 3, 20)
+        expect(support_api).not_to have_received(:feedback_by_day).with(expected_date, 4, 20)
+      end
     end
   end
 end
