392206fd9f5226daa7d2f9091d414a556a75c717
Pablo Manrubia <pmanrubia@gmail.com>
2019-02-06 14:22:21 +0000

    Extract concern for enabling health check in timeframe

--- spec/domain/healthchecks/daily_metrics_check_spec.rb
+++ spec/domain/healthchecks/daily_metrics_check_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Healthchecks::DailyMetricsCheck do
-  let(:today) { Time.new(2018, 1, 15, 16, 0, 0) }
-
-  around do |example|
-    Timecop.freeze(today) { example.run }
-  end
+  include_examples 'Healthcheck enabled/disabled within time range'
 
   describe '#status' do
     context 'When there are metrics' do
@@ -22,45 +18,6 @@ RSpec.describe Healthchecks::DailyMetricsCheck do
       it 'returns status :ok' do
         expect(subject.status).to eq(:critical)
       end
-    end
-  end
-
-  describe '#enabled?' do
-    around do |example|
-      @healthcheck_enabled = ENV['ETL_HEALTHCHECK_ENABLED']
-      @healthcheck_hour = ENV['ETL_HEALTHCHECK_ENABLED_FROM_HOUR']
-      example.run
-      ENV['ETL_HEALTHCHECK_ENABLED'] = @healthcheck_enabled
-      ENV['ETL_HEALTHCHECK_ENABLED_FROM_HOUR'] = @healthcheck_hour
-    end
-
-    context 'when ETL checks are enabled' do
-      before do
-        ENV['ETL_HEALTHCHECK_ENABLED'] = '1'
-        ENV['ETL_HEALTHCHECK_ENABLED_FROM_HOUR'] = '9'
-      end
-
-      context 'within time range' do
-        let(:today) { Time.new(2018, 1, 15, 9, 1, 0) }
-
-        it { is_expected.to be_enabled }
-      end
-
-      context 'out of time range' do
-        let(:today) { Time.new(2018, 1, 15, 1, 0, 0) }
-
-        it { is_expected.to_not be_enabled }
-      end
-    end
-
-    context 'when ETL checks are not enabled' do
-      before do
-        ENV.delete 'ETL_HEALTHCHECK_ENABLED'
-      end
-
-      let(:today) { Time.new(2018, 1, 15, 20, 0, 0) }
-
-      it { is_expected.to_not be_enabled }
     end
   end
 end
