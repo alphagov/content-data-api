50d5012be6bec28fc2a4f713c5731c0e685764e2
Kelvin Gan <dev@kelvingan.co.uk>
2019-02-15 14:14:59 +0000

    Add rake task to repopulate Etl::Aggregations::Monthly
    This provides an action to take for 2ndline support if our monitoring
    alerts them to there being a problem during the nightly Etl process.

--- spec/tasks/etl_spec.rb
+++ spec/tasks/etl_spec.rb
@@ -2,15 +2,30 @@ RSpec.describe 'etl.rake', type: task do
   describe 'rake etl:master'
   it "calls Etl::Master::MasterProcessor.process" do
     processor = class_double(Etl::Master::MasterProcessor, process: true).as_stubbed_const
+
     expect(processor).to receive(:process)
 
     Rake::Task['etl:master'].invoke
   end
 
+  describe 'rake etl:repopulate_aggregations_month' do
+    it 'calls Etl::Aggregations::Monthly with each date' do
+      processor = class_double(Etl::Aggregations::Monthly, process: true).as_stubbed_const
+
+      Rake::Task['etl:repopulate_aggregations_month'].invoke('2018-11-01', '2018-11-03')
+
+      expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 1))
+      expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 2))
+      expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 3))
+    end
+  end
+
   describe 'rake etl:repopulateviews' do
     it 'calls Etl::GA::ViewsAndNavigationProcessor with each date' do
       processor = class_double(Etl::GA::ViewsAndNavigationProcessor, process: true).as_stubbed_const
+
       Rake::Task['etl:repopulateviews'].invoke('2018-11-01', '2018-11-03')
+
       expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 1))
       expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 2))
       expect(processor).to have_received(:process).with(date: Date.new(2018, 11, 3))
