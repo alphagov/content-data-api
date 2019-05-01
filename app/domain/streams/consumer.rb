49f908a5dc99d0309cd743d6cbec666aae05df1d
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-05-01 13:51:17 +0100

    Remove routing key check from consumer
    Within the consumer we check to see whether message have correct
    routing key suffix. However routing keys should be guaranteed
    with bindings setup in RabbitMQ.

--- app/domain/streams/consumer.rb
+++ app/domain/streams/consumer.rb
@@ -1,26 +1,12 @@
 module Streams
   class Consumer
     def process(message)
-      if valid_routing_key?(message)
-        Streams::MessageProcessorJob.perform_later(message.payload, message.delivery_info.routing_key)
-      else
-        Monitor::Messages.increment_discarded
-      end
-
+      Streams::MessageProcessorJob.perform_later(message.payload, message.delivery_info.routing_key)
       message.ack
     rescue StandardError => e
       GovukError.notify(e)
       message.discard
+      Monitor::Messages.increment_discarded
     end
-
-  private
-
-    def valid_routing_key?(message)
-      routing_key = message.delivery_info.routing_key
-
-      ROUTING_KEYS.any? { |suffix| routing_key.ends_with?(suffix) }
-    end
-
-    ROUTING_KEYS = %w(links major minor unpublish bulk.data-warehouse).freeze
   end
 end
--- spec/domain/streams/consumer_spec.rb
+++ spec/domain/streams/consumer_spec.rb
@@ -1,7 +1,8 @@
 RSpec.describe Streams::Consumer do
   subject { described_class.new }
-  it 'increments `discard` in statsd when routing key is not valid' do
-    message = build(:message, routing_key: 'invalid_routing_key')
+  it 'increments `discard` in statsd when error is raised' do
+    message = build(:message)
+    allow(Streams::MessageProcessorJob).to receive(:perform_later).and_raise(StandardError)
     expect(GovukStatsd).to receive(:increment).with("monitor.messages.discarded")
 
     subject.process(message)
