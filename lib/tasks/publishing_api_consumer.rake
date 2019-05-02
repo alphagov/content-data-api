d60ac3105c0508592d1f86ebde33939366f2dfc6
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-05-02 14:23:56 +0100

    Add concurrent consumers
    This intialises concurrent consumers to process messages from
    Publishing API as Sidekiq has been removed.

--- lib/tasks/publishing_api_consumer.rake
+++ lib/tasks/publishing_api_consumer.rake
@@ -1,27 +1,30 @@
 require_relative '../../app/domain/streams/consumer'
 
+def consume_from(queue_name)
+  begin
+    consumers = []
+
+    3.times do
+      consumers << GovukMessageQueueConsumer::Consumer.new(
+        queue_name: queue_name,
+        processor: Streams::Consumer.new,
+      ).run(subscribe_opts: { block: false })
+    end
+
+    sleep 10 until consumers.all? { |c| c.channel.closed? }
+  rescue SignalException
+    logger.info "SignalException suppressed"
+  end
+end
+
 namespace :publishing_api do
   desc "Run worker to publishing API from rabbitmq"
   task consumer: :environment do
-    begin
-      GovukMessageQueueConsumer::Consumer.new(
-        queue_name: ENV['RABBITMQ_QUEUE'],
-        processor: Streams::Consumer.new,
-      ).run
-    rescue SignalException
-      logger.info "SignalException suppressed"
-    end
+    consume_from(ENV['RABBITMQ_QUEUE'])
   end
 
   desc "Run worker to publishing API from rabbitmq for bulk import"
   task bulk_import_consumer: :environment do
-    begin
-      GovukMessageQueueConsumer::Consumer.new(
-        queue_name: ENV['RABBITMQ_QUEUE_BULK'],
-        processor: Streams::Consumer.new,
-      ).run
-    rescue SignalException
-      logger.info "SignalException suppressed"
-    end
+    consume_from(ENV['RABBITMQ_QUEUE_BULK'])
   end
 end
--- spec/tasks/publishing_api_spec.rb
+++ spec/tasks/publishing_api_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe 'rake publishing_api:*', type: task do
-  let(:consumer) { double('message_consumer', run: nil) }
+  let(:num_of_consumers) { 3 }
+  let(:mock_consumers) { build_mock_consumers(num_of_consumers) }
 
   before(:all) do
     ENV['RABBITMQ_QUEUE'] = 'content_data_api'
@@ -10,7 +11,11 @@ RSpec.describe 'rake publishing_api:*', type: task do
     expect(GovukMessageQueueConsumer::Consumer).to receive(:new).with(
       queue_name: 'content_data_api',
       processor: an_instance_of(Streams::Consumer),
-    ).and_return(consumer)
+    ).and_return(*mock_consumers)
+
+    num_of_consumers.times do |i|
+      expect(mock_consumers[i]).to receive(:run).with(subscribe_opts: { block: false })
+    end
 
     Rake::Task['publishing_api:consumer'].invoke
   end
@@ -19,8 +24,22 @@ RSpec.describe 'rake publishing_api:*', type: task do
     expect(GovukMessageQueueConsumer::Consumer).to receive(:new).with(
       queue_name: 'content_data_api_govuk_importer',
       processor: an_instance_of(Streams::Consumer),
-    ).and_return(consumer)
+    ).and_return(*mock_consumers)
+
+    num_of_consumers.times do |i|
+      expect(mock_consumers[i]).to receive(:run).with(subscribe_opts: { block: false })
+    end
 
     Rake::Task['publishing_api:bulk_import_consumer'].invoke
+  end
+end
+
+def build_mock_consumers(num_of_consumers)
+  # sets up the mock consumers and mock channels
+  # channels are stubbed to appear closed to prevent inifite looping
+  (1..num_of_consumers).map do |i|
+    channel = double("bunny_channel#{i}", closed?: true)
+    bunny_consumer = double("bunny_consumer#{i}", channel: channel)
+    double("govuk_consumer#{i}", run: bunny_consumer)
   end
 end
