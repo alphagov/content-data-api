e36a72b40c073526e686cfa9fbb16f240ac3b539
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2019-05-23 22:58:52 +0100

    Add dead letter queue consumer rake task

--- lib/tasks/publishing_api_consumer.rake
+++ lib/tasks/publishing_api_consumer.rake
@@ -1,17 +1,29 @@
 require_relative '../../app/domain/streams/consumer'
 
-def consume_from(queue_name)
-  begin
-    consumers = []
+def setup_consumers(queue_name, number_of_consumers)
+  Array.new(number_of_consumers) do
+    GovukMessageQueueConsumer::Consumer.new(
+      queue_name: queue_name,
+      processor: Streams::Consumer.new,
+    ).run(subscribe_opts: { block: false })
+  end
+end
 
-    3.times do
-      consumers << GovukMessageQueueConsumer::Consumer.new(
-        queue_name: queue_name,
-        processor: Streams::Consumer.new,
-      ).run(subscribe_opts: { block: false })
-    end
+def consume_forever_from(queue_name)
+  begin
+    consumers = setup_consumers(queue_name, 3)
 
     sleep 10 until consumers.all? { |c| c.channel.closed? }
+  rescue SignalException
+    logger.info "SignalException suppressed"
+  end
+end
+
+def consume_until_empty_from(queue_name)
+  begin
+    consumers = setup_consumers(queue_name, 1)
+
+    sleep 10 while consumers.all? { |c| c.queue.message_count.positive? }
   rescue SignalException
     logger.info "SignalException suppressed"
   end
@@ -20,11 +32,16 @@ end
 namespace :publishing_api do
   desc "Run worker to publishing API from rabbitmq"
   task consumer: :environment do
-    consume_from(ENV['RABBITMQ_QUEUE'])
+    consume_forever_from(ENV['RABBITMQ_QUEUE'])
   end
 
   desc "Run worker to publishing API from rabbitmq for bulk import"
   task bulk_import_consumer: :environment do
-    consume_from(ENV['RABBITMQ_QUEUE_BULK'])
+    consume_forever_from(ENV['RABBITMQ_QUEUE_BULK'])
+  end
+
+  desc "Run consumers to re-process the dead letter queue"
+  task dead_letter_consumer: :environment do
+    consume_until_empty_from(ENV['RABBITMQ_QUEUE_DEAD'])
   end
 end
--- spec/tasks/publishing_api_spec.rb
+++ spec/tasks/publishing_api_spec.rb
@@ -1,19 +1,19 @@
 RSpec.describe 'rake publishing_api:*', type: task do
-  let(:num_of_consumers) { 3 }
-  let(:mock_consumers) { build_mock_consumers(num_of_consumers) }
-
   before(:all) do
     ENV['RABBITMQ_QUEUE'] = 'content_data_api'
     ENV['RABBITMQ_QUEUE_BULK'] = 'content_data_api_govuk_importer'
+    ENV['RABBITMQ_QUEUE_DEAD'] = 'content_data_api_dead_letter_queue'
   end
 
   it 'starts listener for PublishingAPI events' do
+    mock_consumers = build_mock_consumers(3)
+
     expect(GovukMessageQueueConsumer::Consumer).to receive(:new).with(
       queue_name: 'content_data_api',
       processor: an_instance_of(Streams::Consumer),
     ).and_return(*mock_consumers)
 
-    num_of_consumers.times do |i|
+    3.times do |i|
       expect(mock_consumers[i]).to receive(:run).with(subscribe_opts: { block: false })
     end
 
@@ -21,16 +21,33 @@ RSpec.describe 'rake publishing_api:*', type: task do
   end
 
   it 'starts listener for PublishingAPI bulk preload events' do
+    mock_consumers = build_mock_consumers(3)
+
     expect(GovukMessageQueueConsumer::Consumer).to receive(:new).with(
       queue_name: 'content_data_api_govuk_importer',
       processor: an_instance_of(Streams::Consumer),
     ).and_return(*mock_consumers)
 
-    num_of_consumers.times do |i|
+    3.times do |i|
       expect(mock_consumers[i]).to receive(:run).with(subscribe_opts: { block: false })
     end
 
     Rake::Task['publishing_api:bulk_import_consumer'].invoke
+  end
+
+  it 'starts listener for PublishingAPI dead letter events' do
+    mock_consumers = build_mock_consumers(1)
+
+    expect(GovukMessageQueueConsumer::Consumer).to receive(:new).with(
+      queue_name: 'content_data_api_dead_letter_queue',
+      processor: an_instance_of(Streams::Consumer),
+    ).and_return(*mock_consumers)
+
+    1.times do |i|
+      expect(mock_consumers[i]).to receive(:run).with(subscribe_opts: { block: false })
+    end
+
+    Rake::Task['publishing_api:dead_letter_consumer'].invoke
   end
 end
 
@@ -38,8 +55,9 @@ def build_mock_consumers(num_of_consumers)
   # sets up the mock consumers and mock channels
   # channels are stubbed to appear closed to prevent inifite looping
   (1..num_of_consumers).map do |i|
+    queue = double("bunny_queue#{i}", message_count: 0)
     channel = double("bunny_channel#{i}", closed?: true)
-    bunny_consumer = double("bunny_consumer#{i}", channel: channel)
+    bunny_consumer = double("bunny_consumer#{i}", channel: channel, queue: queue)
     double("govuk_consumer#{i}", run: bunny_consumer)
   end
 end
