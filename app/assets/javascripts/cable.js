b5689c8fceb2ed94cdf66ce1a2472bb1b4cf64e7
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2019-11-06 14:13:25 +0000

    Remove the unused assets directories

--- app/assets/javascripts/cable.js
+++
@@ -1,9 +0,0 @@
-// Action Cable provides the framework to deal with WebSockets in Rails.
-// You can generate new channels where WebSocket features live using the rails generate channel command.
-//
-//= require_self
-//= require_tree ./channels
-
-(function() {
-  this.App || (this.App = {});
-}).call(this);
