8802bc77759d7c24933e70b5b60fffeb05bfd875
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-08-05 08:44:00 +0100

    Remove 'before' blocks to make tests clearer
    'before' blocks should be used for setup code. They should
    not be used to perform actions on the subject that we then
    assert against in the tests themselves.
    I think someone wanted to DRY the tests up but the result
    is harder to comprehend, and harder to add to.
    
    In the next commit I'll be adding a test which relies on
    mocking the behaviour that would otherwise have been
    swallowed up in the 'before' block.

--- spec/models/dimensions/edition_spec.rb
+++ spec/models/dimensions/edition_spec.rb
@@ -98,15 +98,13 @@ RSpec.describe Dimensions::Edition, type: :model do
       let(:warehouse_item_id) { "warehouse-item-id" }
       let(:old_edition) { build :edition, warehouse_item_id: warehouse_item_id }
 
-      before do
-        edition.promote!(old_edition)
-      end
-
       it "set the live attribute to true" do
+        edition.promote!(old_edition)
         expect(edition.live).to be true
       end
 
       it "sets the live attribute to false for the old version" do
+        edition.promote!(old_edition)
         expect(old_edition.live).to be false
       end
     end
@@ -116,15 +114,13 @@ RSpec.describe Dimensions::Edition, type: :model do
       let(:warehouse_item_id) { "warehouse-item-id" }
       let(:old_edition) { build :edition, warehouse_item_id: warehouse_item_id }
 
-      before do
-        edition.promote!(old_edition)
-      end
-
       it "set the live attribute to true" do
+        edition.promote!(old_edition)
         expect(edition.live).to be false
       end
 
       it "sets the live attribute to false for the old version" do
+        edition.promote!(old_edition)
         expect(old_edition.live).to be false
       end
     end
