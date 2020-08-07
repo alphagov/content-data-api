297445e59e99ec2084144156ca89286086219e49
ChrisBAshton <christopher.ashton@digital.cabinet-office.gov.uk>
2020-08-07 10:57:41 +0100

    Fix some test descriptions

--- spec/models/dimensions/edition_spec.rb
+++ spec/models/dimensions/edition_spec.rb
@@ -40,7 +40,7 @@ RSpec.describe Dimensions::Edition, type: :model do
   end
 
   describe ".find_latest" do
-    it "return the most recent editon for a content item" do
+    it "returns the most recent editon for a content item" do
       content_id = SecureRandom.uuid
       edition1 = create :edition, content_id: content_id, locale: "en"
       edition2 = create :edition, content_id: content_id, locale: "en", replaces: edition1
@@ -50,7 +50,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       expect(latest_edition).to eq(edition2)
     end
 
-    it "return the most recent editon for a content item for locale" do
+    it "returns the most recent editon for a content item for locale" do
       content_id = SecureRandom.uuid
       edition1 = create :edition, content_id: content_id, locale: "en"
       edition2 = create :edition, content_id: content_id, locale: "en", replaces: edition1
@@ -61,7 +61,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       expect(latest_edition).to eq(edition2)
     end
 
-    it "return the most recent editon for content id" do
+    it "returns the most recent editon for content id" do
       content_id = SecureRandom.uuid
       edition1 = create :edition, content_id: content_id, locale: "en"
       edition2 = create :edition, content_id: content_id, locale: "en", replaces: edition1
@@ -73,7 +73,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       expect(latest_edition).to eq(edition2)
     end
 
-    it "return nil for no edition for locale" do
+    it "returns nil for no edition for locale" do
       create :edition, locale: "cy"
       content_id = SecureRandom.uuid
 
@@ -82,7 +82,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       expect(latest_edition).to eq(nil)
     end
 
-    it "return nil for no edition for content_id" do
+    it "returns nil for no edition for content_id" do
       create :edition
       other_content_id = SecureRandom.uuid
 
@@ -98,7 +98,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       let(:warehouse_item_id) { "warehouse-item-id" }
       let(:old_edition) { build :edition, warehouse_item_id: warehouse_item_id }
 
-      it "set the live attribute to true" do
+      it "sets the live attribute to true" do
         edition.promote!(old_edition)
         expect(edition.live).to be true
       end
@@ -114,7 +114,7 @@ RSpec.describe Dimensions::Edition, type: :model do
       let(:warehouse_item_id) { "warehouse-item-id" }
       let(:old_edition) { build :edition, warehouse_item_id: warehouse_item_id }
 
-      it "set the live attribute to true" do
+      it "sets the live attribute to false" do
         edition.promote!(old_edition)
         expect(edition.live).to be false
       end
@@ -238,13 +238,19 @@ RSpec.describe Dimensions::Edition, type: :model do
   end
 
   describe "Unique constraint on `base_path` and `live`" do
-    it "prevent duplicating `base_path` for live items" do
+    it "prevents duplicating `base_path` for live items" do
       create :edition, base_path: "value", live: true
 
       expect(-> { create :edition, base_path: "value", live: true }).to raise_error(ActiveRecord::RecordNotUnique)
     end
 
     it "does not prevent duplicating `base_path` for old items" do
+      create :edition, base_path: "value", live: true
+
+      expect(-> { create :edition, base_path: "value", live: false }).to_not raise_error
+    end
+
+    it "does not prevent duplicating `base_path` for unpublished items" do
       create :edition, base_path: "value", live: true
 
       expect(-> { create :edition, base_path: "value", live: false }).to_not raise_error
