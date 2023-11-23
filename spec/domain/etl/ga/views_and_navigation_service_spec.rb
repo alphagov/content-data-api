e474afb5f727e778466ba4ee277a9a884191e1ff
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2023-11-23 17:06:59 +0000

    Update Views & Navigation service to pull metrics from GA4/BigQuery
    The metrics required will be pre-stored in a table in BigQuery. It
    will contain data from when GA4 was first made available (since 2022),
    and will be configured to pull in data from the past 24hrs daily at
    ~3am where there aren't many users on the site. This may need to be
    tweaked later to ensure the data is available before the scheduled
    cron tasks are run in each environment (https://github.com/alphagov/govuk-helm-charts/blob/main/charts/app-config/values-production.yaml#L499-L580)
    
    Instead of fetching data from UA we now use the BigQuery API which
    allows us to query the table for the specific metrics we need. It
    already returns the information we need in the correct format and
    reduces the amount of work required post query.

--- spec/domain/etl/ga/views_and_navigation_service_spec.rb
+++ spec/domain/etl/ga/views_and_navigation_service_spec.rb
@@ -1,55 +1,44 @@
-require "google/apis/analyticsreporting_v4"
-
 RSpec.describe Etl::GA::ViewsAndNavigationService do
-  include GoogleAnalyticsRequests
-
   subject { Etl::GA::ViewsAndNavigationService }
 
-  let(:google_client) { instance_double(Google::Apis::AnalyticsreportingV4::AnalyticsReportingService) }
-  before { expect(Etl::GA::Client).to receive(:build).and_return(google_client) }
+  let(:google_client) { instance_double(Google::Cloud::Bigquery::Project) }
+  let(:results) { instance_double(Google::Cloud::Bigquery::Data) }
+
+  before do
+    allow(Etl::GA::Bigquery).to receive(:build).and_return(google_client)
+    allow(google_client).to receive(:query).and_return(results)
+
+    allow(results).to receive(:all).and_return([
+      { "page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20" },
+      { "page_path" => "/bar", "pviews" => 2, "upviews" => 2, "date" => "2018-02-20" },
+      { "page_path" => "/cool", "pviews" => 3, "upviews" => 3, "date" => "2018-02-20" },
+    ])
+  end
 
   describe "#find_in_batches" do
     let(:date) { Date.new(2018, 2, 20) }
 
     context "when #find_in_batches is called with a block" do
-      before do
-        allow(google_client).to receive(:fetch_all) do
-          [
-            build_report_data(
-              build_report_row(dimensions: %w[/foo], metrics: %w[1 1 1 1]),
-            ),
-            build_report_data(
-              build_report_row(dimensions: %w[/bar], metrics: %w[2 2 2 2]),
-            ),
-            build_report_data(
-              build_report_row(dimensions: %w[/cool], metrics: %w[3 3 3]),
-            ),
-          ]
-        end
-      end
-
       it "yields successive report data" do
         arg1 = [
-          a_hash_including(
-            "page_path" => "/foo",
-            "pviews" => 1,
-            "upviews" => 1,
-            "date" => "2018-02-20",
-          ),
-          a_hash_including(
-            "page_path" => "/bar",
-            "pviews" => 2,
-            "upviews" => 2,
-            "date" => "2018-02-20",
-          ),
+          a_hash_including("page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20"),
+          a_hash_including("page_path" => "/bar", "pviews" => 2, "upviews" => 2, "date" => "2018-02-20"),
         ]
         arg2 = [
-          a_hash_including(
-            "page_path" => "/cool",
-            "pviews" => 3,
-            "upviews" => 3,
-            "date" => "2018-02-20",
-          ),
+          a_hash_including("page_path" => "/cool", "pviews" => 3, "upviews" => 3, "date" => "2018-02-20"),
+        ]
+
+        expect { |probe| subject.find_in_batches(date:, batch_size: 2, &probe) }
+          .to yield_successive_args(arg1, arg2)
+      end
+
+      it "yields successive report data including the process name" do
+        arg1 = [
+          a_hash_including("process_name" => "views"),
+          a_hash_including("process_name" => "views"),
+        ]
+        arg2 = [
+          a_hash_including("process_name" => "views"),
         ]
 
         expect { |probe| subject.find_in_batches(date:, batch_size: 2, &probe) }
@@ -59,27 +48,16 @@ RSpec.describe Etl::GA::ViewsAndNavigationService do
 
     context "when report data has page path with long query strings" do
       it "should ignore page" do
-        allow(google_client).to receive(:fetch_all) do
+        allow(results).to receive(:all) do
           [
-            build_report_data(
-              build_report_row(dimensions: %w[/foo], metrics: %w[1 1 1]),
-            ),
-            build_report_data(
-              build_report_row(dimensions: [path_with_long_query], metrics: %w[1 1 1 1]),
-            ),
-            build_report_data(
-              build_report_row(dimensions: [long_path], metrics: %w[1 1 1 1]),
-            ),
+            { "page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20" },
+            { "page_path" => path_with_long_query, "pviews" => 2, "upviews" => 2, "date" => "2018-02-20" },
+            { "page_path" => long_path, "pviews" => 3, "upviews" => 3, "date" => "2018-02-20" },
           ]
         end
 
         arg = [
-          a_hash_including(
-            "page_path" => "/foo",
-            "pviews" => 1,
-            "upviews" => 1,
-            "date" => "2018-02-20",
-          ),
+          a_hash_including("page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20"),
         ]
 
         expect { |probe| subject.find_in_batches(date:, batch_size: 1, &probe) }
@@ -89,24 +67,15 @@ RSpec.describe Etl::GA::ViewsAndNavigationService do
 
     context "when report data has page path with an invalid URI" do
       it "should ignore page" do
-        allow(google_client).to receive(:fetch_all) do
+        allow(results).to receive(:all) do
           [
-            build_report_data(
-              build_report_row(dimensions: %w[/foo], metrics: %w[1 1 1 1]),
-            ),
-            build_report_data(
-              build_report_row(dimensions: [path_with_invalid_uri], metrics: %w[1 1 1 1]),
-            ),
+            { "page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20" },
+            { "page_path" => path_with_invalid_uri, "pviews" => 2, "upviews" => 2, "date" => "2018-02-20" },
           ]
         end
 
         arg = [
-          a_hash_including(
-            "page_path" => "/foo",
-            "pviews" => 1,
-            "upviews" => 1,
-            "date" => "2018-02-20",
-          ),
+          a_hash_including("page_path" => "/foo", "pviews" => 1, "upviews" => 1, "date" => "2018-02-20"),
         ]
 
         expect { |probe| subject.find_in_batches(date:, batch_size: 1, &probe) }
