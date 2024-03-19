3b29dd7e76eca97457a93c7796e3252ae8acf578
D Chua <deborah.chua@digital.cabinet-office.gov.uk>
2024-03-19 17:20:14 +0000

    Update Internal Search service to pull metrics from GA4
    Instead of fetching data from UA we now use the BigQuery API which
    allows us to query the table for the specific metrics we need from
    GA4. It already returns the information we need in the correct format
    and reduces the amount of work required post query.
    
    `ga_response_with_govuk_prefix` has not been used in any of the tests and
    looks like it has been missed previously. We should expect paths that
    contain the gov.uk prefix to be transformed into it's base path. If an
    an event already exists with the same path in the Events::GA table, it
    should update the entry with the aggregative total for the metrics which is
    then reflected in the Facts::Metrics table.

--- app/domain/etl/ga/internal_search_service.rb
+++ app/domain/etl/ga/internal_search_service.rb
@@ -6,75 +6,38 @@ class Etl::GA::InternalSearchService
   def find_in_batches(date:, batch_size: 10_000, &block)
     fetch_data(date:)
       .lazy
-      .map(&:to_h)
-      .flat_map(&method(:extract_rows))
-      .map(&method(:extract_dimensions_and_metrics))
+      .map(&:stringify_keys)
       .map(&method(:append_labels))
-      .map { |hash| set_date(hash, date) }
       .each_slice(batch_size, &block)
   end
 
   def client
-    @client ||= Etl::GA::Client.build
+    @client ||= Etl::GA::Bigquery.build
   end
 
 private
 
-  def set_date(hash, date)
-    hash["date"] = date.strftime("%F")
-    hash
-  end
-
-  def append_labels(values)
-    page_path, searches = *values
-    {
-      "page_path" => page_path,
-      "searches" => searches,
-      "process_name" => "searches",
-    }
-  end
-
-  def extract_dimensions_and_metrics(row)
-    dimensions = row.fetch(:dimensions)
-    metrics = row.fetch(:metrics).flat_map do |metric|
-      metric.fetch(:values).map(&:to_i)
-    end
-
-    dimensions + metrics
-  end
-
-  def extract_rows(report)
-    report.fetch(:rows)
+  def append_labels(hash)
+    hash.merge("process_name" => "searches")
   end
 
   def fetch_data(date:)
-    @fetch_data ||= client.fetch_all(items: :data) do |page_token, service|
-      service
-        .batch_get_reports(
-          Google::Apis::AnalyticsreportingV4::GetReportsRequest.new(
-            report_requests: [build_request(date:).merge(page_token:)],
-          ),
-        )
-        .reports
-        .first
-    end
-  end
+    @fetch_data ||= client
+    @date = date.strftime("%Y-%m-%d")
 
-  def build_request(date:)
-    {
-      date_ranges: [
-        { start_date: date.to_fs("%Y-%m-%d"), end_date: date.to_fs("%Y-%m-%d") },
-      ],
-      dimensions: [
-        { name: "ga:searchStartPage" },
-      ],
-      hide_totals: true,
-      hide_value_ranges: true,
-      metrics: [
-        { expression: "ga:searchUniques" },
-      ],
-      page_size: 10_000,
-      view_id: ENV["GOOGLE_ANALYTICS_GOVUK_VIEW_ID"],
-    }
+    query = <<~SQL
+      WITH CTE1 AS (
+        SELECT *
+        FROM `govuk-content-data.ga4.GA4 dataform`
+        WHERE the_date = @date
+      )
+      SELECT
+        cleaned_page_location AS page_path,
+        total_searches AS searches,
+        the_date AS date,
+      FROM CTE1
+    SQL
+
+    @fetch_data.query(query, params: { date: @date }).all
   end
 end
