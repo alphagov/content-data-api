c8bcb4dd3bb0169ea0e6fd9fbf49b7cfd60eb755
pezholio <pezholio@gmail.com>
2026-03-12 10:18:20 +0000

    Correct Rubocop failures

--- app/domain/etl/main/metrics_processor.rb
+++ app/domain/etl/main/metrics_processor.rb
@@ -29,14 +29,14 @@ module Etl
         log process: :metrics, message: "got the Dimensions::Date"
         Dimensions::Edition.live.find_in_batches(batch_size: 5_000)
           .with_index do |batch, index|
-          log process: :metrics, message: "processing #{batch.length} items in batch #{index}"
-          values = batch.pluck(:id).map do |value|
-            {
-              dimensions_date_id: dimensions_date.date,
-              dimensions_edition_id: value,
-            }
-          end
-          Facts::Metric.import(values, validate: false)
+            log process: :metrics, message: "processing #{batch.length} items in batch #{index}"
+            values = batch.pluck(:id).map do |value|
+              {
+                dimensions_date_id: dimensions_date.date,
+                dimensions_edition_id: value,
+              }
+            end
+            Facts::Metric.import(values, validate: false)
         end
       end
     end
